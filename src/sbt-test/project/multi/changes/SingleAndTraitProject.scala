84e98aae1ac4a0b72a60af31f336948343ba006e
dmharrah <dmharrah@d89573ee-9141-11dd-94d4-bdf5e562f29c>
2009-08-02 02:05:32 +0000

    Fixed broken test case
    git-svn-id: https://simple-build-tool.googlecode.com/svn/trunk@937 d89573ee-9141-11dd-94d4-bdf5e562f29c

--- src/sbt-test/project/multi/changes/SingleAndTraitProject.scala
+++ src/sbt-test/project/multi/changes/SingleAndTraitProject.scala
@@ -6,4 +6,4 @@ class TestProject(info: ProjectInfo) extends DefaultProject(info)
 
 trait NotAProject extends Project
 abstract class AnotherNonProject extends Project
-object YetAnotherNonProject extends DefaultProject(ProjectInfo(new java.io.File("."), Nil, None))
+object YetAnotherNonProject extends DefaultProject(ProjectInfo(new java.io.File("."), Nil, None)(new ConsoleLogger))
