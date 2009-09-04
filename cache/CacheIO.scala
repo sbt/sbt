b094fc3cc856ba7b76941d04ab7fa2dd269e10c4
Mark Harrah <dmharrah@gmail.com>
2009-09-03 23:40:47 -0400

    Mostly working cross-compile task. Analyzer plugin is now a proper internal phase to get around bootstrapping issues. Correctly handle source tags.

--- cache/CacheIO.scala
+++ cache/CacheIO.scala
@@ -6,6 +6,15 @@ import scala.reflect.Manifest
 
 object CacheIO
 {
+	def toBytes[T](format: Format[T])(value: T)(implicit mf: Manifest[Format[T]]): Array[Byte] =
+		toBytes[T](value)(format, mf)
+	def toBytes[T](value: T)(implicit format: Format[T], mf: Manifest[Format[T]]): Array[Byte] =
+		Operations.toByteArray(value)(stampedFormat(format))
+	def fromBytes[T](format: Format[T], default: => T)(bytes: Array[Byte])(implicit mf: Manifest[Format[T]]): T =
+		fromBytes(default)(bytes)(format, mf)
+	def fromBytes[T](default: => T)(bytes: Array[Byte])(implicit format: Format[T], mf: Manifest[Format[T]]): T =
+		if(bytes.isEmpty) default else Operations.fromByteArray(bytes)(stampedFormat(format))
+		
 	def fromFile[T](format: Format[T], default: => T)(file: File)(implicit mf: Manifest[Format[T]]): T =
 		fromFile(file, default)(format, mf)
 	def fromFile[T](file: File, default: => T)(implicit format: Format[T], mf: Manifest[Format[T]]): T =
