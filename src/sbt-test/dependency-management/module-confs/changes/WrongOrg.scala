feb7c0dc102be3c9f142c4832f821d65d61cec95
dmharrah <dmharrah@d89573ee-9141-11dd-94d4-bdf5e562f29c>
2009-09-25 13:11:54 +0000

    Test for inline Ivy module configurations
    git-svn-id: https://simple-build-tool.googlecode.com/svn/trunk@1048 d89573ee-9141-11dd-94d4-bdf5e562f29c

---
+++ src/sbt-test/dependency-management/module-confs/changes/WrongOrg.scala
@@ -0,0 +1,14 @@
+import sbt._
+
+class Test(info: ProjectInfo) extends DefaultProject(info)
+{
+	def ivyCacheDirectory = outputPath / "ivy-cache"
+	override def updateOptions =  CacheDirectory(ivyCacheDirectory) :: super.updateOptions.toList
+	
+	def snapshotPattern = "http://scala-tools.org/repo-snapshots/[organization]/[module]/2.8.0-SNAPSHOT/[artifact]-[revision].[ext]"
+	def scalaSnapshots = Resolver.url("Scala Tools Snapshots") artifacts(snapshotPattern) ivys(snapshotPattern) mavenStyle()
+	val scOnly = ModuleConfiguration("org.not-scala-lang", "*", "2.8.0-.*", scalaSnapshots)
+
+	val uniqueScala = "org.scala-lang" % "scala-compiler" % "2.8.0-20090910.003346-219"
+	val otherDep = "org.scala-tools.sxr" % "sxr_2.7.5" % "0.2.3"
+}
