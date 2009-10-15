51d8d242d78a22afcf6cde8c2b9532f1ef8b5f85
Mark Harrah <dmharrah@gmail.com>
2009-10-15 18:09:15 -0400

    some API documentation

--- launch/interface/src/main/java/xsbti/ScalaProvider.java
+++ launch/interface/src/main/java/xsbti/ScalaProvider.java
@@ -2,14 +2,20 @@ package xsbti;
 
 import java.io.File;
 
+/** Provides access to the jars and classes for a particular version of Scala.*/
 public interface ScalaProvider
 {
 	public Launcher launcher();
+	/** The version of Scala this instance provides.*/
 	public String version();
 
+	/** A ClassLoader that loads the classes from scala-library.jar and scala-compiler.jar.*/
 	public ClassLoader loader();
+	/** Returns the scala-library.jar and scala-compiler.jar for this version of Scala. */
 	public File[] jars();
 	public File libraryJar();
 	public File compilerJar();
+	/** Creates an application provider that will use 'loader()' as the parent ClassLoader for
+	* the application given by 'id'.*/
 	public AppProvider app(ApplicationID id);
 }
