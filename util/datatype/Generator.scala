38dbb1d23c4574f3966b6d810a3092ea044ed5d7
Mark Harrah <dmharrah@gmail.com>
2009-11-22 22:54:17 -0500

    Type member support, linearization instead of parents and add inherited members for structure

--- util/datatype/Generator.scala
+++ util/datatype/Generator.scala
@@ -3,11 +3,23 @@ package xsbt.api
 import java.io.File
 import xsbt.FileUtilities
 
+/** Creates immutable datatype classes in Java from the intermediate Definition representation.
+*
+* A ClassDef is written as a class with an optional parent class.  The class has a single constructor with
+* parameters for all declared and inherited members.  Declared members are listed first in the constructor.
+* Inherited members are passed to the super constructor.  The value of each member is held in a private
+* final field and.is accessed by a method of the same name.
+*
+* A toString method is generated for debugging.
+* The classes implement java.io.Serializable.
+*
+*.@param baseDirectory output directory for sources
+* @param pkgName package that classes will be defined in*/
 class Generator(pkgName: String, baseDirectory: File)
 {
 	def writeDefinitions(ds: Iterable[Definition]) =
 	{
-		val (nameSet, duplicates) =
+		val (_, duplicates) =
 			( (Set[String](), Set[String]()) /: ds.map(_.name)) {
 				case ((nameSet, duplicates), name) =>
 					if(nameSet.contains(name)) (nameSet, duplicates + name) else (nameSet + name, duplicates)
@@ -29,10 +41,11 @@ class Generator(pkgName: String, baseDirectory: File)
 	def write(c: ClassDef): Unit = writeSource(c.name, classContent(c))
 	def classContent(c: ClassDef): String =
 	{
+		val hasParent = c.parent.isDefined
 		val allMembers = c.allMembers.map(normalize)
 		val normalizedMembers = c.members.map(normalize)
 		val fields = normalizedMembers.map(m => "private final " + m.asJavaDeclaration + ";")
-		val accessors = normalizedMembers.map(m => "public final " + m.asJavaDeclaration + "() \n\t{\n\t\treturn " + m.name + ";\n\t}")
+		val accessors = normalizedMembers.map(m => "public final " + m.asJavaDeclaration + "()\n\t{\n\t\treturn " + m.name + ";\n\t}")
 		val parameters = allMembers.map(_.asJavaDeclaration)
 		val assignments = normalizedMembers.map(m => "this." + m.name + " = " + m.name + ";")
 		val superConstructor =
@@ -41,23 +54,24 @@ class Generator(pkgName: String, baseDirectory: File)
 			if(inherited.isEmpty) "" else  "super(" + inherited.map(_.name).mkString(", ") + ");\n\t\t"
 		}
 		val parametersString = if(allMembers.isEmpty) "\"\"" else allMembers.map(m => fieldToString(m.name, m.single)).mkString(" + \", \" + ")
-		val toStringMethod = "public String toString()\n\t{\n\t\t" +
-				"return \"" + c.name + "(\" + " + parametersString + "+ \")\";\n\t" +
-			"}\n"
+		val toStringMethod = method("public", "String", "toString", "", "\"" + c.name + "(\" + " + parametersString + "+ \")\"")
 
 		val constructor = "public " + c.name + "("  + parameters.mkString(", ") + ")\n\t" +
 		"{\n\t\t" +
 			superConstructor +
 			assignments.mkString("\n\t\t") + "\n\t" +
 		"}"
-		"\nimport java.util.Arrays;\n" +
-		"public class " + c.name + c.parent.map(" extends " + _.name + " ").getOrElse("") + "\n" +
+		"import java.util.Arrays;\n" +
+		"import java.util.List;\n" +
+		"public class " + c.name + c.parent.map(" extends " + _.name + " ").getOrElse(" implements java.io.Serializable") + "\n" +
 		"{\n\t" +
 			constructor + "\n\t" + 
 			(fields ++ accessors).mkString("\n\t") + "\n\t" +
-			toStringMethod + "\n" +
+			toStringMethod + "\n\t" +
 		"}"
 	}
+	def method(modifiers: String, returnType: String, name: String, parameters: String, content: String) =
+		modifiers + " " + returnType + " " + name + "(" + parameters + ")\n\t{\n\t\treturn " + content + ";\n\t}"
 	def fieldToString(name: String, single: Boolean) = "\"" + name + ": \" + " + fieldString(name + "()", single)
 	def fieldString(arg: String, single: Boolean) = if(single) arg else "Arrays.toString(" + arg + ")"
 	def normalize(m: MemberDef): MemberDef =
