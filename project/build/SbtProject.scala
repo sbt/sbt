8516a40ec2fc368f5ee280e9780295c67df7d3c5
Mark Harrah <dmharrah@gmail.com>
2010-01-10 22:02:01 -0500

    bump to test-interface 0.4

--- project/build/SbtProject.scala
+++ project/build/SbtProject.scala
@@ -58,7 +58,7 @@ class SbtProject(info: ProjectInfo) extends DefaultProject(info) with test.SbtSc
 	val jsch = "com.jcraft" % "jsch" % "0.1.31" intransitive()
 	val jetty = "org.mortbay.jetty" % "jetty" % "6.1.14" % "optional"
 
-	val testInterface = "org.scala-tools.testing" % "test-interface" % "0.3"
+	val testInterface = "org.scala-tools.testing" % "test-interface" % "0.4"
 
 	// xsbt components
 	val xsbti = "org.scala-tools.sbt" % "launcher-interface" % projectVersion.value.toString % "provided"
