f0b7fa0e1a663d381abcf5f43eeb5f178a21fefe
Mark Harrah <dmharrah@gmail.com>
2010-01-29 20:48:59 -0500

    Test for specs/sbt detecting specifications as classes/objects

---
+++ src/sbt-test/tests/specs-run/changes/ClassFailModuleSuccess.scala
@@ -0,0 +1,15 @@
+import org.specs._
+
+class A extends Specification
+{
+	"this" should {
+    "not work" in { 1 must_== 2 }
+  }
+}
+
+object A extends Specification
+{
+	"this" should {
+    "work" in { 1 must_== 1 }
+  }
+}
---
+++ src/sbt-test/tests/specs-run/project/build/TestProject.scala
@@ -0,0 +1,6 @@
+import sbt._
+
+class TestProject(info: ProjectInfo) extends DefaultProject(info)
+{
+	val specs = "org.scala-tools.testing" %% "specs" % "1.6.1"
+}
