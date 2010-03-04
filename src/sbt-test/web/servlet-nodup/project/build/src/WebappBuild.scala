186112ee571f3a49f76bf3e601926386f86885a9
Mark Harrah <dmharrah@gmail.com>
2010-03-04 16:34:27 -0500

    Fix duplicate classpath entries in jetty-run

---
+++ src/sbt-test/web/servlet-nodup/project/build/src/WebappBuild.scala
@@ -0,0 +1,25 @@
+import sbt._
+
+class WebappBuild(info: ProjectInfo) extends DefaultWebProject(info) {
+
+	override def libraryDependencies = if("jetty7".asFile.exists) jetty7Dependencies else jetty6Dependencies
+	def jetty6Dependencies =
+		Set("org.mortbay.jetty"          % "servlet-api-2.5"         % "6.1.14"  % "provided->default",
+		"org.mortbay.jetty"          % "jetty"                   % "6.1.14"  % "test->default")
+	def jetty7Dependencies =
+		Set("javax.servlet" % "servlet-api" % "2.5" % "provided",
+		"org.eclipse.jetty" % "jetty-server" % "7.0.1.v20091125" % "test",
+		"org.eclipse.jetty" % "jetty-webapp" % "7.0.1.v20091125" % "test")
+
+	def indexURL = new java.net.URL("http://localhost:8080")
+	def indexFile = new java.io.File("index.html")
+	import Process._
+	lazy val getPage = execTask { indexURL #> indexFile }
+	lazy val checkPage = task { args => task { checkHelloWorld(args.mkString(" ")) } dependsOn getPage }
+	
+	private def checkHelloWorld(checkString: String) =
+	{
+		val value = xsbt.FileUtilities.read(indexFile)
+		if(value.contains(checkString)) None else Some("index.html did not contain '" + checkString + "' :\n" +value)
+	}
+}
