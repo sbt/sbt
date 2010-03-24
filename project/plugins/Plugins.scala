d554c79e105b063239ea128034c2857385545be5
Mark Harrah <dmharrah@gmail.com>
2010-03-23 20:43:17 -0400

    update posterous-sbt version

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -6,6 +6,6 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info)
 {
 	val scripted = "org.scala-tools.sbt" % "scripted" % "0.7.0"
 	val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
-	val posterous = "net.databinder" % "posterous-sbt" % "0.1.3"
+	val posterous = "net.databinder" % "posterous-sbt" % "0.1.4"
 	val technically = Resolver.url("technically.us", new URL("http://databinder.net/repo/"))(Resolver.ivyStylePatterns)
 }
