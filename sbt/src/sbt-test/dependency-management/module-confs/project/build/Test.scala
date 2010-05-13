f66a3b4f8713f5643e0986cdf382c8d4bcf57f50
Mark Harrah <dmharrah@gmail.com>
2010-05-13 19:02:56 -0400

    use newer nightly for module-confs test

--- sbt/src/sbt-test/dependency-management/module-confs/project/build/Test.scala
+++ sbt/src/sbt-test/dependency-management/module-confs/project/build/Test.scala
@@ -8,7 +8,7 @@ class Test(info: ProjectInfo) extends DefaultProject(info)
 	def scalaSnapshots = Resolver.url("Scala Tools Snapshots") artifacts(snapshotPattern) ivys(snapshotPattern) mavenStyle()
 	val scOnly = ModuleConfiguration("org.scala-lang", "*", "2.8.0-.*", scalaSnapshots)
 
-	val uniqueScala = "org.scala-lang" % "scala-compiler" % "2.8.0-20100115.022156-304"
+	val uniqueScala = "org.scala-lang" % "scala-compiler" % "2.8.0-20100511.013550-380"
 	val otherDep = "org.scala-tools.sxr" % "sxr_2.7.5" % "0.2.3"
 	
 	override def checkExplicitScalaDependencies = false
