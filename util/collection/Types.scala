12336b79f67cc7c7a4588ce493c86de107bd9513
Mark Harrah <dmharrah@gmail.com>
2010-08-30 09:10:25 -0400

    minor updates for p2

--- util/collection/Types.scala
+++ util/collection/Types.scala
@@ -3,13 +3,16 @@
  */
 package sbt
 
-object Types extends TypeFunctions
+object Types extends Types
+{
+	implicit def hconsToK[M[_], H, T <: HList](h: M[H] :+: T)(implicit mt: T => KList[M, T]): KList[M, H :+: T] =
+		KCons[H, T, M](h.head, mt(h.tail) )
+	implicit def hnilToK(hnil: HNil): KNil = KNil
+}
+
+trait Types extends TypeFunctions
 {
 	val :^: = KCons
 	val :+: = HCons
 	type :+:[H, T <: HList] = HCons[H,T]
-
-	implicit def hconsToK[M[_], H, T <: HList](h: M[H] :+: T)(implicit mt: T => KList[M, T]): KList[M, H :+: T] =
-		KCons[H, T, M](h.head, mt(h.tail) )
-	implicit def hnilToK(hnil: HNil): KNil = KNil
 }
