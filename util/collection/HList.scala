b033bc889d298ae7e1d084c1ab67ed15d5733a41
Mark Harrah <dmharrah@gmail.com>
2010-09-12 22:27:11 -0400

    toString for HList and KList

--- util/collection/HList.scala
+++ util/collection/HList.scala
@@ -13,12 +13,16 @@ sealed trait HNil extends HList
 {
 	type Wrap[M[_]] = HNil
 	def :+: [G](g: G): G :+: HNil = HCons(g, this)
+
+	override def toString = "HNil"
 }
 object HNil extends HNil
 final case class HCons[H, T <: HList](head : H, tail : T) extends HList
 {
 	type Wrap[M[_]] = M[H] :+: T#Wrap[M]
 	def :+: [G](g: G): G :+: H :+: T = HCons(g, this)
+
+	override def toString = head + " :+: " + tail.toString
 }
 
 object HList
