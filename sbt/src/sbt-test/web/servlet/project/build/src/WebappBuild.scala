c0e47dbf3eb88ae05a256105ac3282e3cf509612
Mark Harrah <dmharrah@gmail.com>
2010-10-09 14:07:13 -0400

    update servlet test to test Jetty 7.1.x

--- sbt/src/sbt-test/web/servlet/project/build/src/WebappBuild.scala
+++ sbt/src/sbt-test/web/servlet/project/build/src/WebappBuild.scala
@@ -2,13 +2,23 @@ import sbt._
 
 class WebappBuild(info: ProjectInfo) extends DefaultWebProject(info) {
 
-	override def libraryDependencies = if("jetty7".asFile.exists) jetty7Dependencies else jetty6Dependencies
+	override def libraryDependencies =
+		if("jetty7.0".asFile.exists)
+			jetty70Dependencies
+		else if("jetty7.1".asFile.exists)
+			jetty71Dependencies
+		else
+			jetty6Dependencies
+
 	def jetty6Dependencies =
 		Set("org.mortbay.jetty"          % "servlet-api-2.5"         % "6.1.14"  % "provided->default",
 		"org.mortbay.jetty"          % "jetty"                   % "6.1.14"  % "test->default")
-	def jetty7Dependencies =
+	def jetty70Dependencies =
 		Set("javax.servlet" % "servlet-api" % "2.5" % "provided",
 		"org.eclipse.jetty" % "jetty-webapp" % "7.0.1.v20091125" % "test")
+	def jetty71Dependencies =
+		Set("javax.servlet" % "servlet-api" % "2.5" % "provided",
+		"org.eclipse.jetty" % "jetty-webapp" % "7.1.6.v20100715" % "test")
 
 	def indexURL = new java.net.URL("http://localhost:" + jettyPort)
 	def indexFile = new java.io.File("index.html")
