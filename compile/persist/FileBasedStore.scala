e09a638ce34b14e18ac75f316fef66efdcfe2eba
Mark Harrah <dmharrah@gmail.com>
2010-11-24 14:08:20 -0500

    cleanup, small fixes

--- compile/persist/FileBasedStore.scala
+++ compile/persist/FileBasedStore.scala
@@ -4,7 +4,7 @@
 package sbt
 package inc
 
-	import java.io.{File, InputStream, IOException}
+	import java.io.{File, IOException}
 	import sbinary._
 	import Operations.{read, write}
 	import DefaultProtocol._
@@ -14,19 +14,13 @@ object FileBasedStore
 {
 	def apply(file: File)(implicit analysisF: Format[Analysis], setupF: Format[CompileSetup]): AnalysisStore = new AnalysisStore {
 		def set(analysis: Analysis, setup: CompileSetup): Unit =
-			Using.fileOutputStream()(file) { fout =>
-			Using.gzipOutputStream(fout) { outg =>
-			Using.bufferedOutputStream(outg) { out =>
+			IO.gzipFileOut(file) { out =>
 				write[(Analysis, CompileSetup)](out, (analysis, setup) )
-			}}}
+			}
 
 		def get(): Option[(Analysis, CompileSetup)] =
 			try { Some(getUncaught()) } catch { case io: IOException => None }
 		def getUncaught(): (Analysis, CompileSetup) =
-			Using.fileInputStream(file) { fin =>
-			Using.gzipInputStream(fin) { ing =>
-			Using.bufferedInputStream(ing) { in =>
-				read[(Analysis, CompileSetup)]( in )
-			}}}
+			IO.gzipFileIn(file)( in => read[(Analysis, CompileSetup)](in) )
 	}
 }
