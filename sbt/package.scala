202dac644f07404ff60be73279ab4d784dbf4969
Mark Harrah <dmharrah@gmail.com>
2011-02-14 18:54:09 -0500

    added top-level URI alias, uri/file aliases for construction

--- sbt/package.scala
+++ sbt/package.scala
@@ -4,6 +4,9 @@
 package object sbt extends sbt.std.TaskExtra with sbt.Types with sbt.ProcessExtra with sbt.impl.DependencyBuilders with sbt.PathExtra with sbt.ProjectConstructors
 {
 	type File = java.io.File
+	type URI = java.net.URI
 	implicit def maybeToOption[S](m: xsbti.Maybe[S]): Option[S] =
 		if(m.isDefined) Some(m.get) else None
+	def uri(s: String): URI = new URI(s)
+	def file(s: String): File = new File(s)
 }
