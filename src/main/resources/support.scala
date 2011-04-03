84d1909820b18ffc97da0cc899d41bb55db922a6
Paul Phillips <paulp@improving.org>
2011-04-02 22:51:46 -0700

    Updates, and increasingly osx specific bash hacks.

--- src/main/resources/support.scala
+++ src/main/resources/support.scala
@@ -16,29 +16,32 @@ trait ProjectSupport extends ModuleIdDynamifactory {
   val scalaToolsSnapshots = "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots/"
   val jboss               = "JBoss Repo" at "http://repository.jboss.org/maven2"
 
-  protected implicit lazy val implicitTransform: ArtifactTransform =
-    ArtifactTransform(inScope("test"), withSources)
+  private val testConfig: ArtifactConfig = ArtifactConfig(
+    ArtifactRevision(_ => dynamicRevision), ArtifactTransform(inScope("test"), withSources)
+  )
 
-  /*** Libraries ***/  
-  val specs: ModuleID           = "org.scala-tools.testing" %% "specs"
-  val scalacheck: ModuleID      = "org.scala-tools.testing" %% "scalacheck"
+  /*** Libraries ***/
+  val specs: ModuleID           = testConfig("org.scala-tools.testing" %% "specs")
+  val scalacheck: ModuleID      = testConfig("org.scala-tools.testing" %% "scalacheck")
+
+  private implicit lazy val implicitTransform: ArtifactTransform = ArtifactTransform()
 
   // val ant: ModuleID             = "org.apache.ant" % "ant"
-  // val asmAll: ModuleID          = "asm" % "asm-all" withSources()
-  // val commonsVFS: ModuleID      = "org.apache.commons" % "commons-vfs-project"
+  // val jdt: ModuleID             = "org.eclipse.jdt" % "core" notTransitive()
+  // val scalaImproving: ModuleID  = "org.improving" %% "scala-improving"
+  // val scalaSTM: ModuleID        = "org.scala-tools" %% "scala-stm" 
+  // val scalariform: ModuleID     = "org.scalariform" %% "scalariform"
+  // 
+  // val asmAll: ModuleID          = "asm" % "asm-all" % "3.3.1" withSources()
   // val easymock: ModuleID        = "org.easymock" % "easymock"
   // val guava: ModuleID           = "com.google.guava" % "guava"
   // val ivy: ModuleID             = "org.apache.ivy" % "ivy"
-  // val jdt: ModuleID             = "org.eclipse.jdt" % "core" notTransitive()
   // val jetty: ModuleID           = "org.mortbay.jetty" % "jetty"
   // val jmock: ModuleID           = "org.jmock" % "jmock"
   // val jodaTime: ModuleID        = "joda-time" % "joda-time"
   // val liftJson: ModuleID        = "net.liftweb" %% "lift-json" 
   // val maven: ModuleID           = "org.apache.maven" % "maven-ant-tasks"
   // val scalaARM: ModuleID        = "com.github.jsuereth.scala-arm" %% "scala-arm" withSources()
-  // val scalaImproving: ModuleID  = "org.improving" %% "scala-improving"
-  // val scalaSTM: ModuleID        = "org.scala-tools" %% "scala-stm" 
-  // val scalariform: ModuleID     = "org.scalariform" %% "scalariform"
   // val scalazCore: ModuleID      = "org.scalaz" %% "scalaz-core" withSources()
   // val scalazHttp: ModuleID      = "org.scalaz" %% "scalaz-http" withSources()
   // val slf4s: ModuleID           = "com.weiglewilczek.slf4s" %% "slf4s" withSources()
@@ -76,14 +79,12 @@ trait Dynamifactory {
   case class ArtifactTransform(fns: DepFn*) {
     def apply(x: DepOut): DepOut = if (fns.isEmpty) x else fns.reduceLeft(_ andThen _)(x)
   }
-  case class ArtifactConfig(rev: ArtifactRevision, transform: ArtifactTransform) { }
+  case class ArtifactConfig(rev: ArtifactRevision, transform: ArtifactTransform) {
+    def apply(in: DepId): DepOut = transform(finishDependency(in, rev.revisionFn(in)))
+  }
   
   protected implicit def autoassembleConfig(implicit rev: ArtifactRevision, transform: ArtifactTransform): ArtifactConfig =
     ArtifactConfig(rev, transform)
 
-  protected implicit def autoconfigureDependencies(in: DepId)(implicit config: ArtifactConfig): DepOut = {
-    val ArtifactConfig(ArtifactRevision(revisionFn), transform) = config
-    
-    transform(finishDependency(in, revisionFn(in)))
-  }
+  protected implicit def autoconfigureDependencies(in: DepId)(implicit config: ArtifactConfig): DepOut = config(in)
 }
