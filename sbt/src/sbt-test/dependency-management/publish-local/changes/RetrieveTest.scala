6efe299e10458135690cb9b1062efda1d5f0a283
Mark Harrah <dmharrah@gmail.com>
2011-05-26 08:21:26 -0400

    migrate another test

---
+++ sbt/src/sbt-test/dependency-management/publish-local/changes/RetrieveTest.scala
@@ -0,0 +1,24 @@
+	import sbt._
+	import Keys._
+
+object MultiPublishTest extends Build
+{
+	override lazy val settings = super.settings ++ Seq(
+		organization := "A",
+		version := "1.0",
+		ivyPaths <<= baseDirectory( dir => new IvyPaths(dir, Some(dir / "ivy" / "cache")) ),
+		externalResolvers <<= baseDirectory { base => Resolver.file("local", base / "ivy" / "local" asFile)(Resolver.ivyStylePatterns) :: Nil }
+	}
+
+	lazy val projects = Seq(root)
+	lazy val root = Project("root", file(".")) settings(
+		name := "Retrieve Test",
+		libraryDependencies := if("mavenStyle".asFile.exists) mavenStyleDependencies else ivyStyleDependencies
+	)
+
+	def ivyStyleDependencies = parentDep("A") :: subDep("A") :: subDep("B") ::parentDep("D") :: Nil
+	def mavenStyleDependencies = parentDep("B") :: parentDep("C") :: subDep("C") :: subDep("D") :: Nil
+
+	def parentDep(org: String) =  org %% "publish-test" % "1.0"
+	def subDep(org: String) = org %% "sub-project" % "1.0"
+}
---
+++ sbt/src/sbt-test/dependency-management/publish-local/project/MultiPublishTest.scala
@@ -0,0 +1,21 @@
+	import sbt._
+	import Keys._
+
+object MultiPublishTest extends Build
+{
+	override lazy val settings = super.settings ++ Seq(
+		organization := "A",
+		version := "1.0",
+		ivyPaths <<= baseDirectory( dir => new IvyPaths(dir, Some(dir / "ivy" / "cache")) ),
+		externalResolvers <<= baseDirectory { base => Resolver.file("local", base / "ivy" / "local" asFile)(Resolver.ivyStylePatterns) :: Nil }
+	)
+
+	lazy val projects = Seq(root)
+	lazy val root = Project("root", file(".")) settings( mavenStyle, name = "Publish Test" )
+
+	lazy val sub = Project("sub", file("sub")) settings( mavenStyle, name = "Sub Project" )
+
+	lazy val mavenStyle = publishMavenStyle <<= baseDirectory { base => (base / "mavenStyle") exists }
+
+	override def deliverProjectDependencies = if(managedStyle == sub.managedStyle) super.deliverProjectDependencies else Nil
+}
