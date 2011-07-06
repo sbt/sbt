60dcd4404fe3e39bd2f465ccea05ffecaa68767c
Mark Harrah <dmharrah@gmail.com>
2011-07-06 07:30:47 -0400

    pass empty classpath instead of no classpath.  fixes #85. ref #82

--- compile/CompilerArguments.scala
+++ compile/CompilerArguments.scala
@@ -18,7 +18,7 @@ final class CompilerArguments(scalaInstance: ScalaInstance, cp: ClasspathOptions
 	{
 		checkScalaHomeUnset()
 		val cpWithCompiler = finishClasspath(classpath)
-		val classpathOption = if(cpWithCompiler.isEmpty) Nil else Seq("-cp", absString(cpWithCompiler) )
+		val classpathOption = Seq("-cp", if(cpWithCompiler.isEmpty) "" else absString(cpWithCompiler) )
 		val outputOption = Seq("-d", outputDirectory.getAbsolutePath)
 		options ++ outputOption ++ bootClasspathOption ++ classpathOption ++ abs(sources)
 	}
