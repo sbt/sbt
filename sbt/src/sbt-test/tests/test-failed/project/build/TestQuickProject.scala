1c53457adf3e841ccfcb82d3575f482d38e157bf
Mark Harrah <dmharrah@gmail.com>
2011-07-15 15:48:37 -0400

    drop test-failed test until reintroduced

--- sbt/src/sbt-test/tests/test-failed/project/build/TestQuickProject.scala
+++
@@ -1,6 +0,0 @@
-import sbt._
-
-class TestQuickProject(info: ProjectInfo) extends DefaultProject(info) {
-	val snapshots = ScalaToolsSnapshots
-	val specs = "org.scala-tools.testing" %% "specs" % "1.6.1"
-}
