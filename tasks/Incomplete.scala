a7e95ada169539127d87ae5f09b89a1119cb6246
Mark Harrah <dmharrah@gmail.com>
2011-11-19 23:56:30 -0500

    API documentation

--- tasks/Incomplete.scala
+++ tasks/Incomplete.scala
@@ -4,6 +4,14 @@
 package sbt
 
 import Incomplete.{Error, Value => IValue}
+
+/** Describes why a task did not complete.
+*
+* @param node the task that did not complete that is described by this Incomplete instance
+* @param tpe whether the task was incomplete because of an error or because it was skipped.  Only Error is actually used and Skipped may be removed in the future.
+* @param message an optional error message describing this incompletion
+* @param causes a list of incompletions that prevented `node` from completing
+* @param directCause the exception that caused `node` to not complete */
 final case class Incomplete(node: Option[AnyRef], tpe: IValue = Error, message: Option[String] = None, causes: Seq[Incomplete] = Nil, directCause: Option[Throwable] = None)
 	extends Exception(message.orNull, directCause.orNull) {
 		override def toString = "Incomplete(node=" + node + ", tpe=" + tpe + ", msg=" + message + ", causes=" + causes + ", directCause=" + directCause +")"
