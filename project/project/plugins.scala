d8908cc39315a76adfbf9e1c37218ed4cd05323c
Josh Suereth <joshua.suereth@gmail.com>
2012-01-16 15:06:37 -0500

    Build no uses released packaging plugin.

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -4,7 +4,9 @@ import Keys._
 object PluginBuild extends Build {
   override def projects = Seq(root)
   
-  val root = Project("root", file(".")) dependsOn(packager) settings(libraryDependencies += "net.databinder" %% "dispatch-http" % "0.8.6")
-  
-  lazy val packager = RootProject(file("/home/jsuereth/projects/typesafe/sbt-native-packager"))
+  val root = Project("root", file(".")) settings(
+    resolvers += Resolver.url("scalasbt", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
+    addSbtPlugin("com.typesafe" % "sbt-native-packager" % "0.1.0"),
+    libraryDependencies += "net.databinder" %% "dispatch-http" % "0.8.6"
+  )
 }
