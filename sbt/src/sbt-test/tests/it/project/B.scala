048d5157c77461d11a78975f5cbd89d208624f20
Mark Harrah <dmharrah@gmail.com>
2012-01-22 22:06:53 -0500

    fix integration test group 'tests'

--- sbt/src/sbt-test/tests/it/project/B.scala
+++ sbt/src/sbt-test/tests/it/project/B.scala
@@ -6,8 +6,11 @@ object B extends Build
 	lazy val root =
 		Project("root", file("."))
 			.configs( IntegrationTest )
-			.settings( libraryDependencies += specs )
 			.settings( Defaults.itSettings : _*)
+			.settings(
+				libraryDependencies += specs,
+				resolvers += ScalaToolsReleases
+			)
 
-	lazy val specs = "org.scala-tools.testing" %% "specs" % "1.6.7.2" % "it,test" intransitive()
+	lazy val specs = "org.specs2" %% "specs2" % "1.7.1" % "it,test"
 }
