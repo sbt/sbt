7b194e3dee68325ecd71621a519266db80ec62a0
Mark Harrah <dmharrah@gmail.com>
2012-02-25 12:01:07 -0500

    cleanup SourcePosition hierarchy

--- util/collection/Positions.scala
+++ util/collection/Positions.scala
@@ -2,19 +2,19 @@ package sbt
 
 sealed trait SourcePosition
 
-sealed trait FilePosition {
+sealed trait FilePosition extends SourcePosition {
 	def path: String
 	def startLine: Int
 }
 
 case object NoPosition extends SourcePosition
 
-final case class LinePosition(path: String, startLine: Int) extends SourcePosition with FilePosition
+final case class LinePosition(path: String, startLine: Int) extends FilePosition
 
 final case class LineRange(start: Int, end: Int) {
 	def shift(n: Int) = new LineRange(start + n, end + n)
 }
 
-final case class RangePosition(path: String, range: LineRange) extends SourcePosition with FilePosition {
+final case class RangePosition(path: String, range: LineRange) extends FilePosition {
 	def startLine = range.start
 }
