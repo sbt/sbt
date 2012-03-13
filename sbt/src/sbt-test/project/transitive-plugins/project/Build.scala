15b2d59c01b7a7863652a0f70c892bbf986a7c47
Mark Harrah <dmharrah@gmail.com>
2012-03-13 08:01:58 -0400

    fix project/transitive-plugins test

--- sbt/src/sbt-test/project/transitive-plugins/project/Build.scala
+++ sbt/src/sbt-test/project/transitive-plugins/project/Build.scala
@@ -7,7 +7,9 @@ object Build extends Build
 		sbtBinaryVersion <<= sbtVersion
 	)
 
-	lazy val root = Project("root", file(".")) aggregate(a,b,c)
+	lazy val root = Project("root", file(".")) aggregate(a,b,c) settings(
+		ivyPaths in ThisBuild <<= (baseDirectory, target)( (dir, t) => new IvyPaths(dir, Some(t / "ivy-cache")))
+	)
 	lazy val a = Project("a", file("a"))
 	lazy val b = Project("b", file("b"))
 	lazy val c = Project("c", file("c"))
