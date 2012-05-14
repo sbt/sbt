c91159b3638754e21b96fbc1caaa39460632b670
Josh Suereth <joshua.suereth@gmail.com>
2012-05-14 12:35:27 -0400

    fixes debian architecture bug.

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -6,7 +6,7 @@ object PluginBuild extends Build {
   
   val root = Project("root", file(".")) settings(
     resolvers += Resolver.url("scalasbt", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
-    addSbtPlugin("com.typesafe" % "sbt-native-packager" % "0.4.2"),
+    addSbtPlugin("com.typesafe" % "sbt-native-packager" % "0.4.3"),
     libraryDependencies += "net.databinder" %% "dispatch-http" % "0.8.6"
   )
 }
