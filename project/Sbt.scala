20a3a8777282cd08997f333420765b3ff88cfc5a
Mark Harrah <dmharrah@gmail.com>
2012-09-04 11:34:46 -0400

    version bumps

--- project/Sbt.scala
+++ project/Sbt.scala
@@ -14,7 +14,7 @@ object Sbt extends Build
 	override lazy val settings = super.settings ++ buildSettings ++ Status.settings
 	def buildSettings = Seq(
 		organization := "org.scala-sbt",
-		version := "0.12.0",
+		version := "0.12.1-SNAPSHOT",
 		publishArtifact in packageDoc := false,
 		scalaVersion := "2.9.2",
 		publishMavenStyle := false,
