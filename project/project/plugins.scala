872d88ea237301a32f98dac0db1acd75c1b333bb
Josh Suereth <joshua.suereth@gmail.com>
2012-09-07 12:58:02 -0400

    Updated build so you can specify SBT version on the command line.

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -7,6 +7,6 @@ object PluginBuild extends Build {
   val root = Project("root", file(".")) settings(
     resolvers += Resolver.url("scalasbt", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
     addSbtPlugin("com.typesafe" % "sbt-native-packager" % "0.4.3"),
-    libraryDependencies += "net.databinder" %% "dispatch-http" % "0.8.6"
+    libraryDependencies += "net.databinder" % "dispatch-http_2.9.1" % "0.8.6"
   )
 }
