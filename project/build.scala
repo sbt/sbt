5407a218034bdc2e60e4702ced0431bae9bbe8c9
Josh Suereth <joshua.suereth@gmail.com>
2012-09-07 13:52:05 -0400

    Use environment variable thanks to lame sbt jenkins plugin

--- project/build.scala
+++ project/build.scala
@@ -5,7 +5,7 @@ object SbtExtras extends Build {
   // This build creates a SBT plugin with handy features *and* bundles the SBT script for distribution.
   val root = Project("sbt-packaging", file(".")) settings(Packaging.settings:_*) settings(
     sbtVersion <<= sbtVersion apply { v =>
-      sys.props.getOrElse("sbt.build.version", v)
+      sys.props.getOrElse("sbt.build.version", sys.env.getOrElse("sbt.build.version", v))
     }
   )
 
