42fd2aa0603c573957180c1a146a3ca9bcbaa154
nau <anemish@gmail.com>
2012-10-22 14:02:05 +0300

    Update compile/inc/Incremental.scala
    Fix for http://stackoverflow.com/questions/12972183/sbt-always-does-full-rebuild-because-of-modified-binary-dependency-rt-jar
    def externalBinaryModified uses java.io.File.equals() to check if files are the same. It's better to use File.getCanonicalPath in this case.

--- compile/inc/Incremental.scala
+++ compile/inc/Incremental.scala
@@ -214,7 +214,7 @@ object Incremental
 					e <- entry(name)
 				} yield {
 					val resolved = Locate.resolve(e, name)
-					(resolved != dependsOn) || !equivS.equiv(previous.binary(dependsOn), current.binary(resolved))
+					(resolved.getCanonicalPath != dependsOn.getCanonicalPath) || !equivS.equiv(previous.binary(dependsOn), current.binary(resolved))
 				}
 			)
 
