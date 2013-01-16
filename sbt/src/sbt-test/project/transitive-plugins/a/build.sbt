9bb49260c5228099b6fe43845725c2e9929ce609
Mark Harrah <dmharrah@gmail.com>
2013-01-16 10:26:33 -0500

    update project/transitive-plugins test to use a common cache

--- sbt/src/sbt-test/project/transitive-plugins/a/build.sbt
+++ sbt/src/sbt-test/project/transitive-plugins/a/build.sbt
@@ -1,9 +1,9 @@
-publishTo <<= baseDirectory(x =>
-	Some(Resolver.file("test-publish", x / "../repo/"))
+publishTo <<= (baseDirectory in ThisBuild)(x =>
+	Some(Resolver.file("test-publish", x / "repo/"))
 )
 
-resolvers <+= baseDirectory(x =>
-	"test" at (x / "../repo/").asURL.toString
+resolvers <+= (baseDirectory in ThisBuild)(x =>
+	"test" at (x / "repo/").asURL.toString
 )
 
 name := "demo1"
--- sbt/src/sbt-test/project/transitive-plugins/project/Build.scala
+++ sbt/src/sbt-test/project/transitive-plugins/project/Build.scala
@@ -3,19 +3,16 @@ import Keys._
 
 object Build extends Build
 {
-	override def settings = super.settings ++ Seq(
-		sbtBinaryVersion <<= sbtVersion
-	)
-
 	lazy val root = Project("root", file(".")) 
 	lazy val a = project("a")
 	lazy val b = project("b")
 	lazy val c = project("c")
 	def project(s: String) = Project(s, file(s)) settings(
-		ivyPaths <<= (baseDirectory, target in root)( (dir, t) => new IvyPaths(dir, Some(t / "ivy-cache"))),
-		resolvers <+= appConfiguration { app =>
-			val ivyHome = Classpaths.bootIvyHome(app) getOrElse (file(System.getProperty("user.home")) / ".ivy2")
+		ivyPaths <<= (baseDirectory in root, target in root)( (dir, t) => new IvyPaths(dir, Some(t / "ivy-cache"))),
+		resolvers <+= appConfiguration { app => // need this to resolve sbt
+			val ivyHome = Classpaths.bootIvyHome(app) getOrElse error("Launcher did not provide the Ivy home directory.")
 			Resolver.file("real-local",  ivyHome / "local")(Resolver.ivyStylePatterns)
-		}
+		},
+		resolvers += Resolver.typesafeIvyRepo("releases") // not sure why this isn't included by default
 	)
 }
