b3cbdf2bf24ea0ab569e7608b04bdb1a8ee95c9d
Mark Harrah <dmharrah@gmail.com>
2013-03-05 17:21:53 -0500

    fix project/flatten scripted test

--- sbt/src/sbt-test/project/flatten/project/Flat.scala
+++ sbt/src/sbt-test/project/flatten/project/Flat.scala
@@ -11,14 +11,14 @@ object Flat extends Build
 	def baseSettings = Seq(
 		scalaVersion := "2.8.1",
 		libraryDependencies += "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test",
-		sourceFilter := "*.java" | "*.scala"
+		includeFilter in unmanagedSources := "*.java" | "*.scala"
 	)
 
 	def forConfig(conf: Configuration, name: String) = Project.inConfig(conf)( unpackageSettings(name) )
 
 	def unpackageSettings(name: String) = Seq(
 		unmanagedSourceDirectories := (baseDirectory.value / name) :: Nil,
-		defaultExcludes in unmanagedResources := sourceFilter.value,
+		excludeFilter in unmanagedResources := (includeFilter in unmanagedSources).value,
 		unmanagedResourceDirectories := unmanagedSourceDirectories.value,
 		unpackage := IO.unzip(artifactPath in packageSrc value, baseDirectory.value / name)
 	)
