2810668d0104c3b198701a9bfa96aa08ff4c0a66
Mark Harrah <dmharrah@gmail.com>
2013-03-05 12:56:53 -0500

    drop string interpolation used in VersionParts test

--- launch/src/test/scala/VersionParts.scala
+++ launch/src/test/scala/VersionParts.scala
@@ -7,10 +7,10 @@ object VersionParts extends Properties("VersionParts")
 {
 	property("Valid version, no qualifier") = Prop.forAll { (x0: Int, y0: Int, z0: Int) =>
 		val (x,y,z) = (norm(x0), norm(y0), norm(z0))
-		val str = s"$x.$y.$z"
+		val str = "%s.%s.%s".format(x,y,z)
 		val expected =
-			s"$x.$y.$z" ::
-			s"$x.$y" ::
+			"%s.%s.%s".format(x,y,z) ::
+			"%s.%s".format(x,y) ::
 			"" ::
 			Nil
 		check(str, expected)
@@ -18,11 +18,11 @@ object VersionParts extends Properties("VersionParts")
 
 	property("Valid version with qualifier") = Prop.forAll { (x0: Int, y0: Int, z0: Int, q0: String) =>
 		val (x,y,z,q) = (norm(x0), norm(y0), norm(z0), normS(q0))
-		val str = s"$x.$y.$z-$q"
+		val str = "%s.%s.%s-%s".format(x,y,z,q)
 		val expected =
-			s"$x.$y.$z-$q" ::
-			s"$x.$y.$z" ::
-			s"$x.$y" ::
+			str ::
+			"%s.%s.%s".format(x,y,z) ::
+			"%s.%s".format(x,y) ::
 			"" ::
 			Nil
 		check(str, expected)
@@ -32,9 +32,9 @@ object VersionParts extends Properties("VersionParts")
 		val (x,y,z,q) = (norm(x0), norm(y0), norm(z0), normS(q0))
 		val strings =
 			x.toString ::
-			s"$x.$y" ::
-			s"$x.$y-$q" ::
-			s"$x.$y.$z.$q" ::
+			"%s.%s".format(x,y) ::
+			"%s.%s-%s".format(x,y,q) ::
+			"%s.%s.%s.%s".format(x,y,z,q) ::
 			Nil
 		all(strings.map(str => check(str, Configuration.noMatchParts)) : _*)
 	}
@@ -43,9 +43,9 @@ object VersionParts extends Properties("VersionParts")
 	{
 		def printParts(s: List[String]): String = s.map("'" + _ + "'").mkString("(", ", ", ")")
 		val actual = Configuration.versionParts(versionString)
-		s"Version string '$versionString'" |:
-		s"Expected '${printParts(expectedParts)}'" |:
-		s"Actual'${printParts(actual)}'" |:
+		"Version string '%s'".format(versionString) |:
+		"Expected '%s'".format(printParts(expectedParts)) |:
+		"Actual'%s'".format(printParts(actual)) |:
 		(actual == expectedParts)
 	}
 
