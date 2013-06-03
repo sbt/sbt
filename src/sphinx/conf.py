26876571d355eb379e9be236da59c78b3175bda3
Mark Harrah <dmharrah@gmail.com>
2013-06-03 08:51:53 -0400

    Docs: release in docs should be -Beta1

--- src/sphinx/conf.py
+++ src/sphinx/conf.py
@@ -9,7 +9,7 @@ extensions = ['sphinxcontrib.issuetracker', 'sphinx.ext.extlinks', 'howto']
 
 project = 'sbt'
 version = '0.13'
-release = '0.13.0'
+release = '0.13.0-Beta1'
 scalaVersion = "2.10"
 scalaRelease = "2.10.2"
 
