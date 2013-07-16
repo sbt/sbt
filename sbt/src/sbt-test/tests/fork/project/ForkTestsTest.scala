94e6bb406e63e299b716d61d4aba03b9389fcf84
Mark Harrah <dmharrah@gmail.com>
2013-07-16 13:55:10 -0400

    fix tests/fork.  It relied on being able to run a test multiple times in the same forked group.

--- sbt/src/sbt-test/tests/fork/project/ForkTestsTest.scala
+++ sbt/src/sbt-test/tests/fork/project/ForkTestsTest.scala
@@ -4,8 +4,8 @@ import Tests._
 import Defaults._
 
 object ForkTestsTest extends Build {
-	val totalFiles = 9
 	val groupSize = 3
+	val groups = 3
 
 	val check = TaskKey[Unit]("check", "Check all files were created and remove them.")
 
@@ -15,21 +15,18 @@ object ForkTestsTest extends Build {
 	lazy val root = Project("root", file("."), settings = defaultSettings ++  Seq(
 		scalaVersion := "2.9.2",
 		testGrouping in Test <<= definedTests in Test map { tests =>
-			assert(tests.size == 1)
-			val groups = Stream continually tests(0) take totalFiles grouped groupSize
-			for ((ts, idx) <- groups.toSeq.zipWithIndex)	yield {
-				new Group(groupId(idx), ts, SubProcess(Seq("-Dgroup.prefix=" + groupPrefix(idx), "-Dgroup.size=" + ts.size)))
-			}
+			assert(tests.size == 3)
+			for (idx <- 0 until groups) yield
+				new Group(groupId(idx), tests, SubProcess(Seq("-Dgroup.prefix=" + groupPrefix(idx))))
 		},
 		check := {
-			for (i <- 0 until totalFiles/groupSize)
-				for (j <- 1 to groupSize) {
-					val f = file(groupPrefix(i) + j)
-					if (!f.exists)
-						error("File " + f.getName + " was not created.")
-					else
-						f.delete()
-				}
+			val files =
+				for(i <- 0 until groups; j <- 1 to groupSize) yield
+					file(groupPrefix(i) + j)
+			val (exist, absent) = files.partition(_.exists)
+			exist.foreach(_.delete())
+			if(absent.nonEmpty)
+				error("Files were not created:\n\t" + absent.mkString("\n\t"))
 		},
 		concurrentRestrictions := Tags.limit(Tags.ForkedTestGroup, 2) :: Nil,
 		libraryDependencies += "org.scalatest" %% "scalatest" % "1.8" % "test"
