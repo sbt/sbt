3c1819ac4b839162f64653a31a3afe0c27b020f6
Mark Harrah <dmharrah@gmail.com>
2013-10-16 17:17:54 -0400

    remove dead APIFormats code

--- compile/persist/src/main/scala/xsbt/api/SourceFormat.scala
+++ compile/persist/src/main/scala/xsbt/api/SourceFormat.scala
@@ -37,10 +37,3 @@ final class OutputWrapperStream(out: Output) extends java.io.OutputStream
 	override def write(bs: Array[Byte], off: Int, len: Int) = out.writeAll(bs, off, len)
 	def write(b: Int) = out.writeByte(b.toByte)
 }
-
-trait References {
-	def id(s: Structure): Int
-	def id(s: ClassLike): Int
-	def structure(id: Int): Structure
-	def classLike(id: Int): ClassLike
-}
