35837efdd492e867bbb46206001dff5ed3bb4ce9
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2013-10-24 12:21:53 +0200

    Remove AnalysisCallback.{beginSource, endSource} methods.
    As pointed out by @harrah in #705, both beginSource and endSource are
    not used in sbt internally for anything meaningful.
    
    We've discussed an option of deprecating those methods but since they
    are not doing anything meaningful Mark prefers to have compile-time
    error in case somebody implements or calls those methods. I agree with
    that hence removal.

--- interface/src/test/scala/TestCallback.scala
+++ interface/src/test/scala/TestCallback.scala
@@ -5,19 +5,14 @@ package xsbti
 
 class TestCallback extends AnalysisCallback
 {
-	val beganSources = new ArrayBuffer[File]
-	val endedSources = new ArrayBuffer[File]
 	val sourceDependencies = new ArrayBuffer[(File, File, Boolean)]
 	val binaryDependencies = new ArrayBuffer[(File, String, File, Boolean)]
 	val products = new ArrayBuffer[(File, File, String)]
 	val apis = new ArrayBuffer[(File, xsbti.api.SourceAPI)]
 
-	def beginSource(source: File) { beganSources += source }
-
 	def sourceDependency(dependsOn: File, source: File, inherited: Boolean) { sourceDependencies += ((dependsOn, source, inherited)) }
 	def binaryDependency(binary: File, name: String, source: File, inherited: Boolean) { binaryDependencies += ((binary, name, source, inherited)) }
 	def generatedClass(source: File, module: File, name: String) { products += ((source, module, name)) }
-	def endSource(source: File) { endedSources += source }
 
 	def api(source: File, sourceAPI: xsbti.api.SourceAPI) { apis += ((source, sourceAPI)) }
 	def problem(category: String, pos: xsbti.Position, message: String, severity: xsbti.Severity, reported: Boolean) {}
