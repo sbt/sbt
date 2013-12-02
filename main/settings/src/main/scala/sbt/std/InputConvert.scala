23f7df64a82e014e048a161b23b5ee375f147a01
Jason Zaugg <jzaugg@gmail.com>
2013-12-02 10:09:53 +0100

    Revert workaround for the now-fixed inference bug, SI-7688
    Partially reverts e6437fdcc707

--- main/settings/src/main/scala/sbt/std/InputConvert.scala
+++ main/settings/src/main/scala/sbt/std/InputConvert.scala
@@ -14,9 +14,9 @@ object InputInitConvert extends Convert
 {
 	def apply[T: c.WeakTypeTag](c: Context)(nme: String, in: c.Tree): Converted[c.type] =
 		if(nme == InputWrapper.WrapInitName)
-			Converted.Success[c.type](in)
+			Converted.Success(in)
 		else if(nme == InputWrapper.WrapInitTaskName)
-			Converted.Failure[c.type](in.pos, "Internal sbt error: initialize+task wrapper not split")
+			Converted.Failure(in.pos, "Internal sbt error: initialize+task wrapper not split")
 		else
 			Converted.NotApplicable
 }
@@ -27,9 +27,9 @@ object ParserConvert extends Convert
 	def apply[T: c.WeakTypeTag](c: Context)(nme: String, in: c.Tree): Converted[c.type] =
 	{
 		if(nme == ParserInput.WrapName)
-			Converted.Success[c.type](in)
+			Converted.Success(in)
 		else if(nme == ParserInput.WrapInitName)
-			Converted.Failure[c.type](in.pos, "Internal sbt error: initialize+parser wrapper not split")
+			Converted.Failure(in.pos, "Internal sbt error: initialize+parser wrapper not split")
 		else
 			Converted.NotApplicable
 	}
@@ -40,7 +40,7 @@ object TaskConvert extends Convert
 {
 	def apply[T: c.WeakTypeTag](c: Context)(nme: String, in: c.Tree): Converted[c.type] =
 		if(nme == InputWrapper.WrapTaskName)
-			Converted.Success[c.type](in)
+			Converted.Success(in)
 		else
 			Converted.NotApplicable
 }
@@ -51,18 +51,18 @@ object FullConvert extends Convert
 		import InputWrapper._
 	def apply[T: c.WeakTypeTag](c: Context)(nme: String, in: c.Tree): Converted[c.type] =
 		if(nme == WrapInitTaskName)
-			Converted.Success[c.type](in)
+			Converted.Success(in)
 		else if(nme == WrapInitName)
 		{
 			val i = c.Expr[Initialize[T]](in)
 			val t = c.universe.reify( Def.toITask(i.splice) ).tree
-			Converted.Success[c.type](t)
+			Converted.Success(t)
 		}
 		else if(nme == WrapTaskName)
 		{
 			val i = c.Expr[Task[T]](in)
 			val t = c.universe.reify( Def.valueStrict[Task[T]](i.splice) ).tree
-			Converted.Success[c.type](t)
+			Converted.Success(t)
 		}
 		else
 			Converted.NotApplicable
@@ -76,10 +76,10 @@ object InitParserConvert extends Convert
 		if(nme == ParserInput.WrapName) {
 			val e = c.Expr[State => Parser[T]](in)
 			val t = c.universe.reify { Def.valueStrict[State => Parser[T]](e.splice) }
-			Converted.Success[c.type](t.tree)
+			Converted.Success(t.tree)
 		}
 		else if(nme == ParserInput.WrapInitName)
-			Converted.Success[c.type](in)
+			Converted.Success(in)
 		else
 			Converted.NotApplicable
 }
