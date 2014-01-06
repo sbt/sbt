dc246cade7027c0539f607758fc351a32529ad31
Jacek Laskowski <jacek@japila.pl>
2014-01-06 14:08:02 +0100

    Javadoc changes

--- launch/interface/src/main/java/xsbti/Reboot.java
+++ launch/interface/src/main/java/xsbti/Reboot.java
@@ -2,9 +2,11 @@ package xsbti;
 
 import java.io.File;
 
-/** A launched application returns an instance of this class in order to communicate to the launcher
-* that the application should be restarted.  Different versions of the application and Scala can be used.
-* The application can be given different arguments and a new working directory as well.*/
+/**
+ * A launched application returns an instance of this class in order to communicate to the launcher
+ * that the application should be restarted. Different versions of the application and Scala can be used.
+ * The application can be given different arguments as well as a new working directory.
+ */
 public interface Reboot extends MainResult
 {
 	public String[] arguments();
