f0ecf1496e3f46e0d45b44608fdcb827984f972b
Mark Harrah <dmharrah@gmail.com>
2014-01-14 16:18:28 -0500

    applicative for [t] Either[Seq[E], t]

--- util/collection/src/main/scala/sbt/Classes.scala
+++ util/collection/src/main/scala/sbt/Classes.scala
@@ -31,4 +31,18 @@ object Classes
 		def flatten[T](m: List[List[T]]): List[T] = m.flatten
 	}
 	private[this] def app2[S,T] = (f: S => T,v: S) => f(v)
+
+	type VApp[E] = Applicative[ ({ type l[t] = Either[Seq[E], t] })#l ]
+	def validationApp[E]: VApp[E] = new VApp[E] {
+		def pure[S](s: => S): Either[Seq[E], S] = Right(s)
+		def map[S, T](f: S => T, v: Either[Seq[E], S]): Either[Seq[E], T] = v.right.map(f)
+		def apply[S,T](f: Either[Seq[E], S => T], v: Either[Seq[E], S]): Either[Seq[E], T] = apply2(f,v)(app2)
+		def apply2[S,T,U](s: Either[Seq[E], S], t: Either[Seq[E], T])(f: (S,T) => U): Either[Seq[E], U] =
+			(s, t) match {
+				case (Left(es), Left(ts)) => Left(es ++ ts)
+				case (Left(es), Right(_)) => Left(es)
+				case (Right(_), Left(ts)) => Left(ts)
+				case (Right(x), Right(y)) => Right(f(x,y))
+			}
+	}
 }
