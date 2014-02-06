59f66bc549316d2bcd937590d7a8684a49a0ab77
Dan Sanduleac <Dan.Sanduleac@gsacapital.com>
2014-02-06 15:38:37 +0000

    Retrieve dynamic app versions correctly

--- launch/src/main/scala/xsbt/boot/ModuleDefinition.scala
+++ launch/src/main/scala/xsbt/boot/ModuleDefinition.scala
@@ -13,8 +13,12 @@ final class ModuleDefinition(val configuration: UpdateConfiguration, val extraCl
 	private def versionString: String = target match { case _: UpdateScala => configuration.getScalaVersion; case a: UpdateApp => Value.get(a.id.version) }
 }
 
-final class RetrievedModule(val fresh: Boolean, val definition: ModuleDefinition, val detectedScalaVersion: Option[String], val baseDirectories: List[File])
+final class RetrievedModule(val fresh: Boolean, val definition: ModuleDefinition, val detectedScalaVersion: Option[String], val resolvedAppVersion: Option[String], val baseDirectories: List[File])
 {
+	/** Use this constructor only when the module exists already, or when its version is not dynamic (so its resolved version would be the same) */
+	def this(fresh: Boolean, definition: ModuleDefinition, detectedScalaVersion: Option[String], baseDirectories: List[File]) =
+		this(fresh, definition, detectedScalaVersion, None, baseDirectories)
+
 	lazy val classpath: Array[File] = getJars(baseDirectories)
 	lazy val fullClasspath: Array[File] = concat(classpath, definition.extraClasspath)
 
