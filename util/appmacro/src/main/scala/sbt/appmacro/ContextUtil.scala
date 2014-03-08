fb74cd4c278f9026c54392382f516ece2acb8060
Josh Suereth <joshua.suereth@gmail.com>
2014-03-07 22:25:29 -0500

    Revert "Fix task macro's handling of Symbol owners in <qual>.value"
    This reverts commit 3017bfcd07466a2c3aa9bc5fe4760929db8ae0ed.
    
    This was causing sbt to be unable to compile.  Reverting temporarily until
    we have a shot at a full fix.

--- util/appmacro/src/main/scala/sbt/appmacro/ContextUtil.scala
+++ util/appmacro/src/main/scala/sbt/appmacro/ContextUtil.scala
@@ -226,19 +226,17 @@ final class ContextUtil[C <: Context](val ctx: C)
 		object appTransformer extends Transformer
 		{
 			override def transform(tree: Tree): Tree =
-				tree match {
-					case ApplyTree(TypeApply(Select(_, nme), targ :: Nil), qual :: Nil) =>
-						changeOwner(qual, currentOwner, initialOwner) // Fixes https://github.com/sbt/sbt/issues/1150
-						subWrapper(nme.decoded, targ.tpe, qual, tree) match {
-							case Converted.Success(t, finalTx) => finalTx(t)
-							case Converted.Failure(p,m) => ctx.abort(p, m)
-							case _: Converted.NotApplicable[_] => super.transform(tree)
-						}
+				tree match
+				{
+					case ApplyTree(TypeApply(Select(_, nme), targ :: Nil), qual :: Nil) => subWrapper(nme.decoded, targ.tpe, qual, tree) match {
+						case Converted.Success(t, finalTx) => finalTx(t)
+						case Converted.Failure(p,m) => ctx.abort(p, m)
+						case _: Converted.NotApplicable[_] => super.transform(tree)
+					}
 					case _ => super.transform(tree)
 				}
 		}
-		appTransformer.atOwner(initialOwner) {
-			appTransformer.transform(t)
-		}
+
+		appTransformer.transform(t)
 	}
 }
