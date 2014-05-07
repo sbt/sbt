9f9de600ee8b762d2bf2e363b3aded1177eb7e62
Josh Suereth <joshua.suereth@gmail.com>
2014-05-07 11:52:23 -0400

    Scalariforming test code

--- util/collection/src/test/scala/PMapTest.scala
+++ util/collection/src/test/scala/PMapTest.scala
@@ -6,14 +6,13 @@ package sbt
 import Types._
 
 // compilation test
-object PMapTest
-{
-	val mp = new DelegatingPMap[Some, Id](new collection.mutable.HashMap)
-	mp(Some("asdf")) = "a"
-	mp(Some(3)) = 9
-	val x = Some(3) :^: Some("asdf") :^: KNil
-	val y = x.transform[Id](mp)
-	assert(y.head == 9)
-	assert(y.tail.head == "a")
-	assert(y.tail.tail == KNil)
+object PMapTest {
+  val mp = new DelegatingPMap[Some, Id](new collection.mutable.HashMap)
+  mp(Some("asdf")) = "a"
+  mp(Some(3)) = 9
+  val x = Some(3) :^: Some("asdf") :^: KNil
+  val y = x.transform[Id](mp)
+  assert(y.head == 9)
+  assert(y.tail.head == "a")
+  assert(y.tail.tail == KNil)
 }
