2c3f40eb02fe108bf99a139b988adbb7a6735ae0
Josh Suereth <joshua.suereth@gmail.com>
2014-06-02 15:10:17 -0400

    Improve test failure message for #1390.

--- project/Sbt.scala
+++ project/Sbt.scala
@@ -25,7 +25,7 @@ object Sbt extends Build {
     javacOptions in compile ++= Seq("-target", "6", "-source", "6", "-Xlint", "-Xlint:-serial"),
     incOptions := incOptions.value.withNameHashing(true),
     commands += Command.command("setupBuildScala211") { state =>
-      """set scalaVersion in ThisBuild := "2.11.0" """ ::
+      """set scalaVersion in ThisBuild := "2.11.1" """ ::
         "set Util.includeTestDependencies in ThisBuild := true" ::
         state
     },
@@ -46,12 +46,12 @@ object Sbt extends Build {
       def lameAgregateTask(task: String): String =
         s"all control/$task collections/$task io/$task completion/$task"
       "setupBuildScala211" ::
-      /// First test
-      lameAgregateTask("test") ::
-      // Note: You need the sbt-pgp plugin installed to release.
-      lameAgregateTask("publishSigned") ::
-      // Now restore the defaults.
-      "reload" :: state
+        /// First test
+        lameAgregateTask("test") ::
+        // Note: You need the sbt-pgp plugin installed to release.
+        lameAgregateTask("publishSigned") ::
+        // Now restore the defaults.
+        "reload" :: state
     },
     commands += Command.command("release-sbt") { state =>
       // TODO - Any sort of validation
