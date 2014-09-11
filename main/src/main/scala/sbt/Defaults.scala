66d8e72f2a38eb8070621ecec6a29eafc1389e28
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2014-09-10 20:18:28 -0700

    Add developers keySetting
    The motivation for this is mainly to avoid having to define a pomExtra to publish to Maven central

--- main/src/main/scala/sbt/Defaults.scala
+++ main/src/main/scala/sbt/Defaults.scala
@@ -1042,7 +1042,7 @@ object Classpaths {
     organization <<= organization or normalizedName,
     organizationName <<= organizationName or organization,
     organizationHomepage <<= organizationHomepage or homepage,
-    projectInfo <<= (name, description, homepage, startYear, licenses, organizationName, organizationHomepage, scmInfo) apply ModuleInfo,
+    projectInfo <<= (name, description, homepage, startYear, licenses, organizationName, organizationHomepage, scmInfo, developers) apply ModuleInfo,
     overrideBuildResolvers <<= appConfiguration(isOverrideRepositories),
     externalResolvers <<= (externalResolvers.task.?, resolvers, appResolvers) {
       case (Some(delegated), Seq(), _) => delegated
