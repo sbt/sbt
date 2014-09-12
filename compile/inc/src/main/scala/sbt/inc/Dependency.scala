c09a3912d1d5390d84c93607d261cb5dd5c23748
Martin Duhem <martin@c-webhosting.org>
2014-09-12 23:45:45 +0200

    Implement abstraction over dependency kinds
    This commit implements the abstraction over the dependency kinds and
    deprecates the methods that were used to register files and their
    dependencies.
    
    Dependencies are now divided into two categories : Internal and
    External dependencies. Moreover, each internal or external dependency
    has a Context, which describes what introduced the dependency (for
    instance, a dependency may be introduced by member reference or by
    inheritance).
    
    Dependencies must now be registered using the method `addSource` in
    `Analysis` and in `Relations`. This method has the advantage of being
    independent from the existing dependency contexts. That is, its
    signature does not need to be modified whenever a new dependency
    context is introduced.

--- compile/inc/src/main/scala/sbt/inc/Dependency.scala
+++ compile/inc/src/main/scala/sbt/inc/Dependency.scala
@@ -4,39 +4,11 @@ import java.io.File
 import xsbti.api.Source
 
 /**
- * Dependency tracked by incremental compiler consists of two parts:
- *   - `edge` which describes an edge in dependency graph
- *   - `context` which stores context information from a src file where the dependency got introduced
- *
- * The context is needed for incremental compiler to decide what kind of invalidation strategy to use.
- * It might also store some additional information useful for debugging.
- */
-private[inc] final case class Dependency(edge: DependencyEdge, context: DependencyContext)
-
-private[inc] sealed abstract class DependencyEdge
-/**
- * External dependency from src file to class name. External means "outside of enclosing Analysis".
- * Typically that means a dependency coming from another subproject in sbt.
- *
- * The classApi contains snapshot of information about the class (that `toClassName` points at)
- * as observed from point of view of compilation that produced this dependency edge.
- *
- * The classApi is stored so we can detect changes to external apis by comparing snapshots
- * of the api from two Analysis instances.
- */
-private[inc] final case class ExternalDepndencyEdge(fromSrc: File, toClassName: String, classApi: Source)
-	extends DependencyEdge
-
-/**
- * Represents a dependency edge between two source files in the same sbt subproject.
- */
-private[inc] final case class InternalDependencyEdge(fromSrc: File, toSrc: File) extends DependencyEdge
-
-/**
  * Represents contextual information about particular depedency edge. See comments in
  * subtypes for examples of particular contexts.
  */
 private[inc] sealed abstract class DependencyContext
+
 /**
  * Marks dependency edge introduced by referring to a class through inheritance as in
  *
@@ -46,3 +18,13 @@ private[inc] sealed abstract class DependencyContext
  */
 private[inc] final case object DependencyByInheritance extends DependencyContext
 private[inc] final case object DependencyByMemberRef extends DependencyContext
+
+/**
+ * Represents the kind of dependency that exists between `sourceFile` and either `targetFile`
+ * or `targetClassName`.
+ *
+ * `InternalDependency` represent dependencies that exist between the files of a same project,
+ * while `ExternalDependency` represent cross-project dependencies.
+ */
+private[inc] final case class InternalDependency(sourceFile: File, targetFile: File, context: DependencyContext)
+private[inc] final case class ExternalDependency(sourceFile: File, targetClassName: String, targetSource: Source, context: DependencyContext)
