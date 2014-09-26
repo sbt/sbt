e8218faa7fb777ab99e7615fa0fecd1e6580264b
Josh Suereth <joshua.suereth@gmail.com>
2014-09-26 13:48:40 -0400

    Acceptance tests for  and new .sbt parser.

--- sbt/src/sbt-test/project/session-save/project/Build.scala
+++ sbt/src/sbt-test/project/session-save/project/Build.scala
@@ -3,6 +3,9 @@ import sbt._
 object TestBuild extends Build {
 	val k1 = TaskKey[Unit]("k1")
 	val k2 = TaskKey[Unit]("k2")
+  val k3 = TaskKey[Unit]("k3")
+  val k4 = TaskKey[Unit]("k4")
+  val k5 = TaskKey[Unit]("k4")
 
 	lazy val root = Project("root", file("."))
 }
