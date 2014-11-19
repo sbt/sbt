7bad42c6711621c6a8b55ff0ce6f20bddfe733ab
Martin Duhem <martin@c-webhosting.org>
2014-11-19 08:52:52 +0100

    Abstract over dependency context in Compile
    This commit completes the abstraction over dependency kinds in the
    incremental compiler, started with #1340.

--- interface/src/test/scala/xsbti/TestCallback.scala
+++ interface/src/test/scala/xsbti/TestCallback.scala
@@ -3,17 +3,26 @@ package xsbti
 import java.io.File
 import scala.collection.mutable.ArrayBuffer
 import xsbti.api.SourceAPI
+import xsbti.DependencyContext._
 
 class TestCallback(override val nameHashing: Boolean = false) extends AnalysisCallback
 {
-	val sourceDependencies = new ArrayBuffer[(File, File, Boolean)]
-	val binaryDependencies = new ArrayBuffer[(File, String, File, Boolean)]
+	val sourceDependencies = new ArrayBuffer[(File, File, DependencyContext)]
+	val binaryDependencies = new ArrayBuffer[(File, String, File, DependencyContext)]
 	val products = new ArrayBuffer[(File, File, String)]
 	val usedNames = scala.collection.mutable.Map.empty[File, Set[String]].withDefaultValue(Set.empty)
 	val apis: scala.collection.mutable.Map[File, SourceAPI] = scala.collection.mutable.Map.empty
 
-	def sourceDependency(dependsOn: File, source: File, inherited: Boolean) { sourceDependencies += ((dependsOn, source, inherited)) }
-	def binaryDependency(binary: File, name: String, source: File, inherited: Boolean) { binaryDependencies += ((binary, name, source, inherited)) }
+	def sourceDependency(dependsOn: File, source: File, inherited: Boolean) {
+		val context = if(inherited) DependencyByInheritance else DependencyByMemberRef
+		sourceDependency(dependsOn, source, context)
+	}
+	def sourceDependency(dependsOn: File, source: File, context: DependencyContext) { sourceDependencies += ((dependsOn, source, context)) }
+	def binaryDependency(binary: File, name: String, source: File, inherited: Boolean) {
+		val context = if(inherited) DependencyByInheritance else DependencyByMemberRef
+		binaryDependency(binary, name, source, context)
+	}
+	def binaryDependency(binary: File, name: String, source: File, context: DependencyContext) { binaryDependencies += ((binary, name, source, context)) }
 	def generatedClass(source: File, module: File, name: String) { products += ((source, module, name)) }
 
 	def usedName(source: File, name: String) { usedNames(source) += name }
