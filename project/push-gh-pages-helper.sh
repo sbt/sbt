b1a3f578ac8fecb8dede9a6fd4cbb3cb2a8cd66e
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-06-18 01:00:03 +0200

    Pushing to gh-pages from Travis

---
+++ project/push-gh-pages-helper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "$1" = "clone" ]; then
+  git clone "https://alexarchambault:${GH_TOKEN}@github.com/alexarchambault/coursier.git" -b gh-pages coursier-gh-pages >/dev/null 2>&1
+else
+  git push origin gh-pages >/dev/null 2>&1
+fi
