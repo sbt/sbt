02d28ef9bb55bb599a3155113f3e4f403374ec88
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-06-21 18:04:55 +0100

    Cleaning

--- core/src/test/scala/coursier/test/CentralTests.scala
+++ core/src/test/scala/coursier/test/CentralTests.scala
@@ -16,22 +16,15 @@ object CentralTests extends TestSuite {
     'logback{
       async {
         val dep = Dependency(Module("ch.qos.logback", "logback-classic"), "1.1.3")
-        val res0 =
-          await(resolve(Set(dep), fetchFrom(repositories))
-            .runF)
-
-        val res = res0.copy(
-          projectsCache = Map.empty, errors = Map.empty // No validating these here
-        )
+        val res = await(resolve(Set(dep), fetchFrom(repositories)).runF)
+          .copy(projectsCache = Map.empty, errors = Map.empty) // No validating these here
 
         val expected = Resolution(
           rootDependencies = Set(dep.withCompileScope),
           dependencies = Set(
             dep.withCompileScope,
             Dependency(Module("ch.qos.logback", "logback-core"), "1.1.3").withCompileScope,
-            Dependency(Module("org.slf4j", "slf4j-api"), "1.7.7").withCompileScope
-          )
-        )
+            Dependency(Module("org.slf4j", "slf4j-api"), "1.7.7").withCompileScope))
 
         assert(res == expected)
       }
@@ -39,22 +32,15 @@ object CentralTests extends TestSuite {
     'asm{
       async {
         val dep = Dependency(Module("org.ow2.asm", "asm-commons"), "5.0.2")
-        val res0 =
-          await(resolve(Set(dep), fetchFrom(repositories))
-            .runF)
-
-        val res = res0.copy(
-          projectsCache = Map.empty, errors = Map.empty // No validating these here
-        )
+        val res = await(resolve(Set(dep), fetchFrom(repositories)).runF)
+          .copy(projectsCache = Map.empty, errors = Map.empty) // No validating these here
 
         val expected = Resolution(
           rootDependencies = Set(dep.withCompileScope),
           dependencies = Set(
             dep.withCompileScope,
             Dependency(Module("org.ow2.asm", "asm-tree"), "5.0.2").withCompileScope,
-            Dependency(Module("org.ow2.asm", "asm"), "5.0.2").withCompileScope
-          )
-        )
+            Dependency(Module("org.ow2.asm", "asm"), "5.0.2").withCompileScope))
 
         assert(res == expected)
       }
