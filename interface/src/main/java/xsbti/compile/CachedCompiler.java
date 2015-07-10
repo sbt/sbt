5d4e91d6443f4a74cdba3087634576eefb22e96b
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-07-11 00:11:17 +0200

    Remove redundant public modifier in Java interfaces

--- interface/src/main/java/xsbti/compile/CachedCompiler.java
+++ interface/src/main/java/xsbti/compile/CachedCompiler.java
@@ -8,6 +8,6 @@ import java.io.File;
 public interface CachedCompiler
 {
 	/** Returns an array of arguments representing the nearest command line equivalent of a call to run but without the command name itself.*/
-	public String[] commandArguments(File[] sources);
-	public void run(File[] sources, DependencyChanges cpChanges, AnalysisCallback callback, Logger log, Reporter delegate, CompileProgress progress);
+	String[] commandArguments(File[] sources);
+	void run(File[] sources, DependencyChanges cpChanges, AnalysisCallback callback, Logger log, Reporter delegate, CompileProgress progress);
 }
