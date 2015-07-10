98d81a62f92b9d13d1c3a658e140ad9daa227261
Eugene Yokota <eed3si9n@gmail.com>
2015-07-09 21:23:31 -0400

    Switch to serialization for UpdateReport cache

--- main/src/main/scala/sbt/Defaults.scala
+++ main/src/main/scala/sbt/Defaults.scala
@@ -1411,13 +1411,13 @@ object Classpaths {
 
       val outCacheFile = cacheFile / "output"
       def skipWork: In => UpdateReport =
-        Tracked.lastOutput[In, UpdateReport](outCacheFile) {
+        Tracked.lastOutputWithJson[In, UpdateReport](outCacheFile) {
           case (_, Some(out)) => out
           case _              => sys.error("Skipping update requested, but update has not previously run successfully.")
         }
       def doWork: In => UpdateReport =
         Tracked.inputChanged(cacheFile / "inputs") { (inChanged: Boolean, in: In) =>
-          val outCache = Tracked.lastOutput[In, UpdateReport](outCacheFile) {
+          val outCache = Tracked.lastOutputWithJson[In, UpdateReport](outCacheFile) {
             case (_, Some(out)) if uptodate(inChanged, out) => out
             case _ => work(in)
           }
