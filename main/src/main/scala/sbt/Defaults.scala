ef2ffcf34b9bfa731734fca350338202ab5049de
Martin Duhem <martin.duhem@gmail.com>
2015-08-27 11:03:48 +0200

    Implement `projects` for .sbt files
    `projects` gives access to the list of projects that have been loaded
    from .sbt files, just like it does for .scala files.
    
    This works by registering all the projects that are discovered in .sbt
    files in a global variable. The list of projects is then retrieved from
    this global variable.

--- main/src/main/scala/sbt/Defaults.scala
+++ main/src/main/scala/sbt/Defaults.scala
@@ -1824,6 +1824,11 @@ trait BuildExtra extends BuildCommon with DefExtra {
   import Defaults._
 
   /**
+   * The list of all projects that have been loaded.
+   */
+  def projects: Seq[Project] = BuildExtra.knownProjects.values.toSeq.flatten
+
+  /**
    * Defines an alias given by `name` that expands to `value`.
    * This alias is defined globally after projects are loaded.
    * The alias is undefined when projects are unloaded.
@@ -1956,6 +1961,11 @@ trait BuildExtra extends BuildCommon with DefExtra {
   def filterKeys(ss: Seq[Setting[_]], transitive: Boolean = false)(f: ScopedKey[_] => Boolean): Seq[Setting[_]] =
     ss filter (s => f(s.key) && (!transitive || s.dependencies.forall(f)))
 }
+object BuildExtra {
+  import scala.collection.mutable.Map
+  private[sbt] val knownProjects: Map[String, Seq[Project]] = Map.empty
+}
+
 trait DefExtra {
   private[this] val ts: TaskSequential = new TaskSequential {}
   implicit def toTaskSequential(d: Def.type): TaskSequential = ts
