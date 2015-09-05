3bb53ae4fc50ffd7218a5bb465f0bd3f32060a10
Eugene Yokota <eed3si9n@gmail.com>
2015-09-05 00:51:58 -0400

    sbt.util.internal -> sbt.internal.util package

---
+++ internal/util-collection/src/main/scala/sbt/internal/util/Show.scala
@@ -0,0 +1,8 @@
+package sbt.internal.util
+
+trait Show[T] {
+  def apply(t: T): String
+}
+object Show {
+  def apply[T](f: T => String): Show[T] = new Show[T] { def apply(t: T): String = f(t) }
+}
