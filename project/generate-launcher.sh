ee1a53f991cbc9f8d04a09283762c408c2f9df45
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-12-13 12:16:52 +0000

    Update launcher

--- project/generate-launcher.sh
+++ project/generate-launcher.sh
@@ -2,9 +2,13 @@
 
 "$(dirname "$0")/../cli/target/pack/bin/coursier" bootstrap \
   com.github.alexarchambault:coursier-cli_2.11:0.1.0-SNAPSHOT \
-  -D "\$(cd \$(dirname \"\$0\"); pwd)/.coursier/0.1.0-SNAPSHOT-0f15f7f/bootstrap" \
-  -r https://repo1.maven.org/maven2 -r https://oss.sonatype.org/content/repositories/snapshots \
+  -V com.github.alexarchambault:coursier_2.11:0.1.0-M2 \
+  -V com.github.alexarchambault:coursier-files_2.11:0.1.0-M2 \
+  -D "\$(cd \$(dirname \"\$0\"); pwd)/.coursier/0.1.0-M2/bootstrap" \
+  -r https://repo1.maven.org/maven2 \
+  -r https://oss.sonatype.org/content/repositories/releases \
+  -r https://oss.sonatype.org/content/repositories/snapshots \
   -b \
   -f -o coursier \
   -M coursier.cli.Coursier \
-  -e COURSIER_CACHE="\$(cd \$(dirname \"\$0\"); pwd)/.coursier/0.1.0-SNAPSHOT-0f15f7f"
+  -e COURSIER_CACHE="\$(cd \$(dirname \"\$0\"); pwd)/.coursier/0.1.0-M2"
