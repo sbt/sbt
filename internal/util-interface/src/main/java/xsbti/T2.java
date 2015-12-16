121972577dd3179b81b9513d5bb1ecf0a4a72662
Eugene Yokota <eed3si9n@gmail.com>
2015-12-16 15:54:23 -0500

    Define T2 as an interface.

--- internal/util-interface/src/main/java/xsbti/T2.java
+++ internal/util-interface/src/main/java/xsbti/T2.java
@@ -1,12 +1,8 @@
 package xsbti;
 
 /** Used to pass a pair of values. */
-public class T2<A1, A2> {
-  public final A1 _1;
-  public final A2 _2;
-
-  public T2(A1 a1, A2 a2) {
-    this._1 = a1;
-    this._2 = a2;
-  }
- }
+public interface T2<A1, A2>
+{
+  public A1 get1();
+  public A2 get2();
+}
