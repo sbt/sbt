3a6b6aadc0dfe302a2cdcb3791201d15141172aa
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2015-12-23 14:17:47 +0100

    Add scripted (functional test) for #2324.
    Add a test that shows the scenario described in #2324 is handled correctly
    now.

---
+++ sbt/src/sbt-test/compiler-project/private-public-both-modified/changes/A1.scala
@@ -0,0 +1,5 @@
+object A {
+  private def foo: String = "1"
+  def bar: String = "29"
+  def xyz: Int = 101
+}
---
+++ sbt/src/sbt-test/compiler-project/private-public-both-modified/src/main/scala/A.scala
@@ -0,0 +1,5 @@
+object A {
+  private def foo: Int = 1
+  def bar: Int = 29
+  def xyz: Int = 101
+}
