4af9863757b97be615a78c289c201bd0d96b9559
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-12-30 21:29:20 +0100

    Update test

--- tests/jvm/src/test/scala/coursier/test/IvyLocalTests.scala
+++ tests/jvm/src/test/scala/coursier/test/IvyLocalTests.scala
@@ -9,7 +9,7 @@ object IvyLocalTests extends TestSuite {
     'coursier{
       // Assume this module (and the sub-projects it depends on) is published locally
       CentralTests.resolutionCheck(
-        Module("com.github.alexarchambault", "coursier_2.11"), "0.1.0-SNAPSHOT",
+        Module("com.github.alexarchambault", "coursier_2.11"), "1.0.0-SNAPSHOT",
         Some(Cache.ivy2Local))
     }
   }
