e9e034f8175d0bb5dc8f9003481787880211f9cc
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-12-30 01:34:40 +0100

    Clean-up

--- plugin/src/main/scala/coursier/Keys.scala
+++ plugin/src/main/scala/coursier/Keys.scala
@@ -4,10 +4,10 @@ import java.io.File
 import sbt.{ GetClassifiersModule, Resolver, SettingKey, TaskKey }
 
 object Keys {
-  val coursierParallelDownloads = SettingKey[Int]("coursier-parallel-downloads", "") // 6
-  val coursierMaxIterations = SettingKey[Int]("coursier-max-iterations", "") // 50
-  val coursierChecksums = SettingKey[Seq[Option[String]]]("coursier-checksums", "") //Seq(Some("SHA-1"), Some("MD5"))
-  val coursierCachePolicy = SettingKey[CachePolicy]("coursier-cache-policy", "") // = CachePolicy.FetchMissing
+  val coursierParallelDownloads = SettingKey[Int]("coursier-parallel-downloads", "")
+  val coursierMaxIterations = SettingKey[Int]("coursier-max-iterations", "")
+  val coursierChecksums = SettingKey[Seq[Option[String]]]("coursier-checksums", "")
+  val coursierCachePolicy = SettingKey[CachePolicy]("coursier-cache-policy", "")
 
   val coursierVerbosity = SettingKey[Int]("coursier-verbosity", "")
 
