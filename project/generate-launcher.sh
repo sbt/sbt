1b5f573759d4af9dde8c156c494513bcf72fad3e
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-12-31 01:12:55 +0100

    Update launcher

--- project/generate-launcher.sh
+++ project/generate-launcher.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+VERSION=1.0.0-M1
+CACHE_VERSION=v1
+
 "$(dirname "$0")/../cli/target/pack/bin/coursier" bootstrap \
-  com.github.alexarchambault:coursier-cli_2.11:0.1.0-SNAPSHOT \
-  -V com.github.alexarchambault:coursier_2.11:0.1.0-M2 \
-  -V com.github.alexarchambault:coursier-files_2.11:0.1.0-M2 \
-  -D "\$(cd \$(dirname \"\$0\"); pwd)/.coursier/0.1.0-M2/bootstrap" \
+  com.github.alexarchambault:coursier-cli_2.11:$VERSION \
+  -V com.github.alexarchambault:coursier_2.11:$VERSION \
+  -V com.github.alexarchambault:coursier-cache_2.11:$VERSION \
+  -D "\$HOME/.coursier/bootstrap/$VERSION" \
   -r https://repo1.maven.org/maven2 \
   -r https://oss.sonatype.org/content/repositories/releases \
   -r https://oss.sonatype.org/content/repositories/snapshots \
   -b \
   -f -o coursier \
   -M coursier.cli.Coursier \
-  -e COURSIER_CACHE="\$(cd \$(dirname \"\$0\"); pwd)/.coursier/0.1.0-M2"
+  -e COURSIER_CACHE="\$HOME/.coursier/cache/$CACHE_VERSION"
