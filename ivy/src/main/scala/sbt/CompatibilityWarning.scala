bdffad284529097e065b257698e8ee4c6d5e9ec9
Eugene Yokota <eed3si9n@gmail.com>
2015-12-31 22:36:04 -0500

    Improve warning message

--- ivy/src/main/scala/sbt/CompatibilityWarning.scala
+++ ivy/src/main/scala/sbt/CompatibilityWarning.scala
@@ -16,8 +16,9 @@ private[sbt] object CompatibilityWarning {
     directDependencies foreach { m =>
       if (!m.isTransitive) {
         log.warn(
-          s"""Found intransitive dependency ($m), but Maven repositories do not support intransitive dependencies.
-             |  Use exclusions instead so transitive dependencies will be correctly excluded in dependent projects.
+          s"""Found intransitive dependency ($m) while publishMavenStyle is true, but Maven repositories
+             |  do not support intransitive dependencies. Use exclusions instead so transitive dependencies
+             |  will be correctly excluded in dependent projects.
            """.stripMargin)
       } else ()
     }
