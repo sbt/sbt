5708a7f03b3338293cdd5c7fbec5e606bd980b82
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-01-03 19:35:02 +0100

    Update generate-launcher script

--- project/generate-launcher.sh
+++ project/generate-launcher.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-VERSION=1.0.0-M1
+VERSION=1.0.0-M2
 CACHE_VERSION=v1
 
 "$(dirname "$0")/../cli/target/pack/bin/coursier" bootstrap \
   com.github.alexarchambault:coursier-cli_2.11:$VERSION \
-  -V com.github.alexarchambault:coursier_2.11:$VERSION \
-  -V com.github.alexarchambault:coursier-cache_2.11:$VERSION \
   -D "\${user.home}/.coursier/bootstrap/$VERSION" \
-  -r https://repo1.maven.org/maven2 \
-  -r https://oss.sonatype.org/content/repositories/releases \
-  -r https://oss.sonatype.org/content/repositories/snapshots \
   -b \
   -f -o coursier \
   -M coursier.cli.Coursier \
