3e9a37edc358e1960ce607e9958c310afa2a01c8
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-01-23 15:42:06 +0100

    Do not fetch artifacts with a classifier from Ivy repositories if not asked to do so

--- tests/jvm/src/test/scala/coursier/test/IvyLocalTests.scala
+++ tests/jvm/src/test/scala/coursier/test/IvyLocalTests.scala
@@ -1,16 +1,41 @@
 package coursier.test
 
-import coursier.{ Module, Cache }
+import coursier.{ Dependency, Module, Cache }
+import coursier.test.compatibility._
+
+import scala.async.Async.{ async, await }
+
 import utest._
 
 object IvyLocalTests extends TestSuite {
 
   val tests = TestSuite{
     'coursier{
+      val module = Module("com.github.alexarchambault", "coursier_2.11")
+      val version = "1.0.0-SNAPSHOT"
+
+      val extraRepo = Some(Cache.ivy2Local)
+
       // Assume this module (and the sub-projects it depends on) is published locally
       CentralTests.resolutionCheck(
-        Module("com.github.alexarchambault", "coursier_2.11"), "1.0.0-SNAPSHOT",
-        Some(Cache.ivy2Local))
+        module, version,
+        extraRepo
+      )
+
+
+      async {
+        val res = await(CentralTests.resolve(
+          Set(Dependency(module, version)),
+          extraRepo = extraRepo
+        ))
+
+        val artifacts = res.artifacts.map(_.url)
+        val anyJavadoc = artifacts.exists(_.contains("-javadoc"))
+        val anySources = artifacts.exists(_.contains("-sources"))
+
+        assert(!anyJavadoc)
+        assert(!anySources)
+      }
     }
   }
 
