1e4168e87258dce577d0456488fd383b644fdf51
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-02-12 11:55:45 +0100

    Update generate-launcher script

--- project/generate-launcher.sh
+++ project/generate-launcher.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-VERSION=1.0.0-M5
+VERSION=1.0.0-M7
 CACHE_VERSION=v1
 
-"$(dirname "$0")/../cli/target/pack/bin/coursier" bootstrap \
+SBTPACK_LAUNCHER="$(dirname "$0")/../cli/target/pack/bin/coursier"
+
+if [ ! -f "$SBTPACK_LAUNCHER" ]; then
+  sbt cli/pack
+fi
+
+"$SBTPACK_LAUNCHER" bootstrap \
   com.github.alexarchambault:coursier-cli_2.11:$VERSION \
   --no-default \
   -r central \
+  -r sonatype:releases \
   -D "\${user.home}/.coursier/bootstrap/$VERSION" \
   -f -o coursier \
   -M coursier.cli.Coursier \
