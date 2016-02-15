86b53278dfbccdbf80c3053fff0946eec1b2ed12
Martin Duhem <martin.duhem@gmail.com>
2016-02-15 10:55:23 +0100

    Sleep for 500ms after IO file operations in scripted
    Hopefully this will prevent spurious failures on Travis.

--- internal/incrementalcompiler-scripted/src/test/scala/sbt/internal/inc/ScriptedHandlers.scala
+++ internal/incrementalcompiler-scripted/src/test/scala/sbt/internal/inc/ScriptedHandlers.scala
@@ -2,9 +2,20 @@ package sbt.internal.inc
 
 import sbt.internal.scripted._
 
+class SleepingHandler(val handler: StatementHandler, delay: Long) extends StatementHandler {
+  type State = handler.State
+  override def initialState: State = handler.initialState
+  override def apply(command: String, arguments: List[String], state: State): State = {
+    val result = handler.apply(command, arguments, state)
+    Thread.sleep(delay)
+    result
+  }
+  override def finish(state: State) = handler.finish(state)
+}
+
 class IncScriptedHandlers extends HandlersProvider {
   def getHandlers(config: ScriptConfig): Map[Char, StatementHandler] = Map(
-    '$' -> new FileCommands(config.testDirectory()),
+    '$' -> new SleepingHandler(new FileCommands(config.testDirectory()), 500),
     '#' -> CommentHandler,
     '>' -> new IncHandler(config.testDirectory(), config.logger())
   )
