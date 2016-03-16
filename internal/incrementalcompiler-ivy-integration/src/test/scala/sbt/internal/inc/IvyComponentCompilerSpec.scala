f32ea08a5367f61e1611f431677326ab931c4d27
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2016-03-16 20:28:41 +0100

    Zinc is compatible with Scala 2.10 and 2.11 only
    The class-based-dependencies branch nuked compatibility with older Scala
    compiler versions.

--- internal/incrementalcompiler-ivy-integration/src/test/scala/sbt/internal/inc/IvyComponentCompilerSpec.scala
+++ internal/incrementalcompiler-ivy-integration/src/test/scala/sbt/internal/inc/IvyComponentCompilerSpec.scala
@@ -5,22 +5,8 @@ import sbt.util.Logger
 
 class IvyComponentCompilerSpec extends BridgeProviderSpecification {
 
-  val scala282 = "2.8.2"
-  val scala292 = "2.9.2"
   val scala210 = "2.10.5"
   val scala211 = "2.11.7"
-
-  "IvyComponentCompiler" should "compile the bridge for Scala 2.8" in {
-    IO.withTemporaryDirectory { tempDir =>
-      getCompilerBridge(tempDir, Logger.Null, scala282) should exist
-    }
-  }
-
-  it should "compile the bridge for Scala 2.9" in {
-    IO.withTemporaryDirectory { tempDir =>
-      getCompilerBridge(tempDir, Logger.Null, scala292) should exist
-    }
-  }
 
   it should "compile the bridge for Scala 2.10" in {
     IO.withTemporaryDirectory { tempDir =>
