c60578e2972b05a7a13e2eedbf5861c2d21aba21
Eugene Yokota <eed3si9n@gmail.com>
2016-03-29 00:03:12 -0400

    Port actions/aggregate

--- sbt/src/sbt-test/actions/aggregate/project/Marker.scala
+++ sbt/src/sbt-test/actions/aggregate/project/Marker.scala
@@ -3,16 +3,20 @@ import Keys._
 import Import._
 import Project.Initialize
 
-trait Marker
+object Marker extends AutoPlugin
 {
-	final lazy val Mark = TaskKey[Unit]("mark")
-	final def mark: Initialize[Task[Unit]] = mark(baseDirectory)
-	final def mark(project: Reference): Initialize[Task[Unit]] = mark(baseDirectory in project)
-	final def mark(baseKey: SettingKey[File]): Initialize[Task[Unit]] = baseKey map { base =>
-		val toMark = base / "ran"
-		if(toMark.exists)
-			error("Already ran (" + toMark + " exists)")
-		else
-			IO touch toMark
-	}
+  override def trigger = allRequirements
+  override def requires = sbt.plugins.JvmPlugin
+  object autoImport {
+    final lazy val Mark = TaskKey[Unit]("mark")
+    final def mark: Initialize[Task[Unit]] = mark(baseDirectory)
+    final def mark(project: Reference): Initialize[Task[Unit]] = mark(baseDirectory in project)
+    final def mark(baseKey: SettingKey[File]): Initialize[Task[Unit]] = baseKey map { base =>
+      val toMark = base / "ran"
+      if(toMark.exists)
+        error("Already ran (" + toMark + " exists)")
+      else
+        IO touch toMark
+    }
+  }
 }
