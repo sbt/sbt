d09a164c927eca075df87b427664ff3cf4b2dde2
Eugene Yokota <eed3si9n@gmail.com>
2016-05-05 22:51:42 -0400

    Fix project/binary-plugin

--- sbt/src/sbt-test/project/binary-plugin/changes/define/A.scala
+++ sbt/src/sbt-test/project/binary-plugin/changes/define/A.scala
@@ -3,20 +3,20 @@ package sbttest // you need package http://stackoverflow.com/questions/9822008/
 import sbt._, syntax._, Keys._
 
 object C extends AutoPlugin {
-	object autoImport {
-		object bN extends AutoPlugin {
-			override def trigger = allRequirements
-		}
-		lazy val check = taskKey[Unit]("Checks that the AutoPlugin and Build are automatically added.")			
-	}
+  object autoImport {
+    // object bN extends AutoPlugin {
+    //   override def trigger = allRequirements
+    // }
+    lazy val check = taskKey[Unit]("Checks that the AutoPlugin and Build are automatically added.")
+  }
 }
 
-	import C.autoImport._
+  import C.autoImport._
 
 object A extends AutoPlugin {
-	override def requires = bN
-	override def trigger = allRequirements
-	override def projectSettings = Seq(
-		check := {}
-	)
+  // override def requires = bN
+  override def trigger = allRequirements
+  override def projectSettings = Seq(
+    check := {}
+  )
 }
