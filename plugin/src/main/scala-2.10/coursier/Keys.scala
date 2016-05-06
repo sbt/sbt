265d79b156568dd8f421e3cd244dd1e8940d37ec
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-05-06 13:54:10 +0200

    Cleaning

--- plugin/src/main/scala-2.10/coursier/Keys.scala
+++ plugin/src/main/scala-2.10/coursier/Keys.scala
@@ -7,32 +7,32 @@ import coursier.core.Publication
 import sbt.{ GetClassifiersModule, Resolver, SettingKey, TaskKey }
 
 object Keys {
-  val coursierParallelDownloads = SettingKey[Int]("coursier-parallel-downloads", "")
-  val coursierMaxIterations = SettingKey[Int]("coursier-max-iterations", "")
-  val coursierChecksums = SettingKey[Seq[Option[String]]]("coursier-checksums", "")
-  val coursierArtifactsChecksums = SettingKey[Seq[Option[String]]]("coursier-artifacts-checksums", "")
-  val coursierCachePolicies = SettingKey[Seq[CachePolicy]]("coursier-cache-policies", "")
+  val coursierParallelDownloads = SettingKey[Int]("coursier-parallel-downloads")
+  val coursierMaxIterations = SettingKey[Int]("coursier-max-iterations")
+  val coursierChecksums = SettingKey[Seq[Option[String]]]("coursier-checksums")
+  val coursierArtifactsChecksums = SettingKey[Seq[Option[String]]]("coursier-artifacts-checksums")
+  val coursierCachePolicies = SettingKey[Seq[CachePolicy]]("coursier-cache-policies")
 
-  val coursierVerbosity = SettingKey[Int]("coursier-verbosity", "")
+  val coursierVerbosity = SettingKey[Int]("coursier-verbosity")
 
   val coursierSourceRepositories = SettingKey[Seq[File]]("coursier-source-repositories")
-  val coursierResolvers = TaskKey[Seq[Resolver]]("coursier-resolvers", "")
-  val coursierSbtResolvers = TaskKey[Seq[Resolver]]("coursier-sbt-resolvers", "")
+  val coursierResolvers = TaskKey[Seq[Resolver]]("coursier-resolvers")
+  val coursierSbtResolvers = TaskKey[Seq[Resolver]]("coursier-sbt-resolvers")
 
-  val coursierCache = SettingKey[File]("coursier-cache", "")
+  val coursierCache = SettingKey[File]("coursier-cache")
 
-  val coursierFallbackDependencies = TaskKey[Seq[(Module, String, URL, Boolean)]]("coursier-fallback-dependencies", "")
+  val coursierFallbackDependencies = TaskKey[Seq[(Module, String, URL, Boolean)]]("coursier-fallback-dependencies")
 
-  val coursierProject = TaskKey[Project]("coursier-project", "")
-  val coursierProjects = TaskKey[Seq[Project]]("coursier-projects", "")
-  val coursierPublications = TaskKey[Seq[(String, Publication)]]("coursier-publications", "")
+  val coursierProject = TaskKey[Project]("coursier-project")
+  val coursierProjects = TaskKey[Seq[Project]]("coursier-projects")
+  val coursierPublications = TaskKey[Seq[(String, Publication)]]("coursier-publications")
 
-  val coursierSbtClassifiersModule = TaskKey[GetClassifiersModule]("coursier-sbt-classifiers-module", "")
+  val coursierSbtClassifiersModule = TaskKey[GetClassifiersModule]("coursier-sbt-classifiers-module")
 
-  val coursierConfigurations = TaskKey[Map[String, Set[String]]]("coursier-configurations", "")
+  val coursierConfigurations = TaskKey[Map[String, Set[String]]]("coursier-configurations")
 
-  val coursierResolution = TaskKey[Resolution]("coursier-resolution", "")
-  val coursierSbtClassifiersResolution = TaskKey[Resolution]("coursier-sbt-classifiers-resolution", "")
+  val coursierResolution = TaskKey[Resolution]("coursier-resolution")
+  val coursierSbtClassifiersResolution = TaskKey[Resolution]("coursier-sbt-classifiers-resolution")
 
   val coursierDependencyTree = TaskKey[Unit](
     "coursier-dependency-tree",
