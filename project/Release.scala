638c290e620dc5784a62947097da6922cb4f5d5d
Eugene Yokota <eed3si9n@gmail.com>
2016-05-08 16:45:20 -0400

    Fix launcher publishing

--- project/Release.scala
+++ project/Release.scala
@@ -9,7 +9,10 @@ object Release {
   lazy val deployLauncher = TaskKey[Unit]("deploy-launcher", "Upload the launcher to its traditional location for compatibility with existing scripts.")
 
   def launcherSettings(launcher: TaskKey[File]): Seq[Setting[_]] = Seq(
-    launcherRemotePath := s"${organization.value}/${moduleName.value}/${version.value}/${moduleName.value}.jar",
+    launcherRemotePath := {
+      val organizationPath = organization.value.replaceAll("""\.""", "/")
+      s"$organizationPath/${moduleName.value}/${version.value}/${moduleName.value}.jar"
+    },
     deployLauncher := {
       val repo = bintrayRepo.value
       repo.upload(bintrayPackage.value, version.value, launcherRemotePath.value, launcher.value, sLog.value)
