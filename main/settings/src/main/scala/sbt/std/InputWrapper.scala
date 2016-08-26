6631456efff1d95dfe54a3f52f6a8766ededceb0
eugene yokota <eed3si9n@gmail.com>
2016-08-26 14:44:49 -0400

    Deprecate .value extension method from input tasks (#2709)
    Calling the `.value` method on an input task returns `InputTask[A]`, which is completely unintuitive. I think it's basically a bug in `.value`. This change deprecates the `.value` call on input tasks, in preparation to the removal in sbt 1.0.
    
    In most cases `.evaluated` should be called, which returns `A` by evaluating the task. Just in case `InputTask[A]` is needed, `inputTaskValue` method is now provided.
    
    One of the motivation for doing this now is the deprecation of old operators `<<=`, which forces more users to convert to the `:=` style and potentially run into the `.value` behavior.

--- main/settings/src/main/scala/sbt/std/InputWrapper.scala
+++ main/settings/src/main/scala/sbt/std/InputWrapper.scala
@@ -96,8 +96,13 @@ object InputWrapper {
       val tpe = ts.tree.tpe
       if (tpe <:< c.weakTypeOf[Initialize[Task[T]]])
         InputWrapper.wrapInitTask[T](c)(ts, pos)
-      else if (tpe <:< c.weakTypeOf[Initialize[T]])
+      else if (tpe <:< c.weakTypeOf[Initialize[T]]) {
+        if (c.weakTypeOf[T] <:< c.weakTypeOf[InputTask[_]]) {
+          c.warning(pos, """`value` is deprecated for an input task. Use `evaluated` or `inputTaskValue`.
+                           |See http://www.scala-sbt.org/0.13/docs/Input-Tasks.html for more details.""".stripMargin)
+        }
         InputWrapper.wrapInit[T](c)(ts, pos)
+      }
       else if (tpe <:< c.weakTypeOf[Task[T]])
         InputWrapper.wrapTask[T](c)(ts, pos)
       else if (tpe <:< c.weakTypeOf[InputTask[T]])
@@ -106,6 +111,10 @@ object InputWrapper {
         InputWrapper.wrapInitInputTask[T](c)(ts, pos)
       else
         c.abort(pos, s"Internal sbt error. Unexpected type ${tpe.widen}")
+    }
+  def inputTaskValueMacroImpl[T: c.WeakTypeTag](c: Context): c.Expr[InputTask[T]] =
+    ContextUtil.selectMacroImpl[InputTask[T]](c) { (ts, pos) =>
+      InputWrapper.wrapInit[InputTask[T]](c)(ts, pos)
     }
   def taskValueMacroImpl[T: c.WeakTypeTag](c: Context): c.Expr[Task[T]] =
     ContextUtil.selectMacroImpl[Task[T]](c) { (ts, pos) =>
@@ -147,6 +156,8 @@ sealed abstract class ParserInput[T] {
 sealed abstract class InputEvaluated[T] {
   @compileTimeOnly("`evaluated` can only be used within an input task macro, such as := or Def.inputTask.")
   def evaluated: T = macro InputWrapper.valueMacroImpl[T]
+  @compileTimeOnly("`inputTaskValue` can only be used within an input task macro, such as := or Def.inputTask.")
+  def inputTaskValue: InputTask[T] = macro InputWrapper.inputTaskValueMacroImpl[T]
 }
 sealed abstract class ParserInputTask[T] {
   @compileTimeOnly("`parsed` can only be used within an input task macro, such as := or Def.inputTask.")
