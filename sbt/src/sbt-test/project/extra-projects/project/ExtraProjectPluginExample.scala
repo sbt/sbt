675f70fd8536e7612541a6ec09274e3d743303f5
Eugene Yokota <eed3si9n@gmail.com>
2016-09-14 20:09:16 -0400

    Rename to extraProjects/derivedProjects
    Ref #2717

--- sbt/src/sbt-test/project/extra-projects/project/ExtraProjectPluginExample.scala
+++ sbt/src/sbt-test/project/extra-projects/project/ExtraProjectPluginExample.scala
@@ -1,8 +1,8 @@
 import sbt._
 import Keys._
 
-object BuildExtrasPlugin extends AutoPlugin {
-  override def buildExtras: Seq[Project] =
+object ExtraProjectPluginExample extends AutoPlugin {
+  override def extraProjects: Seq[Project] =
     List("foo", "bar", "baz") map generateProject
 
   def generateProject(id: String): Project =
