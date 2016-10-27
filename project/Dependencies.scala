0d86bbdd0e9e1ca8c18a87de5d43af986d367757
Dale Wijnand <dale.wijnand@gmail.com>
2016-10-27 12:05:29 +0100

    Add props to define source dependency on io
    Use either -Dsbtio.path on the command line
    or sbtio.path= in project/local.properties

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,26 @@ object Dependencies {
   lazy val scala211 = "2.11.8"
   lazy val scala212 = "2.12.0-M4"
 
-  lazy val sbtIO = "org.scala-sbt" %% "io" % "1.0.0-M6"
+  private lazy val sbtIO = "org.scala-sbt" %% "io" % "1.0.0-M6"
+
+  def getSbtModulePath(key: String, name: String) = {
+    val localProps = new java.util.Properties()
+    IO.load(localProps, file("project/local.properties"))
+    val path = Option(localProps getProperty key) orElse (sys.props get key)
+    path foreach (f => println(s"Using $name from $f"))
+    path
+  }
+
+  lazy val sbtIoPath = getSbtModulePath("sbtio.path", "sbt/io")
+
+  def addSbtModule(p: Project, path: Option[String], projectName: String, m: ModuleID, c: Option[Configuration] = None) =
+    path match {
+      case Some(f) => p dependsOn c.fold[ClasspathDependency](ProjectRef(file(f), projectName))(ProjectRef(file(f), projectName) % _)
+      case None    => p settings (libraryDependencies += c.fold(m)(m % _))
+    }
+
+  def addSbtIO(p: Project): Project = addSbtModule(p, sbtIoPath, "io", sbtIO)
+
   lazy val jline = "jline" % "jline" % "2.13"
 
   lazy val scalaCompiler = Def.setting { "org.scala-lang" % "scala-compiler" % scalaVersion.value }
