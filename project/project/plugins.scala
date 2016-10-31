1148f6f350ded207a77fe9460083289e71426137
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2016-10-31 21:30:31 +0100

    Upgrade native-packager

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -3,13 +3,10 @@ import Keys._
 
 object PluginBuild extends Build {
   override def projects = Seq(root)
-  
+
   val root = Project("root", file(".")) settings(
     resolvers += Resolver.url("scalasbt", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
-    addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.1.1"),
+    addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.1.4"),
     libraryDependencies += "net.databinder" %% "dispatch-http" % "0.8.10"
-  ) //dependsOn(nativePackager)
-
-
-  //lazy val nativePackager = uri("file:///C:/projects/sbt-native-packager")
+  )
 }
