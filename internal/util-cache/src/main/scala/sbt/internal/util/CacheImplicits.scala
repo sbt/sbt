ab08e1a9d551fb1d647f0486cfdbc2427f5244a9
Dale Wijnand <dale.wijnand@gmail.com>
2016-11-24 15:48:47 +0000

    Add back additional formats.
    At least a subset of these are required for sbt/sbt to migrate away from
    sbinary.
    
    This reverts commit cee43575ce3988b1565489db3166281cd7899cde.

--- internal/util-cache/src/main/scala/sbt/internal/util/CacheImplicits.scala
+++ internal/util-cache/src/main/scala/sbt/internal/util/CacheImplicits.scala
@@ -4,3 +4,8 @@ import sjsonnew.BasicJsonProtocol
 
 object CacheImplicits extends BasicCacheImplicits
   with BasicJsonProtocol
+  with FileFormat
+  with HListFormat
+  with URIFormat
+  with URLFormat
+  with StreamFormat
