b6fc9f95172138cc7741f4b0cec963f8a5f17236
Eugene Yokota <eed3si9n@gmail.com>
2017-02-07 15:26:33 -0500

    Put copyright notice
    Fixes #222

--- internal/zinc-ivy-integration/src/main/scala/sbt/internal/inc/ComponentCompiler.scala
+++ internal/zinc-ivy-integration/src/main/scala/sbt/internal/inc/ComponentCompiler.scala
@@ -1,6 +1,10 @@
-/* sbt -- Simple Build Tool
- * Copyright 2009, 2010  Mark Harrah
+/*
+ * Zinc - The incremental compiler for Scala.
+ * Copyright 2011 - 2017, Lightbend, Inc.
+ * Copyright 2008 - 2010, Mark Harrah
+ * This software is released under the terms written in LICENSE.
  */
+
 package sbt
 package internal
 package inc
