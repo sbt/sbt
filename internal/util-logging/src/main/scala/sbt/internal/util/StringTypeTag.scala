a12045ed4282b26179584593bf553a06b42ad1cc
Eugene Yokota <eed3si9n@gmail.com>
2017-02-15 18:52:04 -0500

    some change for Scala 2.10

--- internal/util-logging/src/main/scala/sbt/internal/util/StringTypeTag.scala
+++ internal/util-logging/src/main/scala/sbt/internal/util/StringTypeTag.scala
@@ -18,10 +18,10 @@ object StringTypeTag {
     }
   def typeToString(tpe: Type): String =
     tpe match {
-      case ref: TypeRef =>
-        if (ref.args.nonEmpty) {
-          val typeCon = ref.typeConstructor.typeSymbol.asType.fullName
-          val typeArgs = ref.typeArgs map typeToString
+      case TypeRef(_, sym, args) =>
+        if (args.nonEmpty) {
+          val typeCon = tpe.typeSymbol.fullName
+          val typeArgs = args map typeToString
           s"""$typeCon[${typeArgs.mkString(",")}]"""
         } else tpe.toString
       case _ => tpe.toString
