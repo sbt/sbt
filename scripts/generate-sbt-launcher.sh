630a780487d662dd994ed1c3246895a22c00cf21
Alexandre Archambault <alexandre.archambault@gmail.com>
2017-02-28 13:14:07 +0100

    Add custom sbt launcher

--- scripts/generate-sbt-launcher.sh
+++ scripts/generate-sbt-launcher.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-COURSIER_VERSION=1.0.0-SNAPSHOT
+COURSIER_VERSION=1.0.0-M15-2
 
 "$(dirname "$0")/../coursier" bootstrap \
   "io.get-coursier:sbt-launcher_2.12:$COURSIER_VERSION" \
+  -r sonatype:releases \
   -i launcher \
   -I launcher:org.scala-sbt:launcher-interface:1.0.0 \
   -o csbt \
