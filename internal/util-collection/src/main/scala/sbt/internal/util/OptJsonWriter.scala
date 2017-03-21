bbeecae0b1d701629321c69e79feb9b86c433441
Dale Wijnand <dale.wijnand@gmail.com>
2017-03-21 00:09:07 +0000

    Define OptJsonWriter & put it on AttributeKey

---
+++ internal/util-collection/src/main/scala/sbt/internal/util/OptJsonWriter.scala
@@ -0,0 +1,14 @@
+package sbt.internal.util
+
+import sjsonnew.JsonWriter
+
+sealed trait OptJsonWriter[A]
+final case class NoJsonWriter[A]() extends OptJsonWriter[A]
+final case class SomeJsonWriter[A](value: JsonWriter[A]) extends OptJsonWriter[A]
+
+trait OptJsonWriter0 {
+  implicit def fallback[A]: NoJsonWriter[A] = NoJsonWriter()
+}
+object OptJsonWriter extends OptJsonWriter0 {
+  implicit def lift[A](implicit z: JsonWriter[A]): SomeJsonWriter[A] = SomeJsonWriter(z)
+}
