ade87bc78f1bdd30da876ea3210d727de1383696
Alexandre Archambault <alexandre.archambault@gmail.com>
2017-04-24 20:46:22 +0200

    Update script

--- scripts/generate-sbt-launcher.sh
+++ scripts/generate-sbt-launcher.sh
@@ -6,7 +6,9 @@ COURSIER_VERSION=1.0.0-M15-2
 "$(dirname "$0")/../coursier" bootstrap \
   "io.get-coursier:sbt-launcher_2.12:$COURSIER_VERSION" \
   -r sonatype:releases \
+  --no-default \
   -i launcher \
   -I launcher:org.scala-sbt:launcher-interface:1.0.0 \
   -o csbt \
-  -J -Djline.shutdownhook=false
+  -J -Djline.shutdownhook=false \
+  "$@"
