ecf14d6ad55e98d20e47a34eb6835e8a31c3a94e
James Koch <jameskoch@gmail.com>
2017-04-30 13:08:44 -0400

    Issue #3154 - ScalaTest nested suites reported in JUnit XML reports as "(It is not a test)".

--- sbt/src/sbt-test/tests/junit-xml-report/project/JUnitXmlReportTest.scala
+++ sbt/src/sbt-test/tests/junit-xml-report/project/JUnitXmlReportTest.scala
@@ -8,34 +8,61 @@ object JUnitXmlReportTest extends Build {
 	val checkReport = taskKey[Unit]("Check the test reports")
 	val checkNoReport = taskKey[Unit]("Check that no reports are present")
 
-	private val oneSecondReportFile = "target/test-reports/a.pkg.OneSecondTest.xml"
-	private val failingReportFile = "target/test-reports/another.pkg.FailingTest.xml"
+	private object JUnit {
+		val oneSecondReportFile = "target/test-reports/a.pkg.OneSecondTest.xml"
+		val failingReportFile = "target/test-reports/another.pkg.FailingTest.xml"
+	}
+
+	private object ScalaTest {
+		val flatSuiteReportFile = "target/test-reports/my.scalatest.MyFlatSuite.xml"
+		val nestedSuitesReportFile = "target/test-reports/my.scalatest.MyNestedSuites.xml"
+	}
 
 	lazy val root = Project("root", file("."), settings = defaultSettings ++ Seq(
-		scalaVersion := "2.9.2",
-		libraryDependencies += "com.novocode" % "junit-interface" % "0.10" % "test",
+		scalaVersion := "2.11.8",
+		libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test",
 
 		// TODO use matchers instead of sys.error
 		checkReport := {
-			val oneSecondReport = XML.loadFile(oneSecondReportFile)
+			val oneSecondReport = XML.loadFile(JUnit.oneSecondReportFile)
 			if( oneSecondReport.label != "testsuite" ) sys.error("Report should have a root <testsuite> element.")
 			// somehow the 'success' event doesn't go through... TODO investigate
 //			if( (oneSecondReport \ "@time").text.toFloat < 1f ) sys.error("expected test to take at least 1 sec")
-			if( (oneSecondReport \ "@name").text != "a.pkg.OneSecondTest" ) sys.error("wrong test name: " + (oneSecondReport \ "@name").text)
+			if( (oneSecondReport \ "@name").text != "a.pkg.OneSecondTest" ) sys.error("wrong fixture name: " + (oneSecondReport \ "@name").text)
 			// TODO more checks
 
-			val failingReport = XML.loadFile(failingReportFile)
+			val failingReport = XML.loadFile(JUnit.failingReportFile)
 			if( failingReport.label != "testsuite" ) sys.error("Report should have a root <testsuite> element.")
 			if( (failingReport \ "@failures").text != "2" ) sys.error("expected 2 failures")
-			if( (failingReport \ "@name").text != "another.pkg.FailingTest" ) sys.error("wrong test name: " + (failingReport \ "@name").text)
+			if( (failingReport \ "@name").text != "another.pkg.FailingTest" ) sys.error("wrong fixture name: " + (failingReport \ "@name").text)
 			// TODO more checks -> the two test cases with time etc..
+
+			val scalaTestFlatReport = XML.loadFile(ScalaTest.flatSuiteReportFile)
+			if( scalaTestFlatReport.label != "testsuite" ) sys.error("Report should have a root <testsuite> element.")
+			if( (scalaTestFlatReport \ "@tests").text != "2" ) sys.error("expected 2 tests")
+			if( (scalaTestFlatReport \ "@failures").text != "1" ) sys.error("expected 1 failures")
+			if( (scalaTestFlatReport \ "@name").text != "my.scalatest.MyFlatSuite" ) sys.error("wrong fixture name: " + (scalaTestFlatReport \ "@name").text)
+
+			val nestedSuitesReport = XML.loadFile(ScalaTest.nestedSuitesReportFile)
+			if( nestedSuitesReport.label != "testsuite" ) sys.error("Report should have a root <testsuite> element.")
+			if( (nestedSuitesReport \ "@tests").text != "2" ) sys.error("expected 2 tests")
+			if( (nestedSuitesReport \ "@failures").text != "1" ) sys.error("expected 1 failures")
+			if( (nestedSuitesReport \ "@name").text != "my.scalatest.MyNestedSuites" ) sys.error("wrong fixture name: " + (nestedSuitesReport \ "@name").text)
+			val actualTestName = (nestedSuitesReport \ "testcase").map(t => (t \ "@name").text)
+			if( actualTestName.toSet != Set("MyInnerSuite.Inner passing test should pass", "MyInnerSuite.Inner failing test should fail")) sys.error(s"wrong test names: ${actualTestName.mkString(", ")}")
 
 			// TODO check console output is in the report
 		},
 
 		checkNoReport := {
-			if( file(oneSecondReportFile).exists() ) sys.error(oneSecondReportFile + " should not exist")
-			if( file(failingReportFile).exists() ) sys.error(failingReportFile + " should not exist")
+			for (f <- Seq(
+				JUnit.oneSecondReportFile,
+				JUnit.failingReportFile,
+				ScalaTest.flatSuiteReportFile,
+				ScalaTest.nestedSuitesReportFile
+			)) {
+				if( file(f).exists() ) sys.error(f + " should not exist")
+			}
 		}
 	))
 }
