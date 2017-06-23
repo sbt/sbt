4cbad9eab1853beaa19df49080b5f44bc229c905
Dale Wijnand <dale.wijnand@gmail.com>
2017-06-23 17:45:36 +0100

    Bring back collection, complete & logic
    Recovers these as they were at the time of their deletion:
    
        git checkout 4053ea92ba8b4978c583fe6ec5328843174a94e7 \
          util/collection util/complete util/logic

---
+++ util/complete/src/test/scala/sbt/complete/FixedSetExamplesTest.scala
@@ -0,0 +1,26 @@
+package sbt.complete
+
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+
+class FixedSetExamplesTest extends Specification {
+
+  "adding a prefix" should {
+    "produce a smaller set of examples with the prefix removed" in new examples {
+      fixedSetExamples.withAddedPrefix("f")() must containTheSameElementsAs(List("oo", "ool", "u"))
+      fixedSetExamples.withAddedPrefix("fo")() must containTheSameElementsAs(List("o", "ol"))
+      fixedSetExamples.withAddedPrefix("b")() must containTheSameElementsAs(List("ar"))
+    }
+  }
+
+  "without a prefix" should {
+    "produce the original set" in new examples {
+      fixedSetExamples() mustEqual exampleSet
+    }
+  }
+
+  trait examples extends Scope {
+    val exampleSet = List("foo", "bar", "fool", "fu")
+    val fixedSetExamples = FixedSetExamples(exampleSet)
+  }
+}
---
+++ util/logic/src/test/scala/sbt/logic/Test.scala
@@ -0,0 +1,115 @@
+package sbt
+package logic
+
+import org.scalacheck._
+import Prop.secure
+import Logic.{ LogicException, Matched }
+
+object LogicTest extends Properties("Logic") {
+  import TestClauses._
+
+  property("Handles trivial resolution.") = secure(expect(trivial, Set(A)))
+  property("Handles less trivial resolution.") = secure(expect(lessTrivial, Set(B, A, D)))
+  property("Handles cycles without negation") = secure(expect(cycles, Set(F, A, B)))
+  property("Handles basic exclusion.") = secure(expect(excludedPos, Set()))
+  property("Handles exclusion of head proved by negation.") = secure(expect(excludedNeg, Set()))
+  // TODO: actually check ordering, probably as part of a check that dependencies are satisifed
+  property("Properly orders results.") = secure(expect(ordering, Set(B, A, C, E, F)))
+  property("Detects cyclic negation") = secure(
+    Logic.reduceAll(badClauses, Set()) match {
+      case Right(res)                      => false
+      case Left(err: Logic.CyclicNegation) => true
+      case Left(err)                       => sys.error(s"Expected cyclic error, got: $err")
+    }
+  )
+
+  def expect(result: Either[LogicException, Matched], expected: Set[Atom]) = result match {
+    case Left(err) => false
+    case Right(res) =>
+      val actual = res.provenSet
+      (actual == expected) || sys.error(s"Expected to prove $expected, but actually proved $actual")
+  }
+}
+
+object TestClauses {
+
+  val A = Atom("A")
+  val B = Atom("B")
+  val C = Atom("C")
+  val D = Atom("D")
+  val E = Atom("E")
+  val F = Atom("F")
+  val G = Atom("G")
+
+  val clauses =
+    A.proves(B) ::
+      A.proves(F) ::
+      B.proves(F) ::
+      F.proves(A) ::
+      (!C).proves(F) ::
+      D.proves(C) ::
+      C.proves(D) ::
+      Nil
+
+  val cycles = Logic.reduceAll(clauses, Set())
+
+  val badClauses =
+    A.proves(D) ::
+      clauses
+
+  val excludedNeg = {
+    val cs =
+      (!A).proves(B) ::
+        Nil
+    val init =
+      (!A) ::
+        (!B) ::
+        Nil
+    Logic.reduceAll(cs, init.toSet)
+  }
+
+  val excludedPos = {
+    val cs =
+      A.proves(B) ::
+        Nil
+    val init =
+      A ::
+        (!B) ::
+        Nil
+    Logic.reduceAll(cs, init.toSet)
+  }
+
+  val trivial = {
+    val cs =
+      Formula.True.proves(A) ::
+        Nil
+    Logic.reduceAll(cs, Set.empty)
+  }
+
+  val lessTrivial = {
+    val cs =
+      Formula.True.proves(A) ::
+        Formula.True.proves(B) ::
+        (A && B && (!C)).proves(D) ::
+        Nil
+    Logic.reduceAll(cs, Set())
+  }
+
+  val ordering = {
+    val cs =
+      E.proves(F) ::
+        (C && !D).proves(E) ::
+        (A && B).proves(C) ::
+        Nil
+    Logic.reduceAll(cs, Set(A, B))
+  }
+
+  def all(): Unit = {
+    println(s"Cycles: $cycles")
+    println(s"xNeg: $excludedNeg")
+    println(s"xPos: $excludedPos")
+    println(s"trivial: $trivial")
+    println(s"lessTrivial: $lessTrivial")
+    println(s"ordering: $ordering")
+  }
+}
