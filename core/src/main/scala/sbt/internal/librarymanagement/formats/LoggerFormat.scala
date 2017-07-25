93c06d29b1f84eece613c51273438fa9dec04a99
Eugene Yokota <eed3si9n@gmail.com>
2017-07-25 16:58:40 -0400

    Implement better fake formats than ???
    Fixes sbt/librarymanagement#67
    Fixes sbt/sbt#3288

--- core/src/main/scala/sbt/internal/librarymanagement/formats/LoggerFormat.scala
+++ core/src/main/scala/sbt/internal/librarymanagement/formats/LoggerFormat.scala
@@ -2,7 +2,15 @@ package sbt.internal.librarymanagement.formats
 
 import sjsonnew._
 import xsbti._
+import sbt.util.Logger.Null
 
+/**
+ * A fake JsonFormat for xsbti.Logger.
+ * This is mostly for making IvyConfiguration serializable to JSON.
+ */
 trait LoggerFormat { self: BasicJsonProtocol =>
-  implicit lazy val LoggerFormat: JsonFormat[Logger] = ???
+  implicit lazy val xsbtiLoggerIsoString: IsoString[Logger] =
+    IsoString.iso(_ => "<logger>", _ => Null)
+
+  implicit lazy val LoggerFormat: JsonFormat[Logger] = implicitly
 }
