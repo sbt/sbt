c9fc72c61056f1c4391a73a0150da5ab3be9a8aa
Eugene Yokota <eed3si9n@gmail.com>
2017-07-25 13:02:24 -0400

    Format enforcement

--- main/settings/src/main/scala/sbt/sbtUnchecked.scala
+++ main/settings/src/main/scala/sbt/sbtUnchecked.scala
@@ -2,7 +2,8 @@ package sbt
 
 import scala.annotation.Annotation
 
-/** An annotation to designate that the annotated entity
+/**
+ * An annotation to designate that the annotated entity
  *  should not be considered for additional sbt compiler checks.
  *  These checks ensure that the DSL is predictable and prevents
  *  users from doing dangerous things at the cost of a stricter
