8f912301be4d6e7bda6188fc8ec5ce51911d161d
Dale Wijnand <dale.wijnand@gmail.com>
2017-07-28 07:56:02 +0100

    Upgrade to Scala 2.12.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.contraband.ContrabandPlugin.autoImport._
 
 object Dependencies {
   val scala211 = "2.11.11"
-  val scala212 = "2.12.2"
+  val scala212 = "2.12.3"
 
   private val ioVersion = "1.0.0-M13"
   private val utilVersion = "1.0.0-M27"
