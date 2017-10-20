f644936755e4c39f969b75c869cbe155403950a3
jvican <jorgevc@fastmail.es>
2017-10-20 23:37:55 +0200

    Enable parallel execution of scripted in the plugin
    The change to enable batched and parallel execution for scripted was
    done only for the scripted-sbt project. This pull request enables it for
    scripted-plugin, so that all sbt plugins in 1.x. can benefit from it.
    
    By default, it configures a number of parallel instances of 1 and batch
    execution is disabled. Users can change the number of parallel sbt hosts
    running scripted tests via the `scriptedParallelInstances` setting.
    
    In some plugins scripted tests', batch execution can cause issues
    because the first time `>` commands are executed they assume sbt starts
    up. This error can be fixed by doing `reload` before running the `>`
    command.
    
    Note that the current scripted plugin does not allow parallel execution
    in non-batched mode.

--- scripted/plugin/src/main/scala/sbt/ScriptedPlugin.scala
+++ scripted/plugin/src/main/scala/sbt/ScriptedPlugin.scala
@@ -23,6 +23,11 @@ object ScriptedPlugin extends AutoPlugin {
     val scriptedBufferLog = SettingKey[Boolean]("scripted-buffer-log")
     val scriptedClasspath = TaskKey[PathFinder]("scripted-classpath")
     val scriptedTests = TaskKey[AnyRef]("scripted-tests")
+    val scriptedBatchExecution =
+      settingKey[Boolean]("Enables or disables batch execution for scripted.")
+    val scriptedParallelInstances =
+      settingKey[Int](
+        "Configures the number of scripted instances for parallel testing, only used in batch mode.")
     val scriptedRun = TaskKey[Method]("scripted-run")
     val scriptedLaunchOpts = SettingKey[Seq[String]](
       "scripted-launch-opts",
@@ -51,6 +56,8 @@ object ScriptedPlugin extends AutoPlugin {
     scriptedBufferLog := true,
     scriptedClasspath := getJars(ScriptedConf).value,
     scriptedTests := scriptedTestsTask.value,
+    scriptedParallelInstances := 1,
+    scriptedBatchExecution := false,
     scriptedRun := scriptedRunTask.value,
     scriptedDependencies := {
       def use[A](x: A*): Unit = () // avoid unused warnings
@@ -68,15 +75,18 @@ object ScriptedPlugin extends AutoPlugin {
       ModuleUtilities.getObject("sbt.test.ScriptedTests", loader)
     }
 
-  def scriptedRunTask: Initialize[Task[Method]] = Def.task(
-    scriptedTests.value.getClass.getMethod("run",
-                                           classOf[File],
-                                           classOf[Boolean],
-                                           classOf[Array[String]],
-                                           classOf[File],
-                                           classOf[Array[String]],
-                                           classOf[java.util.List[File]])
-  )
+  private[this] val fCls = classOf[File]
+  private[this] val bCls = classOf[Boolean]
+  private[this] val asCls = classOf[Array[String]]
+  private[this] val lfCls = classOf[java.util.List[File]]
+  private[this] val iCls = classOf[Integer]
+
+  def scriptedRunTask: Initialize[Task[Method]] = Def.taskDyn {
+    val clazz = scriptedTests.value.getClass
+    if (scriptedBatchExecution.value)
+      Def.task(clazz.getMethod("runInParallel", fCls, bCls, asCls, fCls, asCls, lfCls, iCls))
+    else Def.task(clazz.getMethod("run", fCls, bCls, asCls, fCls, asCls, lfCls))
+  }
 
   import DefaultParsers._
   case class ScriptedTestPage(page: Int, total: Int)
@@ -129,15 +139,18 @@ object ScriptedPlugin extends AutoPlugin {
     val args = scriptedParser(sbtTestDirectory.value).parsed
     scriptedDependencies.value
     try {
-      scriptedRun.value.invoke(
-        scriptedTests.value,
-        sbtTestDirectory.value,
-        scriptedBufferLog.value: java.lang.Boolean,
-        args.toArray,
-        sbtLauncher.value,
-        scriptedLaunchOpts.value.toArray,
-        new java.util.ArrayList()
-      )
+      val method = scriptedRun.value
+      val scriptedInstance = scriptedTests.value
+      val dir = sbtTestDirectory.value
+      val log: java.lang.Boolean = scriptedBufferLog.value
+      val launcher = sbtLauncher.value
+      val opts = scriptedLaunchOpts.value.toArray
+      val empty = new java.util.ArrayList()
+      val instances: Integer = scriptedParallelInstances.value
+
+      if (scriptedBatchExecution.value)
+        method.invoke(scriptedInstance, dir, log, args.toArray, launcher, opts, empty, instances)
+      else method.invoke(scriptedInstance, dir, log, args.toArray, launcher, opts, empty)
     } catch { case e: java.lang.reflect.InvocationTargetException => throw e.getCause }
   }
 
