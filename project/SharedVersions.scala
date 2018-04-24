07989f00406f0b9c0d539197de88a31942f28d51
Denys Shabalin <den.shabalin@gmail.com>
2018-04-24 11:42:14 +0200

    Upgrade to Scala Native 0.3.7 (#849)

--- project/SharedVersions.scala
+++ project/SharedVersions.scala
@@ -4,7 +4,7 @@ object SharedVersions {
   def asm = "5.2"
   def fastParse = "1.0.0"
   def proguard = "5.3.3"
-  def scalaNative = "0.3.6"
+  def scalaNative = "0.3.7"
   def scalaz = "7.2.20"
 
 }
