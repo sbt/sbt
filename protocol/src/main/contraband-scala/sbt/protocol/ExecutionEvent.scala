4560384a890e0e7663fe74988aa1960ca0d48dae
Dale Wijnand <dale.wijnand@gmail.com>
2018-05-11 09:11:16 -0700

    add foo

--- protocol/src/main/contraband-scala/sbt/protocol/ExecutionEvent.scala
+++ protocol/src/main/contraband-scala/sbt/protocol/ExecutionEvent.scala
@@ -7,22 +7,23 @@ package sbt.protocol
 /** Execution event. */
 final class ExecutionEvent private (
   val success: String,
-  val commandLine: String) extends Serializable {
+  val commandLine: String,
+  val foo: Option[String]) extends Serializable {
   
   
   
   override def equals(o: Any): Boolean = o match {
-    case x: ExecutionEvent => (this.success == x.success) && (this.commandLine == x.commandLine)
+    case x: ExecutionEvent => (this.success == x.success) && (this.commandLine == x.commandLine) && (this.foo == x.foo)
     case _ => false
   }
   override def hashCode: Int = {
-    37 * (37 * (37 * (17 + "sbt.protocol.ExecutionEvent".##) + success.##) + commandLine.##)
+    37 * (37 * (37 * (37 * (17 + "sbt.protocol.ExecutionEvent".##) + success.##) + commandLine.##) + foo.##)
   }
   override def toString: String = {
-    "ExecutionEvent(" + success + ", " + commandLine + ")"
+    "ExecutionEvent(" + success + ", " + commandLine + ", " + foo + ")"
   }
-  private[this] def copy(success: String = success, commandLine: String = commandLine): ExecutionEvent = {
-    new ExecutionEvent(success, commandLine)
+  private[this] def copy(success: String = success, commandLine: String = commandLine, foo: Option[String] = foo): ExecutionEvent = {
+    new ExecutionEvent(success, commandLine, foo)
   }
   def withSuccess(success: String): ExecutionEvent = {
     copy(success = success)
@@ -30,8 +31,15 @@ final class ExecutionEvent private (
   def withCommandLine(commandLine: String): ExecutionEvent = {
     copy(commandLine = commandLine)
   }
+  def withFoo(foo: Option[String]): ExecutionEvent = {
+    copy(foo = foo)
+  }
+  def withFoo(foo: String): ExecutionEvent = {
+    copy(foo = Option(foo))
+  }
 }
 object ExecutionEvent {
   
-  def apply(success: String, commandLine: String): ExecutionEvent = new ExecutionEvent(success, commandLine)
+  def apply(success: String, commandLine: String, foo: Option[String]): ExecutionEvent = new ExecutionEvent(success, commandLine, foo)
+  def apply(success: String, commandLine: String, foo: String): ExecutionEvent = new ExecutionEvent(success, commandLine, Option(foo))
 }
