b1d02bee3034d70ee7aba138317a0b15fdeb4b35
Dale Wijnand <dale.wijnand@gmail.com>
2018-08-02 07:52:13 +0100

    Make checkTypeTag lenient on "scala." type prefix

--- internal/util-logging/src/test/scala/LogExchangeSpec.scala
+++ internal/util-logging/src/test/scala/LogExchangeSpec.scala
@@ -11,6 +11,17 @@ class LogExchangeSpec extends FlatSpec with Matchers {
   checkTypeTag("stringTypeTagTraceEvent", stringTypeTagTraceEvent, StringTypeTag[TraceEvent])
   checkTypeTag("stringTypeTagSuccessEvent", stringTypeTagSuccessEvent, StringTypeTag[SuccessEvent])
 
-  private def checkTypeTag[A, B](name: String, actual: A, expected: B): Unit =
-    s"LogExchange.$name" should s"match real StringTypeTag[$expected]" in assert(actual == expected)
+  private def checkTypeTag[A](name: String, inc: StringTypeTag[A], exp: StringTypeTag[A]): Unit =
+    s"LogExchange.$name" should s"match real StringTypeTag[$exp]" in {
+      val StringTypeTag(incomingString) = inc
+      val StringTypeTag(expectedString) = exp
+      if ((incomingString startsWith "scala.") || (expectedString startsWith "scala.")) {
+        // > historically [Scala] has been inconsistent whether `scala.` is included, or not
+        // > would it be hard to make the test accept either result?
+        // https://github.com/scala/community-builds/pull/758#issuecomment-409760633
+        assert((incomingString stripPrefix "scala.") == (expectedString stripPrefix "scala."))
+      } else {
+        assert(incomingString == expectedString)
+      }
+    }
 }
