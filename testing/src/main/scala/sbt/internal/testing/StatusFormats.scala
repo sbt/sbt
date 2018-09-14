4ff4f6e45e7934ab4331cb8fa8b7dbe4079f2d80
Eugene Yokota <eed3si9n@gmail.com>
2018-09-14 03:40:39 -0400

    Update header

--- testing/src/main/scala/sbt/internal/testing/StatusFormats.scala
+++ testing/src/main/scala/sbt/internal/testing/StatusFormats.scala
@@ -1,8 +1,8 @@
 /*
  * sbt
- * Copyright 2011 - 2017, Lightbend, Inc.
+ * Copyright 2011 - 2018, Lightbend, Inc.
  * Copyright 2008 - 2010, Mark Harrah
- * Licensed under BSD-3-Clause license (see LICENSE)
+ * Licensed under Apache License 2.0 (see LICENSE)
  */
 
 package sbt.internal.testing
