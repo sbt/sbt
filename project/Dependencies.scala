ab674321dce1439f80593ba58186346c2934c1a8
Eugene Yokota <eed3si9n@gmail.com>
2018-09-28 02:27:45 -0400

    Scala 2.12.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt.contraband.ContrabandPlugin.autoImport._
 object Dependencies {
   val scala210 = "2.10.7"
   val scala211 = "2.11.12"
-  val scala212 = "2.12.6"
+  val scala212 = "2.12.7"
 
   private val ioVersion = "1.2.1"
 
