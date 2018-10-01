96ae126866c7517fc94d60624295eedb22aed6ab
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-10-01 16:55:27 +0200

    Use handmade metadata from separate repo

--- scripts/start-it-auth-server.ps1
+++
@@ -1,3 +0,0 @@
-# see https://stackoverflow.com/questions/2224350/powershell-start-job-working-directory/2246542#2246542
-Set-Location $args[0]
-& java -jar -noverify coursier launch io.get-coursier:http-server_2.12:1.0.0 -- -d tests/jvm/src/test/resources/test-repo/http/abc.com -u $args[3] -P $args[4] -r realm --host $args[1] --port $args[2] -v
