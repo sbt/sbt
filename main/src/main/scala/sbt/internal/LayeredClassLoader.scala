ef08290eccf45b0701c58453abfb2a07c4577d9b
Ethan Atkins <ethan.atkins@gmail.com>
2018-11-24 12:01:34 -0800

    Add ClassLoaderCache
    In order to speed up the start up time of the test and run tasks, I'm
    introducing a ClassLoaderCache that can be used to avoid reloading the
    classes in the project dependencies (which includes the scala library).
    I made the api as minimal as possible so that we can iterate on the
    implementation without breaking binary compatibility. This feature will
    be gated on a feature flag, so I'm not concerned with the cache class
    loaders being useable in every user configuration. Over time, I hope
    that the CachedClassLoaders will be a drop in replacement for the
    existing one-off class loaders*.
    
    The LayeredClassLoader was adapted from the NativeCopyLoader. The main
    difference is that the NativeCopyLoader extracts named shared libraries
    into the task temp directory to ensure that the ephemeral libraries are
    deleted after each task run. This is a problem if we are caching the
    ClassLoader so for LayeredClassLoader I track the native libraries that
    are extracted by the loader and I delete them either when the loader is
    explicitly closed or in a shutdown hook.
    
    * This of course means that we both must layer the class loaders
    appropriately so that the project code is in a layer above the cached
    loaders and we must correctly invalidate the cache when the project, or
    its dependencies are updated.

---
+++ main/src/main/scala/sbt/internal/LayeredClassLoader.scala
@@ -0,0 +1,68 @@
+/*
+ * sbt
+ * Copyright 2011 - 2018, Lightbend, Inc.
+ * Copyright 2008 - 2010, Mark Harrah
+ * Licensed under Apache License 2.0 (see LICENSE)
+ */
+
+package sbt.internal
+
+import java.io.File
+import java.net.URLClassLoader
+import java.{ util => jutil }
+import scala.collection.JavaConverters._
+
+import sbt.internal.inc.classpath._
+import sbt.io.IO
+
+private[sbt] class LayeredClassLoader(
+    classpath: Seq[File],
+    parent: ClassLoader,
+    override protected val resources: Map[String, String],
+    tempDir: File,
+) extends URLClassLoader(classpath.toArray.map(_.toURI.toURL), parent)
+    with RawResources
+    with NativeCopyLoader
+    with AutoCloseable {
+  private[this] val nativeLibs = new jutil.HashSet[File]().asScala
+  override protected val config = new NativeCopyConfig(
+    tempDir,
+    classpath,
+    IO.parseClasspath(System.getProperty("java.library.path", ""))
+  )
+  override def findLibrary(name: String): String = {
+    super.findLibrary(name) match {
+      case null => null
+      case l =>
+        nativeLibs += new File(l)
+        l
+    }
+  }
+  override def close(): Unit = nativeLibs.foreach(NativeLibs.delete)
+  override def toString: String = s"""LayeredClassLoader(
+  |  classpath =
+  |    ${classpath mkString "\n    "}
+  |  parent =
+  |    ${parent.toString.lines.mkString("\n    ")}
+  |)""".stripMargin
+}
+
+private[internal] object NativeLibs {
+  private[this] val nativeLibs = new jutil.HashSet[File].asScala
+  Runtime.getRuntime.addShutdownHook(new Thread("sbt.internal.native-library-deletion") {
+    override def run(): Unit = {
+      nativeLibs.foreach(IO.delete)
+      IO.deleteIfEmpty(nativeLibs.map(_.getParentFile).toSet)
+      nativeLibs.clear()
+    }
+  })
+  def addNativeLib(lib: String): Unit = {
+    nativeLibs.add(new File(lib))
+    ()
+  }
+  def delete(file: File): Unit = {
+    nativeLibs.remove(file)
+    file.delete()
+    ()
+  }
+}
---
+++ main/src/main/scala/sbt/internal/PrettyPrint.scala
@@ -0,0 +1,15 @@
+/*
+ * sbt
+ * Copyright 2011 - 2018, Lightbend, Inc.
+ * Copyright 2008 - 2010, Mark Harrah
+ * Licensed under Apache License 2.0 (see LICENSE)
+ */
+
+package sbt.internal
+
+private[sbt] object PrettyPrint {
+  private[sbt] def indent(any: Any, level: Int): String = {
+    val i = " " * level
+    any.toString.lines.mkString(i, "\n" + i, "")
+  }
+}
