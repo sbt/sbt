cb00cac5a6e073fe32711045947d8c9fae1816b7
Dale Wijnand <dale.wijnand@gmail.com>
2019-01-27 16:31:28 +0000

    Simplify Append using SAM conversion short-hand

--- main-settings/src/main/scala/sbt/Append.scala
+++ main-settings/src/main/scala/sbt/Append.scala
@@ -27,19 +27,14 @@ object Append {
     def appendValues(a: A, b: B): A
   }
 
-  trait Sequence[A, -B, T] extends Value[A, T] with Values[A, B]
+  // NOTE: the implementations below use SAM conversion short-hand to elide the boilerplate.
 
-  implicit def appendSeq[T, V <: T]: Sequence[Seq[T], Seq[V], V] =
-    new Sequence[Seq[T], Seq[V], V] {
-      def appendValues(a: Seq[T], b: Seq[V]): Seq[T] = a ++ b
-      def appendValue(a: Seq[T], b: V): Seq[T] = a :+ b
-    }
+  implicit def appendSeq1[T, V <: T]: Value[Seq[T], V] = _ :+ _
+  implicit def appendSeqN[T, V <: T]: Values[Seq[T], Seq[V]] = _ ++ _
 
-  implicit def appendSeqImplicit[T, V](implicit ev: V => T): Sequence[Seq[T], Seq[V], V] =
-    new Sequence[Seq[T], Seq[V], V] {
-      def appendValues(a: Seq[T], b: Seq[V]): Seq[T] = a ++ b.map(x => (x: T))
-      def appendValue(a: Seq[T], b: V): Seq[T] = a :+ (b: T)
-    }
+  implicit def appendSeqImplicit1[T, V](implicit ev: V => T): Value[Seq[T], V] = _ :+ ev(_)
+  implicit def appendSeqImplicitN[T, V](implicit ev: V => T): Values[Seq[T], Seq[V]] =
+    _ ++ _.map(ev)
 
   @compileTimeOnly("This can be used in += only.")
   implicit def appendTaskValueSeq[T, V <: T]: Value[Seq[Task[T]], Initialize[Task[V]]] =
@@ -48,57 +43,67 @@ object Append {
   @compileTimeOnly("This can be used in += only.")
   implicit def appendTaskKeySeq[T, V <: T]: Value[Seq[Task[T]], TaskKey[V]] = (_, _) => ???
 
-  implicit def appendList[T, V <: T]: Sequence[List[T], List[V], V] =
-    new Sequence[List[T], List[V], V] {
-      def appendValues(a: List[T], b: List[V]): List[T] = a ::: b
-      def appendValue(a: List[T], b: V): List[T] = a :+ b
-    }
+  implicit def appendList1[T, V <: T]: Value[List[T], V] = _ :+ _
+  implicit def appendListN[T, V <: T]: Values[List[T], List[V]] = _ ::: _
 
-  implicit def appendListImplicit[T, V](implicit ev: V => T): Sequence[List[T], List[V], V] =
-    new Sequence[List[T], List[V], V] {
-      def appendValues(a: List[T], b: List[V]): List[T] = a ::: b.map(x => (x: T))
-      def appendValue(a: List[T], b: V): List[T] = a :+ (b: T)
-    }
+  implicit def appendListImplicit1[T, V](implicit ev: V => T): Value[List[T], V] = _ :+ ev(_)
+  implicit def appendListImplicitN[T, V](implicit ev: V => T): Values[List[T], List[V]] =
+    _ ::: _.map(ev)
 
-  implicit def appendVectorImplicit[T, V](implicit ev: V => T): Sequence[Vector[T], Seq[V], V] =
-    new Sequence[Vector[T], Seq[V], V] {
-      def appendValues(a: Vector[T], b: Seq[V]): Vector[T] = a ++ b.map(x => (x: T))
-      def appendValue(a: Vector[T], b: V): Vector[T] = a :+ (b: T)
-    }
+  implicit def appendVectorImplicit1[T, V](implicit ev: V => T): Value[Vector[T], V] = _ :+ ev(_)
+  implicit def appendVectorImplicitN[T, V](implicit ev: V => T): Values[Vector[T], Seq[V]] =
+    _ ++ _.map(ev)
 
   implicit def appendString: Value[String, String] = _ + _
   implicit def appendInt: Value[Int, Int] = _ + _
   implicit def appendLong: Value[Long, Long] = _ + _
   implicit def appendDouble: Value[Double, Double] = _ + _
 
-  implicit def appendClasspath: Sequence[Classpath, Seq[File], File] =
-    new Sequence[Classpath, Seq[File], File] {
-      def appendValues(a: Classpath, b: Seq[File]): Classpath = a ++ Attributed.blankSeq(b)
-      def appendValue(a: Classpath, b: File): Classpath = a :+ Attributed.blank(b)
+  implicit def appendClasspath1: Value[Classpath, File] = _ :+ Attributed.blank(_)
+  implicit def appendClasspathN: Values[Classpath, Seq[File]] = _ ++ Attributed.blankSeq(_)
+
+  implicit def appendSet1[T, V <: T]: Value[Set[T], V] = _ + _
+  implicit def appendSetN[T, V <: T]: Values[Set[T], Set[V]] = _ ++ _
+
+  implicit def appendMap1[A, B, X <: A, Y <: B]: Value[Map[A, B], (X, Y)] = _ + _
+  implicit def appendMapN[A, B, X <: A, Y <: B, M[K, V] <: Map[K, V]]: Values[Map[A, B], M[X, Y]] =
+    _ ++ _
+
+  implicit def appendOption1[T]: Value[Seq[T], Option[T]] = (a, b) => b.fold(a)(a :+ _)
+  implicit def appendOptionN[T]: Values[Seq[T], Option[T]] = (a, b) => b.fold(a)(a :+ _)
+
+  implicit def appendSource1: Value[Seq[Source], File] =
+    _ :+ new Source(_, AllPassFilter, NothingFilter)
+
+  implicit def appendSourceN: Values[Seq[Source], Seq[File]] =
+    _ ++ _.map(new Source(_, AllPassFilter, NothingFilter))
+
+  // un-implicit, package-private (and JVM public) the old way of defining these
+  @deprecated("No longer required with SAM conversions. Scheduled for removal in 2.0.", "1.3.0")
+  private[sbt] trait Sequence[A, -B, T] extends Value[A, T] with Values[A, B]
+
+  private[sbt] def seq[A, B, T](implicit a1: Value[A, T], aN: Values[A, B]): Sequence[A, B, T] =
+    new Sequence[A, B, T] {
+      def appendValue(a: A, b: T): A = a1.appendValue(a, b)
+      def appendValues(a: A, b: B): A = aN.appendValues(a, b)
     }
 
-  implicit def appendSet[T, V <: T]: Sequence[Set[T], Set[V], V] =
-    new Sequence[Set[T], Set[V], V] {
-      def appendValues(a: Set[T], b: Set[V]): Set[T] = a ++ b
-      def appendValue(a: Set[T], b: V): Set[T] = a + b
-    }
-
-  implicit def appendMap[A, B, X <: A, Y <: B]: Sequence[Map[A, B], Map[X, Y], (X, Y)] =
-    new Sequence[Map[A, B], Map[X, Y], (X, Y)] {
-      def appendValues(a: Map[A, B], b: Map[X, Y]): Map[A, B] = a ++ b
-      def appendValue(a: Map[A, B], b: (X, Y)): Map[A, B] = a + b
-    }
-
-  implicit def appendOption[T]: Sequence[Seq[T], Option[T], Option[T]] =
-    new Sequence[Seq[T], Option[T], Option[T]] {
-      def appendValue(a: Seq[T], b: Option[T]): Seq[T] = b.fold(a)(a :+ _)
-      def appendValues(a: Seq[T], b: Option[T]): Seq[T] = b.fold(a)(a :+ _)
-    }
-
-  implicit def appendSource: Sequence[Seq[Source], Seq[File], File] =
-    new Sequence[Seq[Source], Seq[File], File] {
-      def appendValue(a: Seq[Source], b: File): Seq[Source] = appendValues(a, Seq(b))
-      def appendValues(a: Seq[Source], b: Seq[File]): Seq[Source] =
-        a ++ b.map(new Source(_, AllPassFilter, NothingFilter))
-    }
+  private[sbt] def appendSeq[T, V <: T]: Sequence[Seq[T], Seq[V], V] = seq[Seq[T], Seq[V], V]
+  private[sbt] def appendSeqImplicit[T, V](implicit ev: V => T): Sequence[Seq[T], Seq[V], V] =
+    seq[Seq[T], Seq[V], V]
+  private[sbt] def appendList[T, V <: T]: Sequence[List[T], List[V], V] = seq[List[T], List[V], V]
+  private[sbt] def appendListImplicit[T, V](implicit ev: V => T): Sequence[List[T], List[V], V] =
+    seq[List[T], List[V], V]
+  private[sbt] def appendVectorImplicit[T, V](implicit ev: V => T): Sequence[Vector[T], Seq[V], V] =
+    seq[Vector[T], Seq[V], V]
+  private[sbt] def appendClasspath: Sequence[Classpath, Seq[File], File] =
+    seq[Classpath, Seq[File], File]
+  private[sbt] def appendSet[T, V <: T]: Sequence[Set[T], Set[V], V] = seq[Set[T], Set[V], V]
+  private[sbt] def appendMap[A, B, X <: A, Y <: B]: Sequence[Map[A, B], Map[X, Y], (X, Y)] =
+    seq[Map[A, B], Map[X, Y], (X, Y)]
+  private[sbt] def appendOption[T]: Sequence[Seq[T], Option[T], Option[T]] =
+    seq[Seq[T], Option[T], Option[T]]
+  private[sbt] def appendSource: Sequence[Seq[Source], Seq[File], File] =
+    seq[Seq[Source], Seq[File], File]
+}
 }
