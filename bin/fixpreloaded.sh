2353be9eb688a3a0beee9e6f08852746695a23d9
Eugene Yokota <eed3si9n@gmail.com>
2019-05-09 23:54:01 -0400

    fixpreloaded.sh
    Ref https://github.com/sbt/sbt/issues/4661

---
+++ bin/fixpreloaded.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf $HOME/.sdkman/candidates/sbt/current/lib/local-preloaded/org.scala-lang.modules/scala-parser-combinators_2.12/1.0.4
+rm -rf $HOME/.sdkman/candidates/sbt/current/lib/local-preloaded/org.scala-lang.modules/scala-parser-combinators_2.12/1.0.5
+rm -rf $HOME/.sdkman/candidates/sbt/current/lib/local-preloaded/org.scala-lang.modules/scala-xml_2.12/1.0.6
+rm -rf $HOME/.sdkman/candidates/sbt/current/lib/local-preloaded/com.typesafe/config/1.2.0
+rm -rf $HOME/.sdkman/candidates/sbt/current/lib/local-preloaded/com.typesafe/ssl-config-core_2.12/0.2.2
+rm -rf $HOME/.sdkman/candidates/sbt/current/lib/local-preloaded/com.google.protobuf/protobuf-java/3.3.1
+rm -rf $HOME/.sdkman/candidates/sbt/current/lib/local-preloaded/org.scala-lang/scala-compiler/2.12.6
+
+rm -rf $HOME/.sbt/preloaded/org.scala-lang.modules/scala-parser-combinators_2.12/1.0.4
+rm -rf $HOME/.sbt/preloaded/org.scala-lang.modules/scala-parser-combinators_2.12/1.0.5
+rm -rf $HOME/.sbt/preloaded/org.scala-lang.modules/scala-xml_2.12/1.0.6
+rm -rf $HOME/.sbt/preloaded/com.typesafe/config/1.2.0
+rm -rf $HOME/.sbt/preloaded/com.typesafe/ssl-config-core_2.12/0.2.2
+rm -rf $HOME/.sbt/preloaded/com.google.protobuf/protobuf-java/3.3.1
+rm -rf $HOME/.sbt/preloaded/org.scala-lang/scala-compiler/2.12.6
