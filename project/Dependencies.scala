39d974830330404807f36bf9d6a6b5b6ecfa1db5
Eugene Yokota <eed3si9n@gmail.com>
2019-05-13 22:40:03 -0400

    Zinc 1.3.0-M5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
       case Some(version) => version
       case _             => nightlyVersion.getOrElse("1.3.0-M3")
     }
-  val zincVersion = nightlyVersion.getOrElse("1.3.0-M4")
+  val zincVersion = nightlyVersion.getOrElse("1.3.0-M5")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
