3cd702af9f20d55de755382f6481e9214a267ee7
Eugene Yokota <eed3si9n@gmail.com>
2019-07-15 19:23:58 -0400

    Windows

--- integration-test/src/test/scala/RunnerTest.scala
+++ integration-test/src/test/scala/RunnerTest.scala
@@ -5,7 +5,10 @@ import scala.sys.process._
 import java.io.File
 
 object SbtRunnerTest extends SimpleTestSuite with PowerAssertions {
-  lazy val sbtScript = new File("target/universal/stage/bin/sbt")
+  lazy val isWindows: Boolean = sys.props("os.name").toLowerCase(java.util.Locale.ENGLISH).contains("windows")
+  lazy val sbtScript =
+    if (isWindows) new File("target/universal/stage/bin/sbt.bat")
+    else new File("target/universal/stage/bin/sbt")
   def sbtProcess(arg: String) =
     sbt.internal.Process(sbtScript.getAbsolutePath + " " + arg, new File("citest"),
       "JAVA_OPTS" -> "",
