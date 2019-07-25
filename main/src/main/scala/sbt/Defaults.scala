a7715e90a43d85da83739c3183ea9f88987b2fd2
Ethan Atkins <contact@ethanatkins.com>
2019-07-25 10:04:21 -0700

    Rename cacheStoreFactory attribute
    It references a CacheStoreFactoryFactory so it should have been named
    accordingly.

--- main/src/main/scala/sbt/Defaults.scala
+++ main/src/main/scala/sbt/Defaults.scala
@@ -2772,7 +2772,8 @@ object Classpaths {
 
     import CacheStoreFactory.jvalueIsoString
     val cacheStoreFactory: CacheStoreFactory = {
-      val factory = state.value.get(Keys.cacheStoreFactory).getOrElse(InMemoryCacheStore.factory(0))
+      val factory =
+        state.value.get(Keys.cacheStoreFactoryFactory).getOrElse(InMemoryCacheStore.factory(0))
       factory(cacheDirectory.toPath, Converter)
     }
 
