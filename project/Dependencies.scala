13b110faef7de9da5df7df8a422cf35d225d894a
Eugene Yokota <eed3si9n@gmail.com>
2019-08-19 02:27:52 -0400

    io 1.3.0-M16, util 1.3.0-M9, lm 1.3.0-M6, zinc 1.3.0-M9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,14 +10,14 @@ object Dependencies {
   def nightlyVersion: Option[String] = sys.props.get("sbt.build.version")
 
   // sbt modules
-  private val ioVersion = nightlyVersion.getOrElse("1.3.0-M15")
-  private val utilVersion = nightlyVersion.getOrElse("1.3.0-M8")
+  private val ioVersion = nightlyVersion.getOrElse("1.3.0-M16")
+  private val utilVersion = nightlyVersion.getOrElse("1.3.0-M9")
   private val lmVersion =
     sys.props.get("sbt.build.lm.version") match {
       case Some(version) => version
-      case _             => nightlyVersion.getOrElse("1.3.0-M5")
+      case _             => nightlyVersion.getOrElse("1.3.0-M6")
     }
-  val zincVersion = nightlyVersion.getOrElse("1.3.0-M8")
+  val zincVersion = nightlyVersion.getOrElse("1.3.0-M9")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
