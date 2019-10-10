er1c/sbt-launcher-package

Intial sbt.bat refactor to match the universal bash script - implement --help, --verbose, --version, --script-verion, --numeric-version, .sbtopts, .jvmopts, -D

integration-test/src/test/scala/RunnerTest.scala
@@ -5,10 +5,14 @@ import scala.sys.process._
 import java.io.File
 
 object SbtRunnerTest extends SimpleTestSuite with PowerAssertions {
+  // 1.3.0, 1.3.0-M4
+  private val versionRegEx = "\\d(\\.\\d+){2}(-\\w+)?"
+
   lazy val isWindows: Boolean = sys.props("os.name").toLowerCase(java.util.Locale.ENGLISH).contains("windows")
   lazy val sbtScript =
     if (isWindows) new File("target/universal/stage/bin/sbt.bat")
     else new File("target/universal/stage/bin/sbt")
+
   def sbtProcess(arg: String) = sbtProcessWithOpts(arg, "", "")
   def sbtProcessWithOpts(arg: String, javaOpts: String, sbtOpts: String) =
     sbt.internal.Process(sbtScript.getAbsolutePath + " " + arg, new File("citest"),
@@ -21,6 +25,7 @@ object SbtRunnerTest extends SimpleTestSuite with PowerAssertions {
     assert(out == 0)
     ()
   }
+
 
   test("sbt -no-colors") {
     val out = sbtProcess("compile -no-colors -v").!!.linesIterator.toList
@@ -63,34 +68,43 @@ object SbtRunnerTest extends SimpleTestSuite with PowerAssertions {
     assert(out.contains[String]("-Xmx503m"))
     ()
   }
-
-  test("sbt with -mem 503 in JAVA_OPTS") {
+  
+  test("sbt with -mem 503, -Xmx in JAVA_OPTS") {
     val out = sbtProcessWithOpts("compile -mem 503 -v", "-Xmx1024m", "").!!.linesIterator.toList
     assert(out.contains[String]("-Xmx503m"))
+    assert(!out.contains[String]("-Xms1024m"))
+    ()
+  }
+
+  test("sbt with -mem 503, -Xmx in SBT_OPTS") {
+    val out = sbtProcessWithOpts("compile -mem 503 -v", "", "-Xmx1024m").!!.linesIterator.toList
+    assert(out.contains[String]("-Xmx503m"))
+    assert(!out.contains[String]("-Xms1024m"))
     ()
   }
 
   test("sbt with -Xms2048M -Xmx2048M -Xss6M in SBT_OPTS") {
-    val out = sbtProcessWithOpts("compile -v", "", "-Xms2048M -Xmx2048M -Xss6M").!!.linesIterator.toList
-    assert(out.contains[String]("-Xss6M"))
+    if (!isWindows) {
+      val out = sbtProcessWithOpts("compile -v", "", "-Xms2048M -Xmx2048M -Xss6M").!!.linesIterator.toList
+      assert(out.contains[String]("-Xss6M"))
+    }
     ()
   }
 
   test("sbt with --no-colors in SBT_OPTS") {
-    val out = sbtProcessWithOpts("compile -v", "", "--no-colors").!!.linesIterator.toList
-    assert(out.contains[String]("-Dsbt.log.noformat=true"))
+    if (!isWindows) {
+      val out = sbtProcessWithOpts("compile -v", "", "--no-colors").!!.linesIterator.toList
+      assert(out.contains[String]("-Dsbt.log.noformat=true"))
+    }
     ()
   }
-
-  // 1.3.0, 1.3.0-M4
-  private val versionRegEx = "\\d(\\.\\d+){2}(-\\w+)?"
 
   test("sbt -V|-version|--version should print sbtVersion") {
     val out = sbtProcessWithOpts("-version", "", "").!!.trim
     val expectedVersion =
       s"""|(?m)^sbt version in this project: $versionRegEx
-         |sbt script version: $versionRegEx$$
-         |""".stripMargin.trim.replace("\n", "\\n")
+          |sbt script version: $versionRegEx$$
+          |""".stripMargin.trim.replace("\n", "\\n")
     assert(out.matches(expectedVersion))
 
     val out2 = sbtProcessWithOpts("--version", "", "").!!.trim

sbt/sbt

integration-test/src/test/scala/RunnerTest.scala
@@ -5,10 +5,14 @@ import scala.sys.process._
 import java.io.File
 
 object SbtRunnerTest extends SimpleTestSuite with PowerAssertions {
+  // 1.3.0, 1.3.0-M4
+  private val versionRegEx = "\\d(\\.\\d+){2}(-\\w+)?"
+
   lazy val isWindows: Boolean = sys.props("os.name").toLowerCase(java.util.Locale.ENGLISH).contains("windows")
   lazy val sbtScript =
     if (isWindows) new File("target/universal/stage/bin/sbt.bat")
     else new File("target/universal/stage/bin/sbt")
+
   def sbtProcess(arg: String) = sbtProcessWithOpts(arg, "", "")
   def sbtProcessWithOpts(arg: String, javaOpts: String, sbtOpts: String) =
     sbt.internal.Process(sbtScript.getAbsolutePath + " " + arg, new File("citest"),
@@ -64,33 +68,40 @@ object SbtRunnerTest extends SimpleTestSuite with PowerAssertions {
     ()
   }
 
-  test("sbt with -mem 503 in JAVA_OPTS") {
+  test("sbt with -mem 503, -Xmx in JAVA_OPTS") {
     val out = sbtProcessWithOpts("compile -mem 503 -v", "-Xmx1024m", "").!!.linesIterator.toList
     assert(out.contains[String]("-Xmx503m"))
+    assert(!out.contains[String]("-Xmx1024m"))
+    ()
+  }
+
+  test("sbt with -mem 503, -Xmx in SBT_OPTS") {
+    val out = sbtProcessWithOpts("compile -mem 503 -v", "", "-Xmx1024m").!!.linesIterator.toList
+    assert(out.contains[String]("-Xmx503m"))
+    assert(!out.contains[String]("-Xmx1024m"))
     ()
   }
 
   test("sbt with -Xms2048M -Xmx2048M -Xss6M in SBT_OPTS") {
+    if (isWindows) cancel("Test not supported on windows")
     val out = sbtProcessWithOpts("compile -v", "", "-Xms2048M -Xmx2048M -Xss6M").!!.linesIterator.toList
     assert(out.contains[String]("-Xss6M"))
     ()
   }
 
   test("sbt with --no-colors in SBT_OPTS") {
+    if (isWindows) cancel("Test not supported on windows")
     val out = sbtProcessWithOpts("compile -v", "", "--no-colors").!!.linesIterator.toList
     assert(out.contains[String]("-Dsbt.log.noformat=true"))
     ()
   }
 
-  // 1.3.0, 1.3.0-M4
-  private val versionRegEx = "\\d(\\.\\d+){2}(-\\w+)?"
-
   test("sbt -V|-version|--version should print sbtVersion") {
     val out = sbtProcessWithOpts("-version", "", "").!!.trim
     val expectedVersion =
       s"""|(?m)^sbt version in this project: $versionRegEx
-         |sbt script version: $versionRegEx$$
-         |""".stripMargin.trim.replace("\n", "\\n")
+          |sbt script version: $versionRegEx$$
+          |""".stripMargin.trim.replace("\n", "\\n")
     assert(out.matches(expectedVersion))
 
     val out2 = sbtProcessWithOpts("--version", "", "").!!.trim