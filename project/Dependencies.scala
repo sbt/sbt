4d680a5f384dcf56ec4ffe472f4d07fe0111927c
Eugene Yokota <eed3si9n@gmail.com>
2020-01-27 00:20:12 -0500

    IO 1.3.3 and Zinc 1.3.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,14 +10,14 @@ object Dependencies {
   def nightlyVersion: Option[String] = sys.props.get("sbt.build.version")
 
   // sbt modules
-  private val ioVersion = nightlyVersion.getOrElse("1.3.1")
+  private val ioVersion = nightlyVersion.getOrElse("1.3.3")
   private val utilVersion = nightlyVersion.getOrElse("1.3.3")
   private val lmVersion =
     sys.props.get("sbt.build.lm.version") match {
       case Some(version) => version
       case _             => nightlyVersion.getOrElse("1.3.1")
     }
-  val zincVersion = nightlyVersion.getOrElse("1.3.2")
+  val zincVersion = nightlyVersion.getOrElse("1.3.4")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
