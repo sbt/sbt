41c3f433e647217f84fd9992ef8666b1ea6249b3
Anil Kumar Myla <anilkumarmyla@gmail.com>
2020-03-20 16:12:07 -0700

    Update semanticdb to work with scala 2.12.11

--- main/src/main/scala/sbt/plugins/SemanticdbPlugin.scala
+++ main/src/main/scala/sbt/plugins/SemanticdbPlugin.scala
@@ -21,7 +21,7 @@ object SemanticdbPlugin extends AutoPlugin {
     semanticdbEnabled := false,
     semanticdbIncludeInJar := false,
     semanticdbOptions := List("-Yrangepos"),
-    semanticdbVersion := "4.2.3"
+    semanticdbVersion := "4.3.7"
   )
 
   override lazy val projectSettings: Seq[Def.Setting[_]] = Seq(
