2396b449fe2cb3ca3e11992699dc0abfeb71f922
Eugene Yokota <eed3si9n@gmail.com>
2020-04-22 16:42:53 -0400

    Contraband 0.4.6

--- protocol/src/main/contraband-scala/sbt/internal/protocol/JsonRpcNotificationMessage.scala
+++ protocol/src/main/contraband-scala/sbt/internal/protocol/JsonRpcNotificationMessage.scala
@@ -1,5 +1,5 @@
 /**
- * This code is generated using [[http://www.scala-sbt.org/contraband/ sbt-contraband]].
+ * This code is generated using [[https://www.scala-sbt.org/contraband/ sbt-contraband]].
  */
 
 // DO NOT EDIT MANUALLY
