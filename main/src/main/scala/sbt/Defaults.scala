329d989e73f81de337383fcd8585189cd8fb5fc7
Ethan Atkins <contact@ethanatkins.com>
2020-04-30 18:22:15 -0700

    Use sbt source file stamps in zinc
    This fixes the nio/external-hooks test and also restores the performance
    of the benchmarks for the latest sbt version in
    https://github.com/eatkins/scala-build-watch-performance which had
    regressed when the custom ExternalHooks were disabled in
    7c4b01d9f722252d3e7379f69591861bdd57152c.
    
    The main change is that it changes the ReadStamps object that is passed
    into the compiler options to one that uses the unmanagedFileStampCache
    and managedFileStampCache for source files and falls back to the default
    stamper otherwise. This improves the performance quite significantly
    since we only hash the files once. It also makes it so that the analysis
    file will contain the source file stamps of the files when compilation
    began, rather than when compilation ended. That is what
    nio/external-hooks was testing. In the real world what could happen was
    that one modified a source file during compilation but then no
    incremental re-compilation would occur because after the initial
    compilation completed, zinc wrote the stamp of the modified source file
    in the analysis file even though it may have actually compiled a
    different version of the source file.

--- main/src/main/scala/sbt/Defaults.scala
+++ main/src/main/scala/sbt/Defaults.scala
@@ -214,8 +214,41 @@ object Defaults extends BuildCommon {
           VirtualTerminal.handler,
         ) ++ serverHandlers.value :+ ServerHandler.fallback
       },
-      uncachedStamper := Stamps.uncachedStamps(fileConverter.value),
-      reusableStamper := Stamps.timeWrapLibraryStamps(uncachedStamper.value, fileConverter.value),
+      timeWrappedStamper := Stamps
+        .timeWrapLibraryStamps(Stamps.uncachedStamps(fileConverter.value), fileConverter.value),
+      reusableStamper := {
+        val converter = fileConverter.value
+        val unmanagedCache = unmanagedFileStampCache.value
+        val managedCache = managedFileStampCache.value
+        val backing = timeWrappedStamper.value
+        new xsbti.compile.analysis.ReadStamps {
+          def getAllLibraryStamps()
+              : java.util.Map[xsbti.VirtualFileRef, xsbti.compile.analysis.Stamp] =
+            backing.getAllLibraryStamps()
+          def getAllProductStamps()
+              : java.util.Map[xsbti.VirtualFileRef, xsbti.compile.analysis.Stamp] =
+            backing.getAllProductStamps()
+          def getAllSourceStamps()
+              : java.util.Map[xsbti.VirtualFileRef, xsbti.compile.analysis.Stamp] =
+            new java.util.HashMap[xsbti.VirtualFileRef, xsbti.compile.analysis.Stamp]
+          def library(fr: xsbti.VirtualFileRef): xsbti.compile.analysis.Stamp = {
+            val path = converter.toPath(fr)
+            managedCache
+              .getOrElseUpdate(path, sbt.nio.FileStamper.Hash)
+              .map(_.stamp)
+              .getOrElse(backing.library(fr))
+          }
+          def product(fr: xsbti.VirtualFileRef): xsbti.compile.analysis.Stamp = backing.product(fr)
+          def source(fr: xsbti.VirtualFile): xsbti.compile.analysis.Stamp = {
+            val path = converter.toPath(fr)
+            unmanagedCache
+              .get(path)
+              .orElse(managedCache.getOrElseUpdate(path, sbt.nio.FileStamper.Hash))
+              .map(_.stamp)
+              .getOrElse(backing.source(fr))
+          }
+        }
+      },
       traceLevel in run :== 0,
       traceLevel in runMain :== 0,
       traceLevel in bgRun :== 0,
