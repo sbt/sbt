123f40723d3866b7f29d4077eb125780f4441a50
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-05-19 02:18:15 +0200

    Clean-up scripted test
    Actually unused file

--- modules/sbt-coursier/src/sbt-test/sbt-coursier/dependency-graph/src/main/scala/Main.scala
+++
@@ -1,8 +0,0 @@
-import java.io.File
-import java.nio.file.Files
-
-import org.apache.zookeeper.ZooKeeper
-
-object Main extends App {
-  Files.write(new File("output").toPath, classOf[ZooKeeper].getSimpleName.getBytes("UTF-8"))
-}
