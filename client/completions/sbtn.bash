6bc784a195fbe22f7db4c37ebee1bca5f0904a73
Ethan Atkins <contact@ethanatkins.com>
2020-08-15 09:35:25 -0700

    Change remaining sbtc to sbtn

--- client/completions/sbtn.bash
+++ client/completions/sbtn.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 _do_sbtn_completions() {
-  COMPREPLY=($(sbtc "--completions=${COMP_LINE}"))
+  COMPREPLY=($(sbtn "--completions=${COMP_LINE}"))
 }
 
 complete -F _do_sbtn_completions sbtn
