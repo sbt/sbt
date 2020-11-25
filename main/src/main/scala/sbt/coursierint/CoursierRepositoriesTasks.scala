0021f5ceb44f05eded03471f3f724d70baf35c26
João Ferreira <jtjeferreira@gmail.com>
2020-11-25 18:20:25 +0000

    try getPath

--- main/src/main/scala/sbt/coursierint/CoursierRepositoriesTasks.scala
+++ main/src/main/scala/sbt/coursierint/CoursierRepositoriesTasks.scala
@@ -76,7 +76,7 @@ object CoursierRepositoriesTasks {
     val result2 =
       paths.ivyHome match {
         case Some(ivyHome) =>
-          val ivyHomeUri = IO.toURI(ivyHome).getSchemeSpecificPart
+          val ivyHomeUri = IO.toURI(ivyHome).getPath
           result1 map {
             case r: FileRepository =>
               val ivyPatterns = r.patterns.ivyPatterns map {
