7781c86aea159c90bd55d549eede41c65cb4e140
Eugene Yokota <eed3si9n@gmail.com>
2020-12-02 21:26:41 -0500

    Try shorter decoding

--- .github/decodekey.sh
+++ .github/decodekey.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo $PGP_SECRET | base64 --decode | gpg --import --batch --yes --pinentry-mode loopback --passphrase $PGP_PASSPHRASE
+# echo $PGP_SECRET | base64 --decode | gpg --import --batch --yes --pinentry-mode loopback --passphrase $PGP_PASSPHRASE
+echo $PGP_SECRET | base64 --decode | gpg --batch --import
