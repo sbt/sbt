f6b993f91bbc5cbe80f533f78da0eec73dcca0c7
Eugene Yokota <eed3si9n@gmail.com>
2021-09-19 17:34:40 -0400

    Zinc 1.6.0-M1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,10 +12,10 @@ object Dependencies {
     sys.env.get("BUILD_VERSION") orElse sys.props.get("sbt.build.version")
 
   // sbt modules
-  private val ioVersion = nightlyVersion.getOrElse("1.5.1")
+  private val ioVersion = nightlyVersion.getOrElse("1.6.0-M1")
   private val lmVersion =
-    sys.props.get("sbt.build.lm.version").orElse(nightlyVersion).getOrElse("1.5.2")
-  val zincVersion = nightlyVersion.getOrElse("1.5.5")
+    sys.props.get("sbt.build.lm.version").orElse(nightlyVersion).getOrElse("1.6.0-M1")
+  val zincVersion = nightlyVersion.getOrElse("1.6.0-M1")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
