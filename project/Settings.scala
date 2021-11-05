c4b90111aab6c7df74b032692a79a979ba488d49
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-11-05 18:20:05 +0100

    Remove sbt-eviction-rules / sbt-version-policy for now
    Running into weird issues with scala-xml 1/2, and the latest
    sbt-version-policy needs a more recent sbt than the one we're on.

--- project/Settings.scala
+++ project/Settings.scala
@@ -4,8 +4,6 @@ import java.util.Locale
 import sbt._
 import sbt.Keys._
 import sbt.ScriptedPlugin.autoImport.{scriptedBufferLog, scriptedLaunchOpts}
-import sbtcompatibility.SbtCompatibilityPlugin.autoImport._
-import sbtevictionrules.EvictionRulesPlugin.autoImport._
 
 import com.jsuereth.sbtpgp._
 
@@ -38,19 +36,7 @@ object Settings {
     scalacOptions ++= {
       if (isAtLeastScala213.value) Seq("-Ymacro-annotations")
       else Nil
-    },
-    compatibilityRules ++= Seq(
-      "com.eed3si9n" %% "gigahorse-*" % "semver",
-      "org.scala-lang.modules" % "*" % "semver",
-      "org.scala-sbt" % "*" % "semver",
-      "com.typesafe" %% "ssl-config-core" % "semver",
-      "net.java.dev.jna" % "jna*" % "always"
-    ),
-    compatibilityIgnored += "com.swoval" % "apple-file-events",
-    evictionRules ++= Seq(
-      "com.eed3si9n" %% "gigahorse-*" % "semver",
-      "org.scala-lang.modules" %% "*" % "semver"
-    )
+    }
   ) ++ {
     val prop = sys.props.getOrElse("publish.javadoc", "").toLowerCase(Locale.ROOT)
     if (prop == "0" || prop == "false")
