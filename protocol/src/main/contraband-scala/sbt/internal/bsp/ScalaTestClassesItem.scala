74e5d79f60becc140845548913324193539e7350
Kamil Podsiadlo <kpodsiadlo@virtuslab.com>
2022-02-25 09:37:37 +0100

    feat: add optional framework field to the bsp protocol

--- protocol/src/main/contraband-scala/sbt/internal/bsp/ScalaTestClassesItem.scala
+++ protocol/src/main/contraband-scala/sbt/internal/bsp/ScalaTestClassesItem.scala
@@ -7,25 +7,27 @@ package sbt.internal.bsp
 /**
  * @param target The build target that contains the test classes.
  * @param classes The fully qualified names of the test classes in this target
+ * @param framework test framework name
  */
 final class ScalaTestClassesItem private (
   val target: sbt.internal.bsp.BuildTargetIdentifier,
-  val classes: Vector[String]) extends Serializable {
+  val classes: Vector[String],
+  val framework: Option[String]) extends Serializable {
   
   
   
   override def equals(o: Any): Boolean = this.eq(o.asInstanceOf[AnyRef]) || (o match {
-    case x: ScalaTestClassesItem => (this.target == x.target) && (this.classes == x.classes)
+    case x: ScalaTestClassesItem => (this.target == x.target) && (this.classes == x.classes) && (this.framework == x.framework)
     case _ => false
   })
   override def hashCode: Int = {
-    37 * (37 * (37 * (17 + "sbt.internal.bsp.ScalaTestClassesItem".##) + target.##) + classes.##)
+    37 * (37 * (37 * (37 * (17 + "sbt.internal.bsp.ScalaTestClassesItem".##) + target.##) + classes.##) + framework.##)
   }
   override def toString: String = {
-    "ScalaTestClassesItem(" + target + ", " + classes + ")"
+    "ScalaTestClassesItem(" + target + ", " + classes + ", " + framework + ")"
   }
-  private[this] def copy(target: sbt.internal.bsp.BuildTargetIdentifier = target, classes: Vector[String] = classes): ScalaTestClassesItem = {
-    new ScalaTestClassesItem(target, classes)
+  private[this] def copy(target: sbt.internal.bsp.BuildTargetIdentifier = target, classes: Vector[String] = classes, framework: Option[String] = framework): ScalaTestClassesItem = {
+    new ScalaTestClassesItem(target, classes, framework)
   }
   def withTarget(target: sbt.internal.bsp.BuildTargetIdentifier): ScalaTestClassesItem = {
     copy(target = target)
@@ -33,8 +35,15 @@ final class ScalaTestClassesItem private (
   def withClasses(classes: Vector[String]): ScalaTestClassesItem = {
     copy(classes = classes)
   }
+  def withFramework(framework: Option[String]): ScalaTestClassesItem = {
+    copy(framework = framework)
+  }
+  def withFramework(framework: String): ScalaTestClassesItem = {
+    copy(framework = Option(framework))
+  }
 }
 object ScalaTestClassesItem {
   
-  def apply(target: sbt.internal.bsp.BuildTargetIdentifier, classes: Vector[String]): ScalaTestClassesItem = new ScalaTestClassesItem(target, classes)
+  def apply(target: sbt.internal.bsp.BuildTargetIdentifier, classes: Vector[String], framework: Option[String]): ScalaTestClassesItem = new ScalaTestClassesItem(target, classes, framework)
+  def apply(target: sbt.internal.bsp.BuildTargetIdentifier, classes: Vector[String], framework: String): ScalaTestClassesItem = new ScalaTestClassesItem(target, classes, Option(framework))
 }
