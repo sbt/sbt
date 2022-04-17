20a9269e791747d01721e65b4829d0a22aef9e96
Philippus <philippus@gmail.com>
2022-04-17 12:59:12 +0200

    Add SBTN_AUTO_COMPLETE environment variable

--- main-command/src/main/scala/sbt/internal/client/NetworkClient.scala
+++ main-command/src/main/scala/sbt/internal/client/NetworkClient.scala
@@ -139,7 +139,7 @@ class NetworkClient(
   private val rebooting = new AtomicBoolean(false)
   private lazy val noTab = arguments.completionArguments.contains("--no-tab")
   private lazy val noStdErr = arguments.completionArguments.contains("--no-stderr") &&
-    !sys.env.contains("SBTC_AUTO_COMPLETE")
+    !sys.env.contains("SBTN_AUTO_COMPLETE") && !sys.env.contains("SBTC_AUTO_COMPLETE")
 
   private def mkSocket(file: File): (Socket, Option[String]) = ClientSocket.socket(file, useJNI)
 
