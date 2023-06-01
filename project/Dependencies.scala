1f1ee778e38b1bf495b0420d8ac9a42fffd19144
Eugene Yokota <eed3si9n@gmail.com>
2023-06-01 12:13:40 -0400

    Update Scala to 2.12.18
    Fixes https://github.com/sbt/sbt/issues/7235

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.contraband.ContrabandPlugin.autoImport._
 
 object Dependencies {
   // WARNING: Please Scala update versions in PluginCross.scala too
-  val scala212 = "2.12.17"
+  val scala212 = "2.12.18"
   val scala213 = "2.13.8"
   val checkPluginCross = settingKey[Unit]("Make sure scalaVersion match up")
   val baseScalaVersion = scala212
