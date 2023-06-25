363c0fe015a2492bb9e8c4ec84eb3cdf9eafb644
xuwei-k <6b656e6a69@gmail.com>
2023-06-25 09:46:39 +0900

    avoid deprecated `java.net.URL` constructor
    - https://bugs.openjdk.org/browse/JDK-8295949
    - openjdk/jdk@4338f52

--- core/src/main/scala/sbt/librarymanagement/License.scala
+++ core/src/main/scala/sbt/librarymanagement/License.scala
@@ -1,6 +1,7 @@
 package sbt.librarymanagement
 
 import java.net.URL
+import java.net.URI
 
 /**
  * Commonly used software licenses
@@ -9,16 +10,16 @@ import java.net.URL
  */
 object License {
   lazy val Apache2: (String, URL) =
-    ("Apache-2.0", new URL("https://www.apache.org/licenses/LICENSE-2.0.txt"))
+    ("Apache-2.0", new URI("https://www.apache.org/licenses/LICENSE-2.0.txt").toURL)
 
   lazy val MIT: (String, URL) =
-    ("MIT", new URL("https://opensource.org/licenses/MIT"))
+    ("MIT", new URI("https://opensource.org/licenses/MIT").toURL)
 
   lazy val CC0: (String, URL) =
-    ("CC0-1.0", new URL("https://creativecommons.org/publicdomain/zero/1.0/legalcode"))
+    ("CC0-1.0", new URI("https://creativecommons.org/publicdomain/zero/1.0/legalcode").toURL)
 
   def PublicDomain: (String, URL) = CC0
 
   lazy val GPL3_or_later: (String, URL) =
-    ("GPL-3.0-or-later", new URL("https://spdx.org/licenses/GPL-3.0-or-later.html"))
+    ("GPL-3.0-or-later", new URI("https://spdx.org/licenses/GPL-3.0-or-later.html").toURL)
 }
