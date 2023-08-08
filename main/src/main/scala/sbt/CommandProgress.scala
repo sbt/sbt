193dba708f400ba1f3fad597de785a1ede7490e7
Iulian Dragos <idragos@gradle.com>
2023-08-08 10:14:48 +0200

    Add missing header.

--- main/src/main/scala/sbt/CommandProgress.scala
+++ main/src/main/scala/sbt/CommandProgress.scala
@@ -1,3 +1,11 @@
+/*
+ * sbt
+ * Copyright 2023, Scala center
+ * Copyright 2011 - 2022, Lightbend, Inc.
+ * Copyright 2008 - 2010, Mark Harrah
+ * Licensed under Apache License 2.0 (see LICENSE)
+ */
+
 package sbt
 
 /**
