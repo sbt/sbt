7c400081a1151433f5b8681cb95fc3904d958f10
azdrojowa123 <33667003+azdrojowa123@users.noreply.github.com>
2023-08-25 18:01:49 +0200

    Fix help command for shells (#7358)
    * sbt-help-fix
    * fix BasicCommands.scala formatting

--- main-command/src/main/scala/sbt/BasicCommands.scala
+++ main-command/src/main/scala/sbt/BasicCommands.scala
@@ -129,8 +129,8 @@ object BasicCommands {
   def runHelp(s: State, h: Help)(arg: Option[String]): State = {
 
     val (extraArgs, remainingCommands) = s.remainingCommands match {
-      case xs :+ exec if exec.commandLine == "shell" => (xs, exec :: Nil)
-      case xs                                        => (xs, nil[Exec])
+      case xs :+ exec if exec.commandLine.endsWith(Shell) => (xs, exec :: Nil)
+      case xs                                             => (xs, nil[Exec])
     }
 
     val topic = (arg.toList ++ extraArgs.map(_.commandLine)) match {
