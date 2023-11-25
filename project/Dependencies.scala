f8f8db9e5859503db15860e3d5bcdfc5887548f6
Eugene Yokota <eed3si9n@gmail.com>
2023-11-25 15:50:50 -0500

    Update to Scala 3.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   // WARNING: Please Scala update versions in PluginCross.scala too
   val scala212 = "2.12.17"
   val scala213 = "2.13.8"
-  val scala3 = "3.2.1"
+  val scala3 = "3.3.1"
   val checkPluginCross = settingKey[Unit]("Make sure scalaVersion match up")
   val baseScalaVersion = scala3
   def nightlyVersion: Option[String] =
