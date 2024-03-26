9b554706f749ba5796b5d8dc303cd03b406e2635
Adrien Piquerez <adrien.piquerez@gmail.com>
2024-03-26 16:20:06 +0100

    fix auto-plugins-default-requires-jvmplugin

--- sbt-app/src/sbt-test/project1/auto-plugins-default-requires-jvmplugin/project/TestP.scala
+++ sbt-app/src/sbt-test/project1/auto-plugins-default-requires-jvmplugin/project/TestP.scala
@@ -3,8 +3,8 @@ import sbt._, Keys._
 object TestP extends AutoPlugin {
   override def projectSettings: Seq[Setting[_]] = Seq(
     Compile / resourceGenerators += Def.task {
-      streams.value.log info "resource generated in plugin"
-      Nil
+      streams.value.log.info("resource generated in plugin")
+      Seq.empty[File]
     }
   )
 }
