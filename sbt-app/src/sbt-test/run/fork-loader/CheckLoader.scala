cca078a2742029d4476d3c60f43fa9d36d708d7c
Adrien Piquerez <adrien.piquerez@gmail.com>
2024-03-27 14:09:30 +0100

    enable run/fork-loader

--- sbt-app/src/sbt-test/run/fork-loader/CheckLoader.scala
+++ sbt-app/src/sbt-test/run/fork-loader/CheckLoader.scala
@@ -1,8 +1,8 @@
 object CheckLoader {
-	def main(args: Array[String]): Unit = apply()
-	def apply(): Unit = {
-		val loader = getClass.getClassLoader
-		val appLoader = ClassLoader.getSystemClassLoader
-		assert(loader eq appLoader, "Application classes not loaded in the system class loader")
-	}
+  def main(args: Array[String]): Unit = apply()
+  def apply(): Unit = {
+    val loader = getClass.getClassLoader
+    val appLoader = ClassLoader.getSystemClassLoader
+    assert(loader eq appLoader, "Application classes not loaded in the system class loader")
+  }
 }
