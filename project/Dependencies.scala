7529eefda69c30cc78cb62b97844610d6cafdc97
Eugene Yokota <eed3si9n@gmail.com>
2024-05-05 17:21:40 -0400

    IO 1.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     sys.env.get("BUILD_VERSION") orElse sys.props.get("sbt.build.version")
 
   // sbt modules
-  private val ioVersion = nightlyVersion.getOrElse("1.9.9")
+  private val ioVersion = nightlyVersion.getOrElse("1.10.0")
   private val lmVersion =
     sys.props.get("sbt.build.lm.version").orElse(nightlyVersion).getOrElse("1.10.0-RC1")
   val zincVersion = nightlyVersion.getOrElse("1.10.0-RC2")
