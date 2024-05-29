861b8aa7921090f3abf1106d432181812d5fd6d6
João Ferreira <jtjeferreira@gmail.com>
2024-05-29 17:49:36 +0100

    remove a few more java.net.URL

--- ivy/src/main/scala/sbt/internal/librarymanagement/ComponentManager.scala
+++ ivy/src/main/scala/sbt/internal/librarymanagement/ComponentManager.scala
@@ -101,7 +101,7 @@ object IfMissing {
 object ComponentManager {
   lazy val (version, timestamp) = {
     val properties = new java.util.Properties
-    val propertiesStream = versionResource.openStream
+    val propertiesStream = getClass.getResourceAsStream("/xsbt.version.properties")
     try {
       properties.load(propertiesStream)
     } finally {
@@ -110,7 +110,4 @@ object ComponentManager {
     (properties.getProperty("version"), properties.getProperty("timestamp"))
   }
   lazy val stampedVersion = version + "_" + timestamp
-
-  import java.net.URL
-  private def versionResource: URL = getClass.getResource("/xsbt.version.properties")
 }
