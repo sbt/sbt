cc89c6394916d3dbe076ea8da453ab4026be37bb
João Ferreira <jtjeferreira@gmail.com>
2024-10-08 19:00:13 +0100

    import sbt.given and source compatibility with sbt 1.x

--- main-settings/src/main/scala/sbt/SlashSyntax.scala
+++ main-settings/src/main/scala/sbt/SlashSyntax.scala
@@ -31,39 +31,67 @@ import sbt.internal.util.AttributeKey
 trait SlashSyntax:
   import SlashSyntax.*
 
+  /*
   given Conversion[ScopeAxis[Reference], RichReference] =
     (a: ScopeAxis[Reference]) => RichReference(Scope(a, This, This, This))
+   */
+  implicit def sbtSlashSyntaxRichReferenceAxis(a: ScopeAxis[Reference]): RichReference =
+    new RichReference(Scope(a, This, This, This))
 
+  /*
   given [A](using Conversion[A, Reference]): Conversion[A, RichReference] =
     (a: A) => Select(a: Reference)
+   */
+  implicit def sbtSlashSyntaxRichProject[A](p: A)(implicit x: A => Reference): RichReference = x(p)
 
+  /*
   given Conversion[Reference, RichReference] =
     (r: Reference) => Select(r)
+   */
+  implicit def sbtSlashSyntaxRichReference(r: Reference): RichReference = Select(r)
 
+  /*
   given Conversion[ConfigKey, RichConfiguration] =
     (c: ConfigKey) => RichConfiguration(Scope(This, Select(c), This, This))
+   */
+  implicit def sbtSlashSyntaxRichConfigKey(c: ConfigKey): RichConfiguration =
+    new RichConfiguration(Scope(This, Select(c), This, This))
 
+  /*
   given Conversion[Configuration, RichConfiguration] =
     (c: Configuration) => (c: ConfigKey)
+   */
+  implicit def sbtSlashSyntaxRichConfiguration(c: Configuration): RichConfiguration = (c: ConfigKey)
 
   /**
    * This handles task scoping an existing scoped key (such as `Compile / test`)
    * into a task scoping in `(Compile / test) / name`.
    */
+  /*
   given Conversion[Scoped, Scope] =
     (t: Scoped) => t.scope.copy(task = Select(t.key))
 
   given Conversion[Scoped, RichScope] =
     (t: Scoped) => RichScope(t: Scope)
-
+   */
+  implicit def sbtSlashSyntaxRichScopeFromScoped(t: Scoped): RichScope =
+    new RichScope(t.scope.copy(task = Select(t.key)))
+  /*
   given [A1]: Conversion[AttributeKey[A1], Scope] =
     (a: AttributeKey[A1]) => Scope(This, This, Select(a), This)
 
   given [A1]: Conversion[AttributeKey[A1], RichScope] =
     (a: AttributeKey[A1]) => RichScope(a: Scope)
+   */
+  implicit def sbtSlashSyntaxRichScopeFromAttributeKey(a: AttributeKey[_]): RichScope =
+    Scope(This, This, Select(a), This)
 
+  /*
   given Conversion[Scope, RichScope] =
     (scope: Scope) => RichScope(scope)
+   */
+  implicit def sbtSlashSyntaxRichScope(s: Scope): RichScope = new RichScope(s)
+
 end SlashSyntax
 
 object SlashSyntax:
