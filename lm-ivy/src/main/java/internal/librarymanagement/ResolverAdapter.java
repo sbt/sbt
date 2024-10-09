b61a541ddecf07c283ed2759f86b21f30a384dcf
Adrien Piquerez <adrien.piquerez@gmail.com>
2024-10-09 08:57:50 +0200

    javafmt

--- lm-ivy/src/main/java/internal/librarymanagement/ResolverAdapter.java
+++ lm-ivy/src/main/java/internal/librarymanagement/ResolverAdapter.java
@@ -1,13 +1,16 @@
 package sbt.internal.librarymanagement;
 
-	import java.util.Map;
-	import org.apache.ivy.plugins.resolver.DependencyResolver;
+import java.util.Map;
+import org.apache.ivy.plugins.resolver.DependencyResolver;
 
 // implements the methods with raw types
 @SuppressWarnings("rawtypes")
-public abstract class ResolverAdapter implements DependencyResolver
-{
-    public String[] listTokenValues(String token, Map otherTokenValues) { return new String[0]; }
+public abstract class ResolverAdapter implements DependencyResolver {
+  public String[] listTokenValues(String token, Map otherTokenValues) {
+    return new String[0];
+  }
 
-    public Map[] listTokenValues(String[] tokens, Map criteria) { return new Map[0]; }
+  public Map[] listTokenValues(String[] tokens, Map criteria) {
+    return new Map[0];
+  }
 }
