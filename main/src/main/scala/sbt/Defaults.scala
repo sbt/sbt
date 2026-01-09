2d49a71c9bbfe1a578c4dc8f53ca178761781f70
mkdev11 <jaysmth689@gmail.com>
2026-01-09 05:18:44 +0200

    Use .toTaskable for tuple syntax per reviewer feedback

--- main/src/main/scala/sbt/Defaults.scala
+++ main/src/main/scala/sbt/Defaults.scala
@@ -1126,8 +1126,6 @@ object Defaults extends BuildCommon {
     extraTestDigests ++= IncrementalTest.extraTestDigestsTask.value,
     executeTests := Def.uncached({
       import sbt.TupleSyntax.*
-      val fp = testForkedParallel.value
-      val fpm = testForkedParallelism.value
       (
         test / streams,
         loadedTestFrameworks,
@@ -1135,11 +1133,13 @@ object Defaults extends BuildCommon {
         (test / testGrouping),
         (test / testExecution),
         (test / fullClasspath),
+        testForkedParallel.toTaskable,
+        testForkedParallelism.toTaskable,
         (test / javaOptions),
         (classLoaderLayeringStrategy),
         thisProject,
         fileConverter,
-      ).flatMapN { (s, lt, tl, gp, ex, cp, jo, clls, thisProj, c) =>
+      ).flatMapN { (s, lt, tl, gp, ex, cp, fp, fpm, jo, clls, thisProj, c) =>
         allTestGroupsTask(
           s,
           lt,
