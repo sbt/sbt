212ace69f63fd096b27425d4a8d34205495237f1
byteforge38 <joseph.mc0803@gmail.com>
2026-01-11 15:42:39 -0800

    style: Fix scalafmt formatting

--- main-settings/src/main/scala/sbt/std/KeyMacro.scala
+++ main-settings/src/main/scala/sbt/std/KeyMacro.scala
@@ -72,9 +72,9 @@ private[sbt] object KeyMacro:
     @tailrec
     def enclosingTerm0(sym: Symbol): Symbol =
       sym match
-        case sym if sym.flags.is(Flags.Macro) || sym.flags.is(Flags.Synthetic) =>
-          enclosingTerm0(sym.owner)
-        case sym if !sym.isTerm => enclosingTerm0(sym.owner)
+        case sym if sym.flags.is(Flags.Macro)     => enclosingTerm0(sym.owner)
+        case sym if sym.flags.is(Flags.Synthetic) => enclosingTerm0(sym.owner)
+        case sym if !sym.isTerm                   => enclosingTerm0(sym.owner)
         case _                                    => sym
     enclosingTerm0(Symbol.spliceOwner)
 
