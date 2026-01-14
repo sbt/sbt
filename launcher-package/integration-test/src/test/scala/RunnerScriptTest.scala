f3b045d3774510e4169a84c23f0384bf5fa8b961
0xsid0703 <build.developer373@gmail.com>
2026-01-14 12:47:05 +0100

    Fix infinite hang in test for -D argument without value
    Replace 'compile' with 'about' command to prevent sbt from hanging
    during test execution. The 'about' command exits quickly while still
    allowing -v flag to show command line arguments.
    
    Generated-by: Gen-AI

--- launcher-package/integration-test/src/test/scala/RunnerScriptTest.scala
+++ launcher-package/integration-test/src/test/scala/RunnerScriptTest.scala
@@ -34,7 +34,7 @@ object RunnerScriptTest extends verify.BasicTestSuite with ShellScriptUtil:
   testOutput("sbt -D arguments")("-Dsbt.supershell=false", "compile", "-v"): (out: List[String]) =>
     assert(out.contains[String]("-Dsbt.supershell=false"))
 
-  testOutput("sbt -D argument without value")("-Dfoo", "compile", "-v"): (out: List[String]) =>
+  testOutput("sbt -D argument without value")("-Dfoo", "about", "-v"): (out: List[String]) =>
     assert(out.contains[String]("-Dfoo"))
 
   testOutput("sbt --sbt-version")("--sbt-version", "1.3.13", "-v"): (out: List[String]) =>
