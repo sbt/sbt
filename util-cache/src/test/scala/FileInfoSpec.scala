1abc052085cf1e140b52a081eb9a1bdccb8dee6b
Eugene Yokota <eed3si9n@gmail.com>
2026-01-15 21:59:18 -0500

    Scalafmt

--- util-cache/src/test/scala/FileInfoSpec.scala
+++ util-cache/src/test/scala/FileInfoSpec.scala
@@ -27,7 +27,6 @@ object FileInfoSpec extends BasicTestSuite:
     val y: A = Converter.fromJson[A](jValue).get
     assert(x == y)
 
-
   def toJsonString[A: JsonWriter](x: A): String = CompactPrinter(Converter.toJson(x).get)
 
 end FileInfoSpec
