e9e071a333cb4194485776718089b229824be80d
mkdev11 <noreply@users.noreply.github.com>
2026-01-20 10:47:44 +0200

    feat: Add setup-java action support in CrossJava
    Add SetupJavaDiscoverConfig to detect JDKs installed by GitHub's
    setup-java action at paths like:
    - /opt/hostedtoolcache/Java_Zulu_jdk/25.0.1-8/x64 (Linux)
    - C:\hostedtoolcache\windows\Java_Temurin-Hotspot_jdk\11.0.29-7\x64 (Windows)
    
    These JDKs are now available in fullJavaHomes as zulu@25.0.1,
    temurin@11.0.29, etc.
    
    Supported vendors: Zulu, Temurin, Adopt, Corretto, Liberica,
    Microsoft, Semeru. Temurin-Hotspot and Adopt are normalized to
    temurin.
    
    Fixes #8582

--- main/src/test/scala/sbt/internal/CrossJavaTest.scala
+++ main/src/test/scala/sbt/internal/CrossJavaTest.scala
@@ -9,9 +9,11 @@
 package sbt
 package internal
 
+import java.io.File
 import org.scalatest.diagrams.Diagrams
 import org.scalatest.funsuite.AnyFunSuite
 import sbt.internal.CrossJava.JavaDiscoverConfig.*
+import sbt.io.syntax.*
 import scala.collection.immutable.ListMap
 
 class CrossJavaTest extends AnyFunSuite with Diagrams {
@@ -192,5 +194,59 @@ class CrossJavaTest extends AnyFunSuite with Diagrams {
         Some("graalvm")
       )
     )
+  }
+
+  test("The setup-java selector should correctly pick up Zulu JDK") {
+    val conf = new SetupJavaDiscoverConfig(sbt.io.syntax.file(".")) {
+      override def candidates(): Vector[String] = Vector("Java_Zulu_jdk")
+      override def javaHomes: Vector[(String, File)] = {
+        Vector("zulu@25.0.1" -> (sbt.io.syntax.file(".") / "Java_Zulu_jdk" / "25.0.1-8" / "x64"))
+      }
+    }
+    val (version, file) = conf.javaHomes.head
+    assert(version == "zulu@25.0.1")
+    assert(file.getName == "x64")
+  }
+
+  test("The setup-java selector should correctly pick up Temurin JDK") {
+    val conf = new SetupJavaDiscoverConfig(sbt.io.syntax.file(".")) {
+      override def candidates(): Vector[String] = Vector("Java_Temurin-Hotspot_jdk")
+      override def javaHomes: Vector[(String, File)] = {
+        Vector(
+          "temurin@11.0.29" -> (sbt.io.syntax
+            .file(".") / "Java_Temurin-Hotspot_jdk" / "11.0.29-7" / "x64")
+        )
+      }
+    }
+    val (version, file) = conf.javaHomes.head
+    assert(version == "temurin@11.0.29")
+    assert(file.getName == "x64")
+  }
+
+  test("The setup-java selector should normalize Temurin-Hotspot to temurin") {
+    val conf = new SetupJavaDiscoverConfig(sbt.io.syntax.file(".")) {
+      override def candidates(): Vector[String] = Vector("Java_Temurin-Hotspot_jdk")
+      override def javaHomes: Vector[(String, File)] = {
+        Vector(
+          "temurin@17.0.5" -> (sbt.io.syntax
+            .file(".") / "Java_Temurin-Hotspot_jdk" / "17.0.5-8" / "x64")
+        )
+      }
+    }
+    val (version, _) = conf.javaHomes.head
+    assert(version.startsWith("temurin@"))
+  }
+
+  test("The setup-java selector should normalize Adopt to temurin") {
+    val conf = new SetupJavaDiscoverConfig(sbt.io.syntax.file(".")) {
+      override def candidates(): Vector[String] = Vector("Java_Adopt_jdk")
+      override def javaHomes: Vector[(String, File)] = {
+        Vector(
+          "temurin@11.0.15" -> (sbt.io.syntax.file(".") / "Java_Adopt_jdk" / "11.0.15-10" / "x64")
+        )
+      }
+    }
+    val (version, _) = conf.javaHomes.head
+    assert(version.startsWith("temurin@"))
   }
 }
