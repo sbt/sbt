f7c085ff753342a45b3e1c37ebe765734d91cb2d
Eruis2579 <joewartson757@gmail.com>
2026-02-10 00:38:14 +0100

    fix: Use goto :eof instead of @endlocal/exit for --version in sbt.bat (#8717)
    Using goto :eof is the standard way to exit batch scripts and ensures
    proper cleanup. The previous @endlocal/exit approach may have caused
    hangs in some scenarios.

--- launcher-package/src/universal/bin/sbt.bat
+++ launcher-package/src/universal/bin/sbt.bat
@@ -628,8 +628,7 @@ if !sbt_args_print_version! equ 1 (
   echo.
   echo [info] sbt runner (sbt-the-batch-script) is a runner to run any declared version of sbt.
   echo [info] Actual version of the sbt is declared using project\build.properties for each build.
-  @endlocal
-  exit /B 0
+  goto :end
 )
 
 if !run_native_client! equ 1 (
