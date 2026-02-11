9179f4cf8052d17bb370f3e9481049312c8a9c34
Eruis2579 <joewartson757@gmail.com>
2026-02-11 01:06:18 +0100

    fix: Handle --version before native client to prevent Windows hangs (#8717)
    - Move --version handling to before native client check (matching Unix script)
    - Remove problematic :skip_native_for_version label that was inside :run function
    - Remove unnecessary test files (test.bat, test.sh, test3/) and PR_DESCRIPTION_8717.md
    
    This fixes the Windows hang by ensuring --version exits early before any
    native client or JVM initialization, avoiding execution flow issues.

--- launcher-package/src/universal/bin/sbt.bat
+++ launcher-package/src/universal/bin/sbt.bat
@@ -617,11 +617,21 @@ if !sbt_args_print_sbt_script_version! equ 1 (
   goto :eof
 )
 
-if !run_native_client! equ 1 (
-  rem Skip native client if --version is set to avoid hangs in sbt 2.x project dirs (#8717)
-  if "!sbt_args_print_version!" == "1" (
-    goto :skip_native_for_version
+rem Handle --version before native client to avoid hangs in sbt 2.x project dirs (#8717)
+if "!sbt_args_print_version!" == "1" (
+  if "!is_this_dir_sbt!" == "1" (
+    if not "!build_props_sbt_version!" == "" (
+      echo sbt version in this project: !build_props_sbt_version!
+    )
   )
+  echo sbt runner version: !init_sbt_version!
+  echo.
+  echo [info] sbt runner (sbt-the-batch-script) is a runner to run any declared version of sbt.
+  echo [info] Actual version of the sbt is declared using project\build.properties for each build.
+  goto :eof
+)
+
+if !run_native_client! equ 1 (
   goto :runnative !SBT_ARGS!
   goto :eof
 )
@@ -726,21 +736,6 @@ rem TODO: _SBT_OPTS needs to be processed as args and diffed against SBT_ARGS
 if !sbt_args_print_sbt_version! equ 1 (
   call :set_sbt_version
   echo !sbt_version!
-  goto :eof
-)
-
-rem Handle --version here (after native client check is skipped) for sbt 2.x project dirs (#8717)
-:skip_native_for_version
-if "!sbt_args_print_version!" == "1" (
-  if "!is_this_dir_sbt!" == "1" (
-    if not "!build_props_sbt_version!" == "" (
-      echo sbt version in this project: !build_props_sbt_version!
-    )
-  )
-  echo sbt runner version: !init_sbt_version!
-  echo.
-  echo [info] sbt runner (sbt-the-batch-script) is a runner to run any declared version of sbt.
-  echo [info] Actual version of the sbt is declared using project\build.properties for each build.
   goto :eof
 )
 
