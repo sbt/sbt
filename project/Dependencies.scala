f0da8710952dc33debafb4bc54f1a15aa8b908e0
bitloi <raphaelaloi.eth@gmail.com>
2026-02-19 10:03:13 +0100

    Update launcher to 1.6.0 and add repository-override to sbt.boot.properties
    - Bump launcherVersion to 1.6.0 (launcher-interface, launcher)
    - Add repository-override to [ivy] in default boot properties for
      force-proxy-repos support (repositories_force file)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
-  val launcherVersion = "1.5.2"
+  val launcherVersion = "1.6.0"
   val launcherInterface = "org.scala-sbt" % "launcher-interface" % launcherVersion
   val rawLauncher = "org.scala-sbt" % "launcher" % launcherVersion
   val testInterface = "org.scala-sbt" % "test-interface" % "1.0"
