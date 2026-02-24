efc1470fe3c7d8e5caf38cd993bbb43c89fa4aef
bitloi <89318445+bitloi@users.noreply.github.com>
2026-02-24 00:50:07 -0500

    Update main-command/src/main/scala/sbt/internal/client/NetworkClient.scala
    Co-authored-by: eugene yokota <eed3si9n@gmail.com>

--- main-command/src/main/scala/sbt/internal/client/NetworkClient.scala
+++ main-command/src/main/scala/sbt/internal/client/NetworkClient.scala
@@ -317,7 +317,7 @@ class NetworkClient(
       token = tkn,
       skipAnalysis = Some(skipAnalysis),
       canWork = Some(true),
-      subscribeToAll = Some(true),
+      subscribeToAll = Some(false),
     )
     val initCommand = InitCommand(
       token = tkn, // duplicated with opts for compatibility
