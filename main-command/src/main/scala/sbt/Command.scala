99a435cd1aac5ff475be41e567a59c223e27ba82
since-2017-hub <since2017hub@gmail.com>
2026-02-26 14:44:29 +0000

    fix: correct syntax in Command.scala for flatMap usage

--- main-command/src/main/scala/sbt/Command.scala
+++ main-command/src/main/scala/sbt/Command.scala
@@ -172,11 +172,12 @@ object Command {
       commandMap: Map[String, State => Parser[() => State]]
   ): State => Parser[() => State] =
     state =>
-      token(OpOrID.examples(commandMap.keys.toSet)).flatMap: id =>
+      token(OpOrID.examples(commandMap.keys.toSet)).flatMap { id =>
         (commandMap get id) match {
           case None    => failure(invalidValue("command", commandMap.keys)(id))
           case Some(c) => c(state)
         }
+      }
 
   // overload instead of default parameter to keep binary compatibility
   @deprecated("Use overload that takes the onParseError callback", since = "1.9.4")
