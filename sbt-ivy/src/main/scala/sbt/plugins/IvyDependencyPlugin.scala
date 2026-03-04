04b5c2fe8752718796238a6dbf2338ee013bc691
eureka928 <meobius123@gmail.com>
2026-03-04 14:17:06 +0100

    [2.x] feat: Create sbt-ivy module with IvyDependencyPlugin
    Create a new sbt-ivy/ subproject containing:
    
    - IvyDependencyPlugin: AutoPlugin that provides all Ivy-specific
      functionality (ivySbt, ivyModule, ivyConfiguration, publisher,
      projectDescriptors, projectResolver, deliver/makeIvyXml tasks).
      Triggers on allRequirements when IvyPlugin is present.
    
    - IvyXml: Moved from main/ — generates Ivy XML files for publishing,
      uses IvySbt directly.
    
    This is Step 5 of sbt#7640: extracting lm-ivy to a separate plugin.

---
+++ sbt-ivy/src/main/scala/sbt/plugins/IvyDependencyPlugin.scala
@@ -0,0 +1,116 @@
+/*
+ * sbt
+ * Copyright 2023, Scala center
+ * Copyright 2011 - 2022, Lightbend, Inc.
+ * Copyright 2008 - 2010, Mark Harrah
+ * Licensed under Apache License 2.0 (see LICENSE)
+ */
+
+package sbt
+package plugins
+
+import java.io.File
+import org.apache.ivy.core.module.descriptor.ModuleDescriptor
+import org.apache.ivy.core.module.id.ModuleRevisionId
+import sbt.Def.{ Initialize, Setting }
+import sbt.Keys.*
+import sbt.ProjectExtra.*
+import sbt.internal.LibraryManagement
+import sbt.internal.librarymanagement.{ IvyActions, IvySbt, IvyXml, ProjectResolver }
+import sbt.internal.librarymanagement.ivy.*
+import sbt.io.syntax.*
+import sbt.librarymanagement.*
+import sbt.std.TaskExtra.*
+
+/**
+ * AutoPlugin that provides all Ivy-specific functionality.
+ * This plugin overrides the stub defaults in main/ with real Ivy implementations.
+ */
+object IvyDependencyPlugin extends AutoPlugin:
+  override def requires = IvyPlugin
+  override def trigger = allRequirements
+
+  override lazy val globalSettings: Seq[Setting[?]] = Seq(
+    updateOptions := UpdateOptions(),
+  )
+
+  override lazy val projectSettings: Seq[Setting[?]] = Seq(
+    ivyConfiguration := Def.uncached(mkIvyConfiguration.value),
+    publisher := Def.uncached(IvyPublisher(ivyConfiguration.value.asInstanceOf[IvyConfiguration])),
+    ivySbt := Def.uncached(ivySbt0.value),
+    ivyModule := Def.uncached {
+      val is = ivySbt.value.asInstanceOf[IvySbt]
+      new is.Module(moduleSettings.value)
+    },
+    projectDescriptors := Def.uncached(depMap.value.asInstanceOf[Map[Any, Any]]),
+    projectResolver := Def.uncached(projectResolverTask.value),
+  ) ++ IvyXml.generateIvyXmlSettings() ++ ivyPublishOrSkipSettings
+
+  private lazy val ivySbt0: Initialize[Task[IvySbt]] =
+    Def.task {
+      IvyCredentials.register(credentials.value, streams.value.log)
+      new IvySbt(ivyConfiguration.value.asInstanceOf[IvyConfiguration])
+    }
+
+  private lazy val mkIvyConfiguration: Initialize[Task[InlineIvyConfiguration]] =
+    Def.task {
+      val (rs, other) = (fullResolvers.value.toVector, otherResolvers.value.toVector)
+      val s = streams.value
+      Classpaths.warnResolversConflict(rs ++: other, s.log)
+      Classpaths.errorInsecureProtocol(rs ++: other, s.log)
+      InlineIvyConfiguration()
+        .withPaths(ivyPaths.value)
+        .withResolvers(rs)
+        .withOtherResolvers(other)
+        .withModuleConfigurations(moduleConfigurations.value.toVector)
+        .withLock(LibraryManagement.lock(appConfiguration.value))
+        .withChecksums((update / checksums).value.toVector)
+        .withResolutionCacheDir(target.value / "resolution-cache")
+        .withUpdateOptions(updateOptions.value.asInstanceOf[UpdateOptions])
+        .withLog(s.log)
+    }
+
+  private def depMap: Initialize[Task[Map[ModuleRevisionId, ModuleDescriptor]]] =
+    import sbt.TupleSyntax.*
+    (buildDependencies.toTaskable, thisProjectRef.toTaskable, settingsData, streams)
+      .flatMapN { (bd, thisProj, data, s) =>
+        depMap(bd.classpathTransitiveRefs(thisProj), data, s.log)
+      }
+
+  private def depMap(
+      projects: Seq[ProjectRef],
+      data: Def.Settings,
+      log: sbt.util.Logger
+  ): Task[Map[ModuleRevisionId, ModuleDescriptor]] =
+    val ivyModules = projects.flatMap { proj =>
+      (proj / ivyModule).get(data)
+    }.join
+    ivyModules.mapN { mod =>
+      mod.map { m => m.asInstanceOf[IvySbt#Module].dependencyMapping(log) }.toMap
+    }
+
+  private def projectResolverTask: Initialize[Task[Resolver]] =
+    projectDescriptors.map { m =>
+      val typedMap = m.asInstanceOf[Map[ModuleRevisionId, ModuleDescriptor]]
+      val resolver = new ProjectResolver(ProjectResolver.InterProject, typedMap)
+      new RawRepository(resolver, resolver.getName)
+    }
+
+  private def ivyPublishOrSkipSettings: Seq[Setting[?]] =
+    Seq(
+      deliver := deliverTask(makeIvyXmlConfiguration).value,
+      deliverLocal := deliverTask(makeIvyXmlLocalConfiguration).value,
+      makeIvyXml := deliverTask(makeIvyXmlConfiguration).value,
+    )
+
+  private def deliverTask(config: TaskKey[PublishConfiguration]): Initialize[Task[File]] =
+    Def.task {
+      Def.unit(update.value)
+      if !useIvy.value then sys.error("deliver/makeIvyXml requires useIvy := true")
+      IvyActions.deliver(
+        ivyModule.value.asInstanceOf[IvySbt#Module],
+        config.value,
+        streams.value.log
+      )
+    }
+end IvyDependencyPlugin
