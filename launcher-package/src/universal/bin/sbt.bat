e857252132e77303455c7305aa06d79f2f1375d6
oolokioo7 <loki021107@gmail.com>
2026-03-09 05:29:48 -0700

    fix: project not found error on windows

--- launcher-package/src/universal/bin/sbt.bat
+++ launcher-package/src/universal/bin/sbt.bat
@@ -94,7 +94,7 @@ if defined JAVA_HOMES (
 )
 
 if exist "project\build.properties" (
-  for /F "eol=# delims== tokens=1*" %%a in (project\build.properties) do (
+  for /F "usebackq eol=# delims== tokens=1*" %%a in ("project\build.properties") do (
     if "%%a" == "sbt.version" if not "%%b" == "" (
       set build_props_sbt_version=%%b
     )
