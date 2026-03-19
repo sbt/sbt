18fe2c0cd8a78aee07b55187b1853da1bcfaefc4
bitloi <raphaelaloi.eth@gmail.com>
2026-03-19 21:24:32 +0100

    style: remove returns and use fewer-braces in RelocationCycleDetector

--- lm-coursier/src/main/scala/lmcoursier/internal/RelocationCycleDetector.scala
+++ lm-coursier/src/main/scala/lmcoursier/internal/RelocationCycleDetector.scala
@@ -13,68 +13,58 @@ import scala.annotation.tailrec
  * Mirrors one step of `coursier.graph.DependencyTree.Node.relocation` so we
  * only skip `Conflict` when Coursier would spin on the same graph.
  */
-private[internal] object RelocationCycleDetector {
+private[internal] object RelocationCycleDetector:
 
   type Mvc = CoreResolution.ModuleVersionConstraint
 
-  private def oneRelocationStep(resolution: Resolution, dep: Dependency): Option[Mvc] = {
-    val reconciledVersion =
-      resolution.reconciledVersions.get(dep.module) match {
-        case Some(v) => v
-        case None    => return None
-      }
-    val dep0 =
-      if (dep.versionConstraint == reconciledVersion) dep
-      else dep.withVersionConstraint(reconciledVersion)
-    val (_, proj) =
-      resolution.projectCache0.get(dep0.moduleVersionConstraint) match {
-        case Some(v) => v
-        case None    => return None
-      }
-    val mavenRelocatedOpt =
-      if (proj.relocated && proj.dependencies0.lengthCompare(1) == 0)
-        Some(proj.dependencies0.head._2)
-      else None
-    def gradleModuleRelocatedOpt =
-      dep0.variantSelector match {
-        case attr: VariantSelector.AttributesBased =>
-          if (proj.variants.isEmpty) None
-          else
-            proj.variantFor(attr) match {
-              case Left(_)        => None
-              case Right(variant) => proj.isRelocatedVariant(variant)
-            }
-        case _: VariantSelector.ConfigurationBased => None
-      }
-    mavenRelocatedOpt.orElse(gradleModuleRelocatedOpt) match {
-      case Some(relocatedTo) =>
-        val relocatedTo0 =
-          if (relocatedTo.variantSelector.isEmpty)
-            relocatedTo.withVariantSelector(dep0.variantSelector)
-          else relocatedTo
-        Some(relocatedTo0.moduleVersionConstraint)
-      case None => None
-    }
-  }
+  private def oneRelocationStep(resolution: Resolution, dep: Dependency): Option[Mvc] =
+    resolution.reconciledVersions
+      .get(dep.module)
+      .flatMap: recon =>
+        val dep0 =
+          if dep.versionConstraint == recon then dep
+          else dep.withVersionConstraint(recon)
+        resolution.projectCache0
+          .get(dep0.moduleVersionConstraint)
+          .flatMap:
+            case (_, proj) =>
+              val mavenRelocatedOpt =
+                if proj.relocated && proj.dependencies0.lengthCompare(1) == 0 then
+                  Some(proj.dependencies0.head._2)
+                else None
+              def gradleRelocated: Option[Dependency] =
+                dep0.variantSelector match
+                  case attr: VariantSelector.AttributesBased =>
+                    if proj.variants.isEmpty then None
+                    else
+                      proj.variantFor(attr) match
+                        case Left(_)        => None
+                        case Right(variant) => proj.isRelocatedVariant(variant)
+                  case _: VariantSelector.ConfigurationBased => None
+              mavenRelocatedOpt
+                .orElse(gradleRelocated)
+                .map: relocatedTo =>
+                  val relocatedTo0 =
+                    if relocatedTo.variantSelector.isEmpty then
+                      relocatedTo.withVariantSelector(dep0.variantSelector)
+                    else relocatedTo
+                  relocatedTo0.moduleVersionConstraint
 
   /** When true, `coursier.graph.Conflict(resolution)` can run indefinitely. */
-  def hasRelocationCycle(resolution: Resolution): Boolean = {
-    if (!resolution.isDone || resolution.conflicts.nonEmpty || resolution.errors0.nonEmpty)
-      return false
-    val keys = resolution.projectCache0.keySet
-    keys.exists { start =>
-      @tailrec
-      def walk(visited: Set[Mvc], cur: Option[Mvc]): Boolean =
-        cur match {
-          case None => false
-          case Some(mvc) =>
-            if (visited.contains(mvc)) true
-            else {
-              val dep = Dependency(module = mvc._1, version = mvc._2)
-              walk(visited + mvc, oneRelocationStep(resolution, dep))
-            }
-        }
-      walk(Set.empty, Some(start))
+  def hasRelocationCycle(resolution: Resolution): Boolean =
+    resolution.isDone && resolution.conflicts.isEmpty && resolution.errors0.isEmpty && {
+      val keys = resolution.projectCache0.keySet
+      keys.exists: start =>
+        @tailrec
+        def walk(visited: Set[Mvc], cur: Option[Mvc]): Boolean =
+          cur match
+            case None => false
+            case Some(mvc) =>
+              if visited.contains(mvc) then true
+              else
+                val dep = Dependency(module = mvc._1, version = mvc._2)
+                walk(visited + mvc, oneRelocationStep(resolution, dep))
+        walk(Set.empty, Some(start))
     }
-  }
-}
+
+end RelocationCycleDetector
