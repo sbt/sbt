3295ebe09752aa5aca384bf9d365f698d6faa280
xuwei-k <6b656e6a69@gmail.com>
2026-03-20 15:23:17 +0900

    Enable sbt-test/run/fork

--- sbt-app/src/sbt-test/run/fork/src/main/scala/ForkFail.scala
+++ sbt-app/src/sbt-test/run/fork/src/main/scala/ForkFail.scala
@@ -1,9 +1,9 @@
 object ForkTest {
   def main(args:Array[String]): Unit = {
-		val name = sys.env.getOrElse("flag.name", "flag")
-		println("Name: " + name)
-		val cwd = (new java.io.File(name)).getAbsoluteFile
-		cwd.getParentFile.mkdirs()
-		cwd.createNewFile()
+    val name = sys.env.getOrElse("flag.name", "flag")
+    println("Name: " + name)
+    val cwd = (new java.io.File(name)).getAbsoluteFile
+    cwd.getParentFile.mkdirs()
+    cwd.createNewFile()
   }
 }
