d9c896317bf2a73d0eccb51b98e166d370132dcc
xuwei-k <6b656e6a69@gmail.com>
2026-03-25 09:10:47 +0900

    Update ZincComponentCompilerSpec

--- zinc-lm-integration/src/test/scala/sbt/internal/inc/ZincComponentCompilerSpec.scala
+++ zinc-lm-integration/src/test/scala/sbt/internal/inc/ZincComponentCompilerSpec.scala
@@ -12,29 +12,19 @@ import sbt.internal.util.ConsoleLogger
 import sbt.io.IO
 
 class ZincComponentCompilerSpec extends IvyBridgeProviderSpecification {
-  val scala2105 = "2.10.5"
-  val scala2106 = "2.10.6"
-  val scala2118 = "2.11.8"
-  val scala21111 = "2.11.11"
+  val scala2107 = "2.10.7"
+  val scala21112 = "2.11.12"
   val scala21221 = "2.12.21"
   val scala21311 = "2.13.11"
 
-  def isJava8: Boolean = sys.props("java.specification.version") == "1.8"
-
   val logger = ConsoleLogger()
 
-  it should "compile the bridge for Scala 2.10.5 and 2.10.6" in { case given FixtureParam =>
-    if (isJava8) {
-      IO.withTemporaryDirectory(t => getCompilerBridge(t, logger, scala2105) should exist)
-      IO.withTemporaryDirectory(t => getCompilerBridge(t, logger, scala2106) should exist)
-    } else ()
+  it should "compile the bridge for Scala 2.10.7" in { case given FixtureParam =>
+    IO.withTemporaryDirectory(t => getCompilerBridge(t, logger, scala2107) should exist)
   }
 
-  it should "compile the bridge for Scala 2.11.8 and 2.11.11" in { case given FixtureParam =>
-    if (isJava8) {
-      IO.withTemporaryDirectory(t => getCompilerBridge(t, logger, scala2118) should exist)
-      IO.withTemporaryDirectory(t => getCompilerBridge(t, logger, scala21111) should exist)
-    } else ()
+  it should "compile the bridge for Scala 2.11.12" in { case given FixtureParam =>
+    IO.withTemporaryDirectory(t => getCompilerBridge(t, logger, scala21112) should exist)
   }
 
   it should "compile the bridge for Scala 2.12.21" in { case given FixtureParam =>
