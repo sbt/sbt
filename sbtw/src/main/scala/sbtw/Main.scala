58f254a291fb9fe805e65b71ce26b05acf78d22a
kenji yoshida <6b656e6a69@gmail.com>
2026-04-12 10:47:48 +0900

    [2.0.x] hide JDK warnings if JDK 26 or later

--- sbtw/src/main/scala/sbtw/Main.scala
+++ sbtw/src/main/scala/sbtw/Main.scala
@@ -83,7 +83,7 @@ object Main:
     else Memory.addDefaultMemory(javaOpts, sbtOpts, javaVer, LauncherOptions.defaultMemMb)
     sbtOpts = finalSbt
 
-    if !opts.noHideJdkWarnings && javaVer == 25 then
+    if !opts.noHideJdkWarnings && javaVer >= 25 then
       sbtOpts = sbtOpts ++ Seq(
         "--sun-misc-unsafe-memory-access=allow",
         "--enable-native-access=ALL-UNNAMED"
