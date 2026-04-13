d028427067cfb500c6f91a86c1b576584fe770b3
carlos4s <71615127+carlos4s@users.noreply.github.com>
2026-04-13 16:40:13 +0200

    Fix clean task to delete managed sources located outside the target directory

--- main/src/main/scala/sbt/Defaults.scala
+++ main/src/main/scala/sbt/Defaults.scala
@@ -1965,18 +1965,7 @@ object Defaults extends BuildCommon with DefExtra {
   }
 
   /** Implements `cleanFiles` task. */
-  private[sbt] def cleanFilesTask: Initialize[Task[Vector[File]]] = {
-    import ScopeFilter.Make.*
-    val allConfigs = ScopeFilter(configurations = inAnyConfiguration)
-    Def.task {
-      val targetDir = target.value.toPath
-      val managedSrcDirs = managedSourceDirectories.?.all(allConfigs).value.flatten.flatten
-      val managedRscDirs = managedResourceDirectories.?.all(allConfigs).value.flatten.flatten
-      (managedSrcDirs ++ managedRscDirs)
-        .filter(d => !d.toPath.startsWith(targetDir))
-        .toVector
-    }
-  }
+  private[sbt] def cleanFilesTask: Initialize[Task[Vector[File]]] = Def.task { Vector.empty[File] }
 
   def runMainTask(
       classpath: Initialize[Task[Classpath]],
