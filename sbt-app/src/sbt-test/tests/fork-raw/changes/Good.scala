5a8195317ab8d8ad04bcd75cf622c425d1e9320f
xuwei-k <6b656e6a69@gmail.com>
2026-05-01 07:40:04 +0900

    Add more ClassLoaderLayeringStrategy.Raw test

--- sbt-app/src/sbt-test/tests/fork-raw/changes/Good.scala
+++ sbt-app/src/sbt-test/tests/fork-raw/changes/Good.scala
@@ -5,4 +5,7 @@ import munit.*
 class ATest extends FunSuite:
   test("sum"):
     assert(1 + 1 == 2)
+
+  test("classpath"):
+    assert(scala.util.Properties.javaClassPath.contains("munit"))
 end ATest
