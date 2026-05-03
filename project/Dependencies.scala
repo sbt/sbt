1f3bce12ccb10137a9333216a2a1e67927b420c0
Eugene Yokota <eed3si9n@gmail.com>
2026-05-03 15:54:33 -0400

    sjson-new 0.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -62,7 +62,7 @@ object Dependencies {
   def addSbtZinc = addSbtModule(sbtZincPath, "zinc", zinc)
   def addSbtZincCompileCore = addSbtModule(sbtZincPath, "zincCompileCore", zincCompileCore)
 
-  lazy val sjsonNewVersion = "0.14.0-M5"
+  lazy val sjsonNewVersion = "0.14.0"
   def sjsonNew(n: String) = Def.setting(
     "com.eed3si9n" %% n % sjsonNewVersion
   ) // contrabandSjsonNewVersion.value
