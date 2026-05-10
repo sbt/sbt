d5fee10a528bf7af5f7a2902d9613f74c36e0341
Eugene Yokota <eed3si9n@gmail.com>
2026-05-10 03:33:47 -0400

    Port InclExclSpec to lm-coursier

--- lm-coursier/src/test/scala/lmcoursier/InclExclSpec.scala
+++ lm-coursier/src/test/scala/lmcoursier/InclExclSpec.scala
@@ -1,27 +1,27 @@
-package sbt.internal.librarymanagement
+package lmcoursier
 
 import sbt.librarymanagement.*
 import sbt.librarymanagement.syntax.*
-import DependencyBuilders.OrganizationArtifactName
 import scala.annotation.nowarn
+import TestKit.*
 
-object InclExclSpec extends BaseIvySpecification {
+object InclExclSpec extends verify.BasicTestSuite {
   val scala210 = Some("2.10.4")
   test("it should exclude any version of lift-json via a new exclusion rule") {
     val toExclude = ExclusionRule("net.liftweb", "lift-json_2.10")
-    val report = getIvyReport(createLiftDep(toExclude), scala210)
+    val report = getUpdateReport(createLiftDep(toExclude), scala210)
     testLiftJsonIsMissing(report)
   }
 
   test("it should exclude any version of lift-json with explicit Scala version") {
-    val excluded: OrganizationArtifactName = "net.liftweb" % "lift-json_2.10"
-    val report = getIvyReport(createLiftDep(excluded), scala210)
+    val excluded = "net.liftweb" % "lift-json_2.10"
+    val report = getUpdateReport(createLiftDep(excluded), scala210)
     testLiftJsonIsMissing(report)
   }
 
   test("it should exclude any version of cross-built lift-json") {
-    val excluded: OrganizationArtifactName = "net.liftweb" %% "lift-json"
-    val report = getIvyReport(createLiftDep(excluded), scala210)
+    val excluded = "net.liftweb" %% "lift-json"
+    val report = getUpdateReport(createLiftDep(excluded), scala210)
     testLiftJsonIsMissing(report)
   }
 
@@ -32,7 +32,7 @@ object InclExclSpec extends BaseIvySpecification {
     val liftDep =
       ("net.liftweb" %% "lift-mapper" % "2.6-M4" % "compile")
         .exclude("net.liftweb", "lift-json_2.10")
-    val report = getIvyReport(liftDep, scala210)
+    val report = getUpdateReport(liftDep, scala210)
     testLiftJsonIsMissing(report)
   }
 
@@ -42,7 +42,7 @@ object InclExclSpec extends BaseIvySpecification {
     val liftDep =
       ("net.liftweb" %% "lift-mapper" % "2.6-M4" % "compile")
         .exclude("net.liftweb" %% "lift-json")
-    val report = getIvyReport(liftDep, scala210)
+    val report = getUpdateReport(liftDep, scala210)
     testLiftJsonIsMissing(report)
   }
 
@@ -50,32 +50,26 @@ object InclExclSpec extends BaseIvySpecification {
     val liftDep =
       ("net.liftweb" %% "lift-mapper" % "2.6-M4" % "compile")
         .excludeAll("net.liftweb" %% "lift-json")
-    val report = getIvyReport(liftDep, scala210)
+    val report = getUpdateReport(liftDep, scala210)
     testLiftJsonIsMissing(report)
   }
 
   val scala2122 = Some("2.12.2")
   test("it should exclude a concrete version of lift-json when it's full cross version") {
     val excluded: ModuleID = ("org.scalameta" % "scalahost" % "1.7.0").cross(CrossVersion.full)
-    val report = getIvyReport(createMetaDep(excluded), scala2122)
-    testScalahostIsMissing(report)
-  }
-
-  test("it should exclude any version of lift-json when it's full cross version") {
-    val excluded = new OrganizationArtifactName("net.liftweb", "lift-json", CrossVersion.full)
-    val report = getIvyReport(createMetaDep(excluded), scala2122)
+    val report = getUpdateReport(createMetaDep(excluded), scala2122)
     testScalahostIsMissing(report)
   }
 
   test("it should exclude any version of scala-library via * artifact id") {
     val toExclude = ExclusionRule("org.scala-lang", "*")
-    val report = getIvyReport(createLiftDep(toExclude), scala210)
+    val report = getUpdateReport(createLiftDep(toExclude), scala210)
     testScalaLibraryIsMissing(report)
   }
 
   test("it should exclude any version of scala-library via * org id") {
     val toExclude = ExclusionRule("*", "scala-library")
-    val report = getIvyReport(createLiftDep(toExclude), scala210)
+    val report = getUpdateReport(createLiftDep(toExclude), scala210)
     testScalaLibraryIsMissing(report)
   }
 
@@ -87,10 +81,9 @@ object InclExclSpec extends BaseIvySpecification {
       .cross(CrossVersion.full)
       .excludeAll(toExclude)
 
-  def getIvyReport(dep: ModuleID, scalaVersion: Option[String]): UpdateReport = {
-    cleanIvyCache()
-    val ivyModule = module(defaultModuleId, Vector(dep), scalaVersion)
-    ivyUpdate(ivyModule)
+  def getUpdateReport(dep: ModuleID, scalaVersion: Option[String]): UpdateReport = {
+    val m = module(defaultModuleId, Vector(dep), scalaVersion)
+    coursierUpdate(m)
   }
 
   def testLiftJsonIsMissing(report: UpdateReport): Unit = {
