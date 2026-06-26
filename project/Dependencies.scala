9d097012e0eb48d5976ac72a6d2578d3da5b7829
Jason Zaugg <jzaugg@gmail.com>
2026-06-26 16:58:33 +1000

    Update Coursier to version 2.1.25-M26
    Includes performance improvements.
    
    https://github.com/coursier/coursier/compare/v2.1.25-M25...v2.1.25-M26

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -121,7 +121,7 @@ object Dependencies {
 
   // lm-coursier dependencies
   val dataclassScalafixVersion = "0.3.0"
-  val coursierVersion = "2.1.25-M25"
+  val coursierVersion = "2.1.25-M26"
 
   val coursier = ("io.get-coursier" %% "coursier" % coursierVersion)
     .cross(CrossVersion.for3Use2_13)
