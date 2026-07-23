6f40a2a82724ce11037859f8304be66b97f2032f
Eugene Yokota <eed3si9n@gmail.com>
2026-07-23 18:58:08 -0400

    [2.x] fix: Fixes common settings with extraProjects
    **Problem**
    The presence of extraProjects broke common settings.
    
    **Solution**
    This fixes it by passing finalRoot.commonSettings.

--- main/src/main/scala/sbt/internal/Load.scala
+++ main/src/main/scala/sbt/internal/Load.scala
@@ -1129,7 +1129,7 @@ private[sbt] object Load {
       val newProjects = rest ++ discovered ++ projectLevelExtra
       val newAcc = acc :+ finalRoot
       val newGenerated = generated ++ generatedConfigClassFiles
-      loadTransitive1(newProjects, newAcc, newGenerated, commonSettings)
+      loadTransitive1(newProjects, newAcc, newGenerated, finalRoot.commonSettings)
     }
 
     // Load all config files AND process the project at the root directory, if it exists.
