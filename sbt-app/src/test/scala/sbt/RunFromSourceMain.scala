b823da2cd42fb36c0d8248f690b951c8b2149d78
Eugene Yokota <eed3si9n@gmail.com>
2026-08-23 19:09:49 -0400

    Remove explicit resolvers

--- sbt-app/src/test/scala/sbt/RunFromSourceMain.scala
+++ sbt-app/src/test/scala/sbt/RunFromSourceMain.scala
@@ -142,31 +142,26 @@ object RunFromSourceMain {
       val scalaHome1 = fakeboot / s"scala-$scalaVersion"
       val scalaHome1Lib = scalaHome1 / "lib"
       val scalaHome1Temp = scalaHome1 / "temp"
-      if (scalaHome1Lib.exists) log.info(s"""using $scalaHome1 that was found""")
+      if scalaHome1Lib.exists then log.info(s"""using $scalaHome1 that was found""")
       else {
         log.info(s"""creating $scalaHome1 by downloading scala-compiler $scalaVersion""")
         IO.createDirectories(List(scalaHome1Lib, scalaHome1Temp))
         val lm = {
           import lmcoursier.{ CoursierConfiguration, CoursierDependencyResolution }
-          CoursierDependencyResolution(
-            CoursierConfiguration().withResolvers(Vector(Resolver.mavenCentral))
-          )
+          CoursierDependencyResolution(CoursierConfiguration())
         }
         val Name = """(.*)(?:\-[\d.]+)\.jar""".r
         val BinPre = """(.*)(?:\-[\d.]+)-(?:bin|pre)-.*\.jar""".r
         val module = "org.scala-lang" % "scala3-compiler_3" % scalaVersion
-        lm.retrieve(module, scalaModuleInfo = None, scalaHome1Temp, log) match {
+        lm.retrieve(module, scalaModuleInfo = None, scalaHome1Temp, log) match
           case Left(w)     => throw w.resolveException
           case Right(jars) =>
             assert(jars.nonEmpty, s"no jars for scala $scalaVersion")
-            jars.foreach { f =>
-              val name = f.getName match {
+            jars.foreach: f =>
+              val name = f.getName match
                 case Name(name)   => name
                 case BinPre(name) => name
-              }
               IO.copyFile(f, scalaHome1Lib / s"$name.jar")
-            }
-        }
       }
       scalaHome1
     }
